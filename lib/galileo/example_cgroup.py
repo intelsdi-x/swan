ac100538e6116abb75bd3318b78912f617084cef
Niklas Q. Nielsen <nik@qni.dk>
2016-02-03 11:09:06 +0100

    Added first unit test for experiment driver

---
+++ lib/galileo/example_cgroup.py
@@ -0,0 +1,35 @@
+import ga
+from perf_counters import Perf
+from shell import Shell
+from taskset import Taskset
+from cgroup import Cgroup
+
+
+class CgroupExperiment(ga.Experiment):
+    def __init__(self):
+        ga.Experiment.__init__(self)
+
+        def baseline(configuration):
+            cg = Cgroup(["/A/cpu.shares=8192", "/A/hp/cpu.shares=1024", "/A/be/cpu.shares=2"])
+
+            Shell([
+                cg.execute("/A/hp", Perf(Taskset(["0"], "echo foobar"))),
+                cg.execute("/A/be", Perf(Taskset(["0"], "echo foobar")))
+            ])
+
+            cg.destroy()
+
+        def experiment(configuration):
+            Shell([Perf(Taskset(["0"], "sleep 1"))])
+
+        self.add_phase("baseline", baseline)
+        self.add_phase("experiment", experiment)
+
+
+def main():
+    s = CgroupExperiment()
+    s.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ lib/galileo/example_matrix.py
@@ -0,0 +1,25 @@
+import ga
+import glog as log
+
+
+class SimpleExperiment(ga.Experiment):
+    def __init__(self):
+        ga.Experiment.__init__(self)
+
+        def baseline(configuration):
+            pass
+
+        def experiment(configuration):
+            log.info("Test configuration: " + str(configuration))
+
+        self.add_phase("baseline", baseline)
+        self.add_phase("experiment", experiment, [["cpu", "mem"], range(0, 8), range(100, 1000, 100)])
+
+
+def main():
+    s = SimpleExperiment()
+    s.run()
+
+
+if __name__ == "__main__":
+    main()
