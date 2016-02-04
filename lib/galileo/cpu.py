0dbc36e025aaa293c7dac4e6790e036b6cd30b0f
Niklas Q. Nielsen <nik@qni.dk>
2016-02-04 13:28:48 +0100

    Added API comments for CPU abstraction

--- lib/galileo/cpu.py
+++ lib/galileo/cpu.py
@@ -2,6 +2,11 @@ import random
 
 
 def unique(source, count):
+    """
+    :param source: Map of sockets, cores or hyper threads.
+    :param count: Number of unique items to sample.
+    :return: A list of unique non-overlapping items from source map.
+    """
     keys = random.sample(source, count)
     output = []
     for key in keys:
@@ -11,30 +16,69 @@ def unique(source, count):
 
 class HyperThread:
     def __init__(self, id):
+        """
+        :param id: Processor Id for hyper threaded core.
+                   If hyper threads are not enabled,
+                   there will be a 1:1 mapping between hyper thread and core structure.
+        """
         self.id = id
         self.raw_data = {}
 
 
 class Core:
+    """
+    Represents a processor core. Cores consist of hyper threads.
+    If hyper threads are disabled, the hyper thread count is 1.
+    """
+
     def __init__(self, id):
+        """
+        :param id: Core Id
+        """
         self.id = id
         self.hyper_threads = {}
 
     def unique_hyper_threads(self, count):
+        """
+        :param count: Number of hyper threads.
+        :return: A list of "count" unique (non overlapping) hyper threads.
+                 If count is greater than the available hyper threads, all hyper threads are returned.
+        """
         return unique(self.hyper_threads, count)
 
 
 class Socket:
+    """
+    Represents physical processor die (with it's own socket).
+    Sockets contains a number of "cores", which then contains the actual "hyper threads".
+    """
+
     def __init__(self, id):
+        """
+        :param id: Physical Processor Id
+        """
         self.id = id
         self.cores = {}
 
     def unique_cores(self, count):
+        """
+        :param count: Number of cores.
+        :return: A list of "count" unique (non overlapping) cores. If count is greater than the available cores,
+                 all cores are returned.
+        """
         return unique(self.cores, count)
 
 
 class Cpus:
+    """
+    Represents all available sockets, cores and hyper threads available on the system.
+    """
+
     def __init__(self, cpu_info_file="/proc/cpuinfo"):
+        """
+        :param cpu_info_file: cpuinfo file to use i.e. /proc/cpuinfo
+               Usually, this should be left alone and is only made configurable for testing purposes.
+        """
         self.hyper_threads = {}
         self.sockets = {}
         next_cpu = None
@@ -74,4 +118,9 @@ class Cpus:
                 self.sockets[socket_id].cores[core_id].hyper_threads[hyper_thread.id] = hyper_thread
 
     def unique_sockets(self, count):
+        """
+        :param count: Number of sockets.
+        :return: A list of "count" unique (non overlapping) sockets. If count is greater than the available sockets,
+                 all sockets are returned.
+        """
         return unique(self.sockets, count)
