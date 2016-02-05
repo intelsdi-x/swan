4503da10a8af9b2a6005fb18640d3951c6ae6522
Niklas Q. Nielsen <nik@qni.dk>
2016-02-05 12:30:01 +0100

    Added cgroup tests

--- lib/galileo/cgroup.py
+++ lib/galileo/cgroup.py
@@ -1,22 +1,99 @@
 import glog as log
 import subprocess
 import sys
+import time
+import collections
 
 
 class Cgroup:
-    def __init__(self, desired_states):
+    """
+    Cgroup hierarchy control class: creates and configures cgroups hierarchies based on a set of 'desired states'.
+    See __init__() documentation for format of desired states.
+    """
+
+    def __init__(self, desired_states, create_func=None, destroy_func=None, set_func=None):
+        """
+        :param desired_states: List of desired states in form of <path>=<value>.
+                               For example, ["/A/B/cpu.shares=1", "/A/C/mem.limit_in_bytes=512"]
+                               The above will create:
+                               1) The 'A' root of the two children with 'cpu' and 'mem' cgroups
+                               2) 'B' child cgroup with 'cpu' and 'mem' and set cpu shares to 1
+                               3) 'C' child cgroup with 'cpu' and 'mem' and set memory limit to 512 bytes
+
+                               The settings will be applied in order, so settings in the parents can be set before the
+                               children's settings are applied. This is necessary for setting 'cpuset's.
+        :param create_func:    Function to use for creating cgroups. For testing purposes only.
+        :param destroy_func:   Function to use for destroy cgroups. For testing purposes only.
+        :param set_func:       Function to use for setting cgroup parameters. For testing purposes only.
+        """
         self.desired_states = desired_states
+        self.cgroup_types = collections.OrderedDict()
+        self.hierarchies = []
 
-        self.cgroup_types = {}
-        self.hierachies = []
+        # For testing purposes, creating, destroying and changing cgroups settings have been pulled into functions
+        # which can be provided by the caller.
+        def create(cgroup_types, location):
+            """
+            :param cgroup_types: List of types. For example ['cpu', 'mem']
+            :param location: Path of cgroup (relative from '/sys/fs/cgroup/<type>').
+                   For example, '/A/B' for '/sys/fs/cgroup/cpu/A/B'.
+            :return: True if operation succeeded.
+            """
+            log.info("creating cgroup " + ",".join(cgroup_types) + ":" + location)
+            if subprocess.call(["cgcreate", "-g", ",".join(self.cgroup_types) + ":" + location]) is not 0:
+                return False
+            return True
 
-        hierarchy_tree = {}
+        def destroy(cgroup_types, location):
+            """
+            :param cgroup_types: List of types. For example ['cpu', 'mem']
+            :param location:     Path of cgroup (relative from '/sys/fs/cgroup/<type>').
+                                 For example, '/A/B' for '/sys/fs/cgroup/cpu/A/B'.
+            :return:             True if operation succeeded.
+            """
+            if subprocess.call(["cgdelete", ",".join(cgroup_types) + ":" + location]) is not 0:
+                return False
+            return True
+
+        def set_parameter(cgroup_type, location, parameter, value):
+            """
+            :param cgroup_type:  Cgroup type. For example 'cpu' or 'mem'
+            :param location:     Path of cgroup (relative from '/sys/fs/cgroup/<type>').
+                                 For example, '/A/B' for '/sys/fs/cgroup/cpu/A/B'.
+            :param parameter:    Parameter to set. For example 'cpu.shares' for '/A/B/cpu.shares=1'
+            :param value:        Value to set. For example '1' for '/A/B/cpu.shares=1'
+            :return:             True if operation succeeded.
+            """
+            if subprocess.call(["sh", "-c", "echo '%s' > /sys/fs/cgroup/%s%s/%s" % (value, cgroup_type, location, parameter)]) is not 0:
+                return False
+            return True
+
+        if create_func is None:
+            create_func = create
+
+        if destroy_func is None:
+            destroy_func = destroy
+
+        if set_func is None:
+            set_func = set_parameter
+
+        self.create_func = create_func
+        self.destroy_func = destroy_func
+        self.set_func = set_func
+
+        hierarchy_tree = collections.OrderedDict()
 
         for desired_state in desired_states:
             components = desired_state.split("=")
-            state_value = components[1]
 
             if len(components) != 2:
+                log.warning("unknown format: '%s'. skipping!" % desired_state)
+                continue
+
+            state_value = components[1]
+
+            if state_value == "":
+                log.warning("no value set in: '%s'. skipping!" % desired_state)
                 continue
 
             hierarchy_components = components[0].split("/")
@@ -30,13 +107,18 @@ class Cgroup:
             cgroup_type = hierarchy_components[len(hierarchy_components) - 1]
 
             cgroup_type_components = cgroup_type.split(".")
+
+            if len(cgroup_type_components) != 2:
+                log.warning("Could not determine cgroup type from parameter name '%s'. skipping!")
+                continue
+
             self.cgroup_types[cgroup_type_components[0]] = cgroup_type_components[0]
 
             # Build hierarchy tree
             root = hierarchy_tree
             for path_component in hierarchy:
                 if path_component not in root:
-                    root[path_component] = {}
+                    root[path_component] = collections.OrderedDict()
 
                 root = root[path_component]
 
@@ -51,8 +133,8 @@ class Cgroup:
             # NOTE: Skip first (empty) root
             if location is not "":
                 log.info("creating cgroup " + ",".join(self.cgroup_types) + ":" + location)
-                if subprocess.call(["cgcreate", "-g", ",".join(self.cgroup_types) + ":" + location]) is not 0:
-                    log.fatal("could not create cgroup")
+                if not self.create_func(",".join(self.cgroup_types), location):
+                    log.fatal("could not create cgroup: %s" % location)
                     sys.exit(1)
 
             # NOTE: We have to apply parent settings before child setting in cgroups like "cpuset".
@@ -61,10 +143,9 @@ class Cgroup:
                     parameter_component = key.split(".")
                     parameter_type = parameter_component[0]
 
-                    log.info("setting %s=%s" % (location + key, element))
+                    log.info("setting %s=%s" % (location + "/" + key, element))
 
-                    if subprocess.call(["sh", "-c", "echo '%s' > /sys/fs/cgroup/%s%s/%s" % (
-                    element, parameter_type, location, key)]) is not 0:
+                    if not self.set_func(parameter_type, location, key, element):
                         log.fatal("Could not set configuration: %s = %s" % (key, element))
                         sys.exit(1)
 
@@ -88,11 +169,12 @@ class Cgroup:
             if location is not "":
                 log.info("deleting cgroup " + ",".join(self.cgroup_types) + ":" + location)
 
-                if subprocess.call(["cgdelete", ",".join(self.cgroup_types) + ":" + location]) is not 0:
+                if not self.destroy_func(",".join(self.cgroup_types), location):
                     log.fatal("could not delete cgroup: " + location)
                     sys.exit(1)
 
         dfs(self.hierarchy_tree, "")
 
         # HACK: Make sure cgroups deletion is done before recreating exlusive cpusets (which will otherwise fail).
+        # TODO(nnielsen): Add more deterministic way of telling that the operation is done.
         time.sleep(0.5)
