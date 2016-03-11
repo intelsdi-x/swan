6b5d6417f60d0ff3ced86d4fe4b9f5dc79ec0a3d
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-03-11 08:37:05 +0100

    Implemented Experiment component;
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- experiments/memcached_profile/main.go
+++ experiments/memcached_profile/main.go
@@ -2,10 +2,86 @@ package main
 
 import (
 	log "github.com/Sirupsen/logrus"
+
+	"github.com/intelsdi-x/swan/pkg/experiment"
 )
 
+type MemcachedSensitivityProfile struct {
+	*experiment.Experiment
+}
+
+func NewMemcachedSensitivityProfile() *MemcachedSensitivityProfile {
+	experiment := MemcachedSensitivityProfile{experiment.NewExperiment()}
+	return &experiment
+}
+
+func (m *MemcachedSensitivityProfile) Init() {
+	m.Name = "Memcached Sensitivity Profile"
+	// Default experiment length in seconds
+	m.Duration = 30
+	// Expected SLO: 99%ile latency 5ms
+	m.TargetSlo99pUs = 5000
+
+	m.FindQPSAndLoadPoints()
+
+	// Add phases.
+	m.AddBaselinePhase(MemcachedBaselinePhase{})
+	m.AddPhase(MemcachedWithL1InstructionPressurePhase{})
+}
+
+func (m *MemcachedSensitivityProfile) FindQPSAndLoadPoints() {
+	log.Debug("Tuning phase. Finding QPS for ", m.TargetSlo99pUs, " SLO")
+	// TODO(bplotka): Find QPSs from SLO
+
+	// Let's assume for test 100:
+	targetQPS := uint(100)
+	m.InitLoadPoints(targetQPS)
+}
+
+// Baseline Phase
+type MemcachedBaselinePhase struct {}
+
+func (m MemcachedBaselinePhase) GetBestEffortWorkloadName() string {
+	return "None"
+}
+
+func (m MemcachedBaselinePhase) Run(stresserLoad uint) float64 {
+	// TODO(bplotka): Baseline memcached:
+	// - create Cgroup topology
+	// - run memcached
+	// - run mutilate
+	// - clean cgroups
+	// - return SLIs (Measurements)
+
+	// TODO(bplotka): Move that to tests
+	// Fake SLI measurement.
+	return float64(stresserLoad * 10)
+}
+
+// L1InstructionPressure Test
+type MemcachedWithL1InstructionPressurePhase struct {}
+
+func (m MemcachedWithL1InstructionPressurePhase) GetBestEffortWorkloadName() string {
+	return "L1 Instruction Pressure"
+}
+
+func (m MemcachedWithL1InstructionPressurePhase) Run(stresserLoad uint) float64 {
+	// TODO(bplotka): Run Measurement with memcached & antagonist:
+	// - create Cgroup topology
+	// - run memcached
+	// - run aggressor (here: L1InstructionPressure)
+	// - run mutilate
+	// - clean cgroups
+	// - return SLIs (Measurements)
+
+	// TODO(bplotka): Move that to tests
+	// Fake SLI measurement (+ 10 to all latencies ((: )
+	return float64((stresserLoad * 10) + 10)
+}
 
 func main() {
-	logger := log.New()
-	logger.Info("Running memcached_profile")
+	experiment := NewMemcachedSensitivityProfile()
+	experiment.Init()
+	experiment.Run()
+
 }
