08327ffcad410a045654f8a23a793520819700b0
Niklas Q. Nielsen <nik@qni.dk>
2016-03-17 11:47:35 -0700

    Initial commit for shell abstraction

---
+++ pkg/shell/command.go
@@ -0,0 +1,20 @@
+package shell
+
+// Command encapsulated a command to be executed.
+// Instead of using a flat string, this struct is meant to contain
+// details which user to run as, permission, work directory, etc.
+type Command struct {
+	command string
+}
+
+// NewCommand returns a command instance.
+func NewCommand(command string) *Command {
+	return &Command{
+		command: command,
+	}
+}
+
+// CommandString returns the command to run.
+func (c *Command) CommandString() string {
+	return c.command
+}
