78ff75ede068bd7c43bc4f01a61ace739f7bd89a
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-04-04 14:06:00 +0200

    Fixed scripts
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
+
 echo "Getting golint if not found"
 golint || go get -u github.com/golang/lint/golint
 
 echo "Checking dummy style"
 sh -c "cd pkg/dummy && golint"
 
-echo "Checking shell style"
-sh -c "cd pkg/shell && golint"
+echo "Checking provisioning style"
+sh -c "cd pkg/provisioning && golint"
