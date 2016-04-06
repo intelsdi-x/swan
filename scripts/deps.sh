04b97d1392b9a244e6d2544bb31dfd5930f51b0c
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-04-06 12:25:28 +0200

    Fixed scripts to automatically discover all packages.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+
 echo "Getting godep if not found"
 go get github.com/tools/godep
 
-echo "Checking dummy dependencies"
-sh -c "cd pkg/dummy && godep restore"
+for package in `scripts/get_all_pkg.sh`
+do
+    echo "Checking ${package} style"
+    sh -c "cd pkg/${package} && godep restore"
+done
