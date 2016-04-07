371e780809e6e0eed8464c59eb2eaf6b010da202
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-04-07 19:38:28 +0200

    Reorder local provisioing.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- pkg/provisioning/local.go
+++ pkg/provisioning/local.go
@@ -10,6 +10,79 @@ import (
 	"errors"
 )
 
+// Local provisioning is responsible for providing the execution environment
+// on local machine via exec.Command. It also needed to setup given isolation
+// using Isolation Manager.
+// It runs command as current user.
+type Local struct{
+	isolations []isolation.ProcessIsolation
+}
+
+// NewLocal returns a Local instance.
+func NewLocal(isolations []isolation.ProcessIsolation) Local {
+	l := Local{
+		isolations: isolations,
+	}
+	return l
+}
+
+// Run runs the command given as input.
+// Returned Task is able to stop & monitor the provisioned process.
+func (l Local) Run(command string) (Task, error) {
+	statusCh := make(chan Status)
+
+	log.Debug("Starting ", command)
+
+	cmd := exec.Command("sh", "-c", command)
+
+	// It is important to set additional Process Group ID for parent process and his children
+	// to have ability to kill all the children processes.
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	// Setting Buffer as io.Writer for Command output.
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Start()
+	if (err != nil) {
+		return nil, err
+	}
+
+	log.Debug("Started with pid ", cmd.Process.Pid)
+
+	// Wait for local task in goroutine.
+	go func() {
+		// Wait for task completion.
+		cmd.Wait()
+
+		log.Debug(
+		  "Ended ", command,
+		  " with output: ", stdout.String(),
+		  " with err output: ", stderr.String(),
+		  " with status code: ",
+		  (cmd.ProcessState.Sys().(syscall.WaitStatus)).ExitStatus())
+
+		statusCh <- Status{
+			(cmd.ProcessState.Sys().(syscall.WaitStatus)).ExitStatus(),
+			stdout.String(),
+			stderr.String(),
+		}
+	}()
+
+	taskPid := isolation.TaskPID(cmd.Process.Pid)
+
+	// Perform rest of the isolation synchronously.
+	for _, isolation := range l.isolations {
+		isolation.Isolate(taskPid)
+	}
+
+	t := newLocalTask(taskPid, statusCh)
+
+	return t, err
+}
+
 // LocalTask implements Task interface.
 type LocalTask struct{
 	pid isolation.TaskPID
@@ -86,77 +159,4 @@ func (task *LocalTask) Wait(timeoutMs int) bool {
 	}
 
 	return true
-}
-
-// Local provisioning is responsible for providing the execution environment
-// on local machine via exec.Command. It also needed to setup given isolation
-// using Isolation Manager.
-// It runs command as current user.
-type Local struct{
-	isolations []isolation.ProcessIsolation
-}
-
-// NewLocal returns a Local instance.
-func NewLocal(isolations []isolation.ProcessIsolation) Local {
-	l := Local{
-		isolations: isolations,
-	}
-	return l
-}
-
-// Run runs the command given as input.
-// Returned Task is able to stop & monitor the provisioned process.
-func (l Local) Run(command string) (Task, error) {
-	statusCh := make(chan Status)
-
-	log.Debug("Starting ", command)
-
-	cmd := exec.Command("sh", "-c", command)
-
-	// It is important to set additional Process Group ID for parent process and his children
-	// to have ability to kill all the children processes.
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-
-	// Setting Buffer as io.Writer for Command output.
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err := cmd.Start()
-	if (err != nil) {
-		return nil, err
-	}
-
-	log.Debug("Started with pid ", cmd.Process.Pid)
-
-	// Wait for local task in goroutine.
-	go func() {
-		// Wait for task completion.
-		cmd.Wait()
-
-		log.Debug(
-		  "Ended ", command,
-		  " with output: ", stdout.String(),
-		  " with err output: ", stderr.String(),
-		  " with status code: ",
-		  (cmd.ProcessState.Sys().(syscall.WaitStatus)).ExitStatus())
-
-		statusCh <- Status{
-			(cmd.ProcessState.Sys().(syscall.WaitStatus)).ExitStatus(),
-			stdout.String(),
-			stderr.String(),
-		}
-	}()
-
-	taskPid := isolation.TaskPID(cmd.Process.Pid)
-
-	// Perform rest of the isolation synchronously.
-	for _, isolation := range l.isolations {
-		isolation.Isolate(taskPid)
-	}
-
-	t := newLocalTask(taskPid, statusCh)
-
-	return t, err
 }
