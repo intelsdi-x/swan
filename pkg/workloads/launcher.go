ab4e7ba2e30e2f167853d41ac2c2eccec2798f60
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-04-07 15:10:16 +0200

    Added Launcher and LoadGenerator interfaces.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

---
+++ pkg/workloads/launcher.go
@@ -0,0 +1,8 @@
+package workloads
+
+// Launcher defines how workload should be run using specified Provisioners.
+type Launcher interface {
+	// Launch launches the workload.
+	// Returned Task is able to stop & monitor the provisioned process.
+	Launch() Task
+}
---
+++ pkg/workloads/load.go
@@ -0,0 +1,10 @@
+package workloads
+
+// LoadGenerator launches stresser which generates load on specified workload.
+type LoadGenerator interface {
+	// Tune does the tuning phase. With the given SLO.
+	Tune(slo int, timeoutMs int) (targetQPS int)
+
+	// Load generates load on specific workload.
+	Load(qps int, durationMs int) (sli int)
+}
