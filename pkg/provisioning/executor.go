229003909c3cfcfb95e2e3cdc5bb0b7894d6f0c9
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-04-11 12:33:04 +0200

    Renamed provisioning pkg to executor.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- pkg/provisioning/executor.go
+++
@@ -1,7 +0,0 @@
-package provisioning
-
-// Executor is responsible for creating execution environment for given workload.
-// It returns a Task interface.
-type Executor interface{
-	Execute(command string) (Task, error)
-}
