ed95f229ada7c15c803eec615bf9c1043c4ece2c
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-04-11 11:47:37 +0200

    Moved from bool to enum in Status() return type.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- pkg/executor/task.go
+++ pkg/executor/task.go
@@ -1,12 +1,22 @@
 package executor
 
-// Task represents a process which can be stoped or monitored.
+// TaskState is an enum presenting current task state.
+type TaskState int
+
+const (
+	// RUNNING task state means that task is still running.
+	RUNNING TaskState = iota
+	// TERMINATED task state means that task completed or stopped.
+	TERMINATED
+)
+
+// Task represents a process which can be stopped or monitored.
 type Task interface{
 	// Stops a task.
 	Stop() error
-	// Fetches status of the task when the task is terminated. Otherwise return running true
-	// and nil
-	Status() (bool, *Status)
+	// Status returns a state of the task. If task is terminated it returns the Status as a
+	// second item in tuple. Otherwise returns nil.
+	Status() (TaskState, *Status)
 	// Waits for the task completion.
 	// In case of 0 timeout it will be endlessly blocked.
 	// Returns false after timeout exceeds.
