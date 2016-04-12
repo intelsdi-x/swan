dfae722afdb2a1d42f2e2d2bc130ed70fda4dae7
Maciej Patelczyk <maciej.patelczyk@intel.com>
2016-04-12 16:54:41 +0200

    SCE-155: Renamed experimentDriver to sensitivity and added new Convey test
    Renamed package name and internal structures from experimentDriver to
    sensitivity which simplifies naming.
    
    Added first Convey test with faked launchers to test framework.
    
    Signed-off-by: Maciej Patelczyk <maciej.patelczyk@intel.com>

---
+++ pkg/sensitivity/sensitivity.go
@@ -0,0 +1,123 @@
+package sensitivity
+
+import "github.com/intelsdi-x/swan/pkg/workloads"
+
+//Configuration
+type Configuration struct {
+	SLO             int
+	TuningTimeout   int
+	LoadDuration    int
+	LoadPointsCount int
+}
+
+//Experiment
+type Experiment struct {
+	// Latency Sensitivity workload
+	pr workloads.Launcher
+	// Workload Generator for Latency Sensitivity workload
+	lgForPr workloads.LoadGenerator
+	// Aggressors to be but aside to LC workload
+	be         []workloads.Launcher
+	ec         Configuration
+	targetLoad int
+	slis       [][]int
+}
+
+// Construct new Experiment object.
+//NewExperiment()
+func NewExperiment(
+	configuration Configuration,
+	productionTaskLauncher workloads.Launcher,
+	loadGeneratorForProductionTask workloads.LoadGenerator,
+	aggressorTaskLaunchers []workloads.Launcher) *Experiment {
+
+	//TODO: Validate configuration.
+
+	return &Experiment{
+		pr:      productionTaskLauncher,
+		lgForPr: loadGeneratorForProductionTask,
+		be:      aggressorTaskLaunchers,
+		ec:      configuration,
+	}
+}
+
+// Runs single measurement of PR workload with given aggressor.
+// Takes aggressor workload and specific loadPoint (rps)
+// Return (sli, nil) on success (0, error) otherwise.
+func (e *Experiment) runMeasurement(
+	aggressor workloads.Launcher,
+	qps int) (sli int, err error) {
+
+	prTask, err := e.pr.Launch()
+	if err != nil {
+		return 0, err
+	}
+	defer prTask.Stop()
+
+	agrTask, err := aggressor.Launch()
+	if err != nil {
+		return 0, err
+	}
+	sli, err = e.lgForPr.Load(qps, e.ec.LoadDuration)
+
+	agrTask.Stop()
+	return sli, err
+}
+
+func (e *Experiment) runPhase(
+	aggressor workloads.Launcher) (slis []int, err error) {
+	slis = make([]int, e.ec.LoadPointsCount)
+
+	loadStep := int(e.targetLoad / e.ec.LoadPointsCount)
+
+	for load := loadStep; load < e.targetLoad; load += loadStep {
+		result, err := e.runMeasurement(aggressor, load)
+		if err != nil {
+			return nil, err
+		}
+		slis = append(slis, result)
+	}
+	return slis, err
+}
+
+func (e *Experiment) runTunning() error {
+	prTask, err := e.pr.Launch()
+	if err != nil {
+		return err
+	}
+	defer prTask.Stop()
+
+	e.targetLoad, err = e.lgForPr.Tune(e.ec.SLO, e.ec.TuningTimeout)
+	if err != nil {
+		e.targetLoad = -1
+		return err
+	}
+	return err
+}
+
+// Prints nice output. TBD
+func (e *Experiment) PrintSensitivityProfile() error {
+	return nil
+}
+
+func (e *Experiment) Run() error {
+	var err error
+
+	err = e.runTunning()
+	if err != nil {
+		//Stop here
+		return err
+	}
+
+	for _, aggressor := range e.be {
+		slis, err := e.runPhase(aggressor)
+		if err != nil {
+			return err
+		}
+		e.slis = append(e.slis, slis)
+	}
+
+	//That's TBD.
+	err = e.PrintSensitivityProfile()
+	return err
+}
