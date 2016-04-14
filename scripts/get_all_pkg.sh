8d2ec1cf0799f461e2dca717ffd05cb71da4d118
Maciej Patelczyk <maciej.patelczyk@intel.com>
2016-04-14 16:22:56 +0200

    Improvement for list packages in project needs for current build system
    Signed-off-by: Maciej Patelczyk <maciej.patelczyk@intel.com>

--- scripts/get_all_pkg.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This scripts discover all packages under pkg directory.
-cd ./pkg
-
-for package in *
-do
-    echo ${package}
-done
--- scripts/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for package in `scripts/get_all_pkg.sh`
-do
-    go test -v github.com/intelsdi-x/swan/pkg/${package}
-done
