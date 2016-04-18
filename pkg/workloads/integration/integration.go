b6219277f8b30ad43c893513f613b29daaf73370
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-04-18 17:46:44 +0200

    Moved integration test to separate directory with special tag.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

---
+++ pkg/workloads/integration/integration.go
@@ -0,0 +1,3 @@
+package workloads_integration
+
+// This file necessary to load the package when tag is unit.
--- pkg/workloads/memcached_test.go
+++ pkg/workloads/memcached_test.go
@@ -3,7 +3,6 @@ package workloads
 import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/intelsdi-x/swan/mocks"
-	"github.com/intelsdi-x/swan/pkg/executor"
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
@@ -33,48 +32,6 @@ func TestMemcachedWithMockedExecutor(t *testing.T) {
 		Convey("Arguments passed to Executor should be a proper command", func() {
 			memcachedLauncher.Launch()
 			mockedExecutor.AssertExpectations(t)
-		})
-	})
-}
-
-// Before test, make sure you have built memcached in /usr/bin e.g:
-// ln -s SWAN_REPO/workloads/data_caching/memcached/memcached-1.4.25/memcached /usr/bin/memcached
-const (
-	memcachedPath = "/usr/bin/memcached"
-)
-
-// TestMemcachedWithExecutor is an integration test with local executor.
-func TestMemcachedWithExecutor(t *testing.T) {
-	log.SetLevel(log.ErrorLevel)
-
-	SkipConvey("While using Local Shell in Memcached launcher", t, func() {
-		memcachedLauncher := NewMemcached(
-			executor.NewLocal(),
-			DefaultMemcachedConfig(memcachedPath))
-
-		Convey("When memcached is launched", func() {
-			task, err := memcachedLauncher.Launch()
-
-			Convey("There should be no error", func() {
-				So(err, ShouldBeNil)
-
-				task.Stop()
-			})
-
-			Convey("When we stop the task", func() {
-				err := task.Stop()
-
-				Convey("There should be no error", func() {
-					So(err, ShouldBeNil)
-				})
-
-				Convey("The task should be terminated and the task status should be -1", func() {
-					taskState, taskStatus := task.Status()
-					So(taskState, ShouldEqual, executor.TERMINATED)
-					So(taskStatus.ExitCode, ShouldEqual, -1)
-				})
-			})
-
 		})
 	})
 }
