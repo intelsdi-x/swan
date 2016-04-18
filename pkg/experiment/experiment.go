d9ed502f5828b0202d2ac52be6bb5bd782be2a3e
Maciej Patelczyk <maciej.patelczyk@intel.com>
2016-04-18 14:43:21 +0200

    Core refactoring
    Added Phase structure to hold phase name
    Moved math func into math.go file.
    
    Signed-off-by: Maciej Patelczyk <maciej.patelczyk@intel.com>

--- pkg/experiment/experiment.go
+++ pkg/experiment/experiment.go
@@ -1,27 +1,29 @@
 package experiment
 
-import (
-	"errors"
-	"math"
-)
+import "errors"
 
 type PhaseFunc func() (float64, error)
 
-type Configuration struct {
+type Phase struct {
+	Name      string
+	PhaseFunc PhaseFunc
+}
+
+type ExperimentConfiguration struct {
 	MaxVariance   float64
 	PhaseRepCount int
 }
 
 type Experiment struct {
-	conf    Configuration
-	phases  []PhaseFunc
+	conf    ExperimentConfiguration
+	phases  []Phase
 	results []float64
 }
 
 // Construct new Experiment object.
 func NewExperiment(
-	configuration Configuration,
-	phases []PhaseFunc) *Experiment {
+	configuration ExperimentConfiguration,
+	phases []Phase) *Experiment {
 
 	return &Experiment{
 		conf:   configuration,
@@ -29,36 +31,12 @@ func NewExperiment(
 	}
 }
 
-func (e *Experiment) AddPhase(p PhaseFunc) error {
-	e.phases = append(e.phases, p)
-	return nil
-}
-
-func average(x []float64) float64 {
-	var avr float64
-	for _, val := range x {
-		avr += val
-	}
-	avr /= float64(len(x))
-	return avr
-}
-func variance(x []float64) float64 {
-
-	avr := average(x)
-	variance := float64(0)
-	for _, val := range x {
-		variance += math.Sqrt(math.Abs(avr - val))
-	}
-	variance /= float64(len(x))
-	return variance
-}
-
 func (e *Experiment) Run() error {
 	var err error
 
 	for _, phase := range e.phases {
 		for i := 0; i < e.conf.PhaseRepCount; i++ {
-			result, err := phase()
+			result, err := phase.PhaseFunc()
 			if err != nil {
 				return nil
 			}
