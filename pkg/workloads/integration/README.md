002b3ad05306cb80886c32c006ee3435668ab0eb
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-04-18 17:46:44 +0200

    Moved integration test to separate directory with special tag.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

---
+++ pkg/workloads/integration/README.md
@@ -0,0 +1,18 @@
+# Workload integration tests.
+
+In this directory there all integration tests for each workload. These tests requires some
+setup before execution like special packages and building the workload binary.
+
+After setup you can run them in following manner:
+
+`go test -tags=integration`
+
+To create integration test file we use build tags, so you need to place
+
+```
+// +build integration
+<newline>
+package workloads_integration
+```
+
+(Make sure you place newline between package name and build flag)
---
+++ pkg/workloads/integration/memcached_test.go
@@ -0,0 +1,55 @@
+// +build integration
+
+package workloads_integration
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/intelsdi-x/swan/pkg/executor"
+	"github.com/intelsdi-x/swan/pkg/workloads"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+// Before test, make sure you have built memcached in /usr/bin e.g:
+// ln -s SWAN_REPO/workloads/data_caching/memcached/memcached-1.4.25/memcached /usr/bin/memcached
+// And user memcached is present.
+
+const (
+	memcachedPath = "/usr/bin/memcached"
+)
+
+// TestMemcachedWithExecutor is an integration test with local executor.
+func TestMemcachedWithExecutor(t *testing.T) {
+	log.SetLevel(log.ErrorLevel)
+
+	Convey("While using Local Shell in Memcached launcher", t, func() {
+		memcachedLauncher := workloads.NewMemcached(
+			executor.NewLocal(),
+			workloads.DefaultMemcachedConfig(memcachedPath))
+
+		Convey("When memcached is launched", func() {
+			task, err := memcachedLauncher.Launch()
+
+			Convey("There should be no error", func() {
+				So(err, ShouldBeNil)
+
+				task.Stop()
+			})
+
+			Convey("When we stop the task", func() {
+				err := task.Stop()
+
+				Convey("There should be no error", func() {
+					So(err, ShouldBeNil)
+				})
+
+				Convey("The task should be terminated and the task status should be -1", func() {
+					taskState, taskStatus := task.Status()
+					So(taskState, ShouldEqual, executor.TERMINATED)
+					So(taskStatus.ExitCode, ShouldEqual, -1)
+				})
+			})
+
+		})
+	})
+}
