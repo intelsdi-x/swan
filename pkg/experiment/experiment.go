1deed3f80c03f82812558dc7bb67b54bb129b047
Maciej Patelczyk <maciej.patelczyk@intel.com>
2016-04-18 16:30:12 +0200

    Added Convey tests and error checking in NewExperiment
    Signed-off-by: Maciej Patelczyk <maciej.patelczyk@intel.com>

--- pkg/experiment/experiment.go
+++ pkg/experiment/experiment.go
@@ -23,12 +23,27 @@ type Experiment struct {
 // Construct new Experiment object.
 func NewExperiment(
 	configuration ExperimentConfiguration,
-	phases []Phase) *Experiment {
+	phases []Phase) (*Experiment, error) {
+
+	if configuration.MaxVariance <= 0 {
+		return nil, errors.New("Invalid argument: variance")
+	}
+	if configuration.PhaseRepCount <= 0 {
+		return nil, errors.New("Invalid argument: PhaseRepCount")
+	}
+	if len(phases) == 0 {
+		return nil, errors.New("Invalid argument: nil phase slice")
+	}
+	for _, phase := range phases {
+		if phase.PhaseFunc == nil {
+			return nil, errors.New("Invalid argument: nil PhaseFunc")
+		}
+	}
 
 	return &Experiment{
 		conf:   configuration,
 		phases: phases,
-	}
+	}, nil
 }
 
 func (e *Experiment) Run() error {
@@ -38,7 +53,7 @@ func (e *Experiment) Run() error {
 		for i := 0; i < e.conf.PhaseRepCount; i++ {
 			result, err := phase.PhaseFunc()
 			if err != nil {
-				return nil
+				return err
 			}
 			e.results = append(e.results, result)
 		}
