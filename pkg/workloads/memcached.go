bc6401cc7166c5f988769f665aca69d78aba726d
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-04-18 15:50:33 +0200

    Refactored memcached and add memcached test.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- pkg/workloads/memcached.go
+++ pkg/workloads/memcached.go
@@ -1,38 +1,71 @@
 package workloads
 
-import "github.com/intelsdi-x/swan/pkg/executor"
-
-const (
-	memcachedBinaryName = "memcached"
+import (
+	"fmt"
+	"github.com/intelsdi-x/swan/pkg/executor"
 )
 
-// Memcached is a launcher for memcached data caching application.
-type Memcached struct {
-	exec         executor.Executor
-	pathToBinary string
-	user         string
-	threadsNum   int
-	connNum      int
+const (
+	memcachedDefaultPort           = 11211
+	memcachedDefaultUser           = "memcached"
+	memcachedDefaultNumThreads     = 4
+	memcachedDefaultMaxMemoryMB    = 64
+	memcachedDefaultNumConnections = 1024
+)
+
+// MemcachedConfig is a config for the memcached data caching application v 1.4.25.
+// memcached 1.4.25, supported options:
+// -p <num>      TCP port number to listen on (default: 11211)
+// -s <file>     UNIX socket path to listen on (disables network support)
+// -u <username> assume identity of <username> (only when run as root)
+// -m <num>      max memory to use for items in megabytes (default: 64 MB)
+// -c <num>      max simultaneous connections (default: 1024)
+//-v            verbose (print errors/warnings while in event loop)
+//-vv           very verbose (also print client commands/reponses)
+//-vvv          extremely verbose (also print internal state transitions)
+//-t <num>      number of threads to use (default: 4)
+type MemcachedConfig struct {
+	pathToBinary   string
+	port           int
+	user           string
+	numThreads     int
+	maxMemoryMB    int
+	numConnections int
 }
 
-// NewMemcached is a constructor for Memcached.
-func NewMemcached(exec executor.Executor, pathToBinary string,
-	user string, threadsNum int, connNum int) Memcached {
-	return Memcached{
-		exec:         exec,
-		pathToBinary: pathToBinary,
-		user:         user,
-		threadsNum:   threadsNum,
-		connNum:      connNum,
+// DefaultMemcachedConfig is a constructor for MemcachedConfig with default parameters.
+func DefaultMemcachedConfig(pathToBinary string) MemcachedConfig {
+	return MemcachedConfig{
+		pathToBinary,
+		memcachedDefaultPort,
+		memcachedDefaultUser,
+		memcachedDefaultNumThreads,
+		memcachedDefaultMaxMemoryMB,
+		memcachedDefaultNumConnections,
 	}
 }
 
+// Memcached is a launcher for the memcached data caching application v 1.4.25.
+type Memcached struct {
+	exec executor.Executor
+	conf MemcachedConfig
+}
+
+// NewMemcached is a constructor for Memcached.
+func NewMemcached(exec executor.Executor, config MemcachedConfig) Memcached {
+	return Memcached{
+		exec: exec,
+		conf: config,
+	}
+
+}
 func (m Memcached) buildCommand() string {
-	return m.pathToBinary + memcachedBinaryName +
-		" -u " + m.user +
-		" -t " + string(m.threadsNum) +
-		" -c " + string(m.connNum) +
-		" -m mem"
+	return fmt.Sprint(m.conf.pathToBinary,
+		" -p ", m.conf.port,
+		" -u ", m.conf.user,
+		" -t ", m.conf.numThreads,
+		" -m ", m.conf.maxMemoryMB,
+		" -c ", m.conf.numConnections)
 }
 
 // Launch starts the workload (process or group of processes). It returns a workload
--- pkg/workloads/memcached_test.go
+++ pkg/workloads/memcached_test.go
@@ -1,9 +1,69 @@
 package workloads
 
 import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/intelsdi-x/swan/mocks"
+	"github.com/intelsdi-x/swan/pkg/executor"
+	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
 
-// TestMemcached
-func TestMemcached(t *testing.T) {
+// TestMemcachedBuildCommand
+func TestMemcachedBuildCommand(t *testing.T) {
+	log.SetLevel(log.ErrorLevel)
+
+	Convey("While using Memcached launcher", t, func() {
+		memcachedLauncher := NewMemcached(
+			&mocks.Executor{},
+			DefaultMemcachedConfig("test"))
+
+		Convey("Build command should create proper command", func() {
+			command := memcachedLauncher.buildCommand()
+
+			So(command, ShouldEqual,
+				"test -p 11211 -u memcached -t 4 -m 64 -c 1024")
+		})
+	})
+}
+
+// Before test, make sure you have built memcached in /usr/bin e.g:
+// ln -s SWAN_REPO/workloads/data_caching/memcached/memcached-1.4.25/memcached /usr/bin/memcached
+const (
+	memcachedPath = "/usr/bin/memcached"
+)
+
+// TestMemcachedWithExecutor is an integration test with local executor.
+func TestMemcachedWithExecutor(t *testing.T) {
+	log.SetLevel(log.ErrorLevel)
+
+	SkipConvey("While using Local Shell in Memcached launcher", t, func() {
+		memcachedLauncher := NewMemcached(
+			executor.NewLocal(),
+			DefaultMemcachedConfig(memcachedPath))
+
+		Convey("When memcached is launched", func() {
+			task, err := memcachedLauncher.Launch()
+
+			Convey("There should be no error", func() {
+				So(err, ShouldBeNil)
+
+				task.Stop()
+			})
+
+			Convey("When we stop the task", func() {
+				err := task.Stop()
+
+				Convey("There should be no error", func() {
+					So(err, ShouldBeNil)
+				})
+
+				Convey("The task should be terminated and the task status should be -1", func() {
+					taskState, taskStatus := task.Status()
+					So(taskState, ShouldEqual, executor.TERMINATED)
+					So(taskStatus.ExitCode, ShouldEqual, -1)
+				})
+			})
+
+		})
+	})
 }
