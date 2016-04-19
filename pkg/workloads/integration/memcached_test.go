9919742ad00f7d94e4f140895eeef959cb586819
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-04-19 13:58:49 +0200

    Added netstat test for memcached.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- pkg/workloads/integration/memcached_test.go
+++ pkg/workloads/integration/memcached_test.go
@@ -7,26 +7,34 @@ import (
 	"github.com/intelsdi-x/swan/pkg/executor"
 	"github.com/intelsdi-x/swan/pkg/workloads"
 	. "github.com/smartystreets/goconvey/convey"
+	"os"
 	"testing"
 )
 
+const (
+	netstatCommand        = "echo stats | nc -q 5 127.0.0.1 11211"
+	memcachedRelativePath = "workloads/data_caching/memcached/memcached-1.4.25/memcached"
+)
+
 // Before test, make sure you have built memcached in /usr/bin e.g:
-// - ln -s SWAN_REPO/workloads/data_caching/memcached/memcached-1.4.25/memcached /usr/bin/memcached
+// - have $SWAN_ROOT environment variable for Swan repo location.
 // - user memcached is present.
 // - apt-get netstat or yum ...
-
-const (
-	memcachedPath = "/usr/bin/memcached"
-)
 
 // TestMemcachedWithExecutor is an integration test with local executor.
 func TestMemcachedWithExecutor(t *testing.T) {
 	log.SetLevel(log.ErrorLevel)
 
+	memcachedPath := os.Getenv("SWAN_ROOT")
+
+	if memcachedPath == "" {
+		t.Fatal("There is no $SWAN_ROOT environment variable for Swan repo location.")
+	}
+
 	Convey("While using Local Shell in Memcached launcher", t, func() {
+		l := executor.NewLocal()
 		memcachedLauncher := workloads.NewMemcached(
-			executor.NewLocal(),
-			workloads.DefaultMemcachedConfig(memcachedPath))
+			l, workloads.DefaultMemcachedConfig(memcachedPath+"/"+memcachedRelativePath))
 
 		Convey("When memcached is launched", func() {
 			task, err := memcachedLauncher.Launch()
@@ -37,17 +45,44 @@ func TestMemcachedWithExecutor(t *testing.T) {
 				task.Stop()
 			})
 
-			Convey("When we stop the task", func() {
-				err := task.Stop()
+			Convey("When we check the memcached endpoint for stats", func() {
+				netstatTask, netstatErr := l.Execute(netstatCommand)
 
 				Convey("There should be no error", func() {
-					So(err, ShouldBeNil)
+					So(netstatErr, ShouldBeNil)
+
+					task.Stop()
+					netstatTask.Stop()
 				})
 
-				Convey("The task should be terminated and the task status should be -1", func() {
-					taskState, taskStatus := task.Status()
-					So(taskState, ShouldEqual, executor.TERMINATED)
-					So(taskStatus.ExitCode, ShouldEqual, -1)
+				Convey("When we wait for netstat ", func() {
+					netstatTask.Wait(0)
+
+					Convey("The netstat task should be terminated, the task status should be 0"+
+						" and output resultes with a STATA information", func() {
+						netstatTaskState, netstatTaskStatus := netstatTask.Status()
+
+						So(netstatTaskState, ShouldEqual, executor.TERMINATED)
+						So(netstatTaskStatus.ExitCode, ShouldEqual, 0)
+						So(netstatTaskStatus.Stdout, ShouldStartWith, "STAT")
+					})
+
+					task.Stop()
+					netstatTask.Stop()
+				})
+
+				Convey("When we stop the memcached task", func() {
+					err := task.Stop()
+
+					Convey("There should be no error", func() {
+						So(err, ShouldBeNil)
+					})
+
+					Convey("The task should be terminated and the task status should be -1", func() {
+						taskState, taskStatus := task.Status()
+						So(taskState, ShouldEqual, executor.TERMINATED)
+						So(taskStatus.ExitCode, ShouldEqual, -1)
+					})
 				})
 			})
 
