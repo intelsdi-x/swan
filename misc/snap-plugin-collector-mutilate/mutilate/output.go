cdb94115d3c7a03e35ddb8bc03f9a891b231524b
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-04-26 17:12:34 +0200

    SCE-250 - parsing mutilate output file

---
+++ misc/snap-plugin-collector-mutilate/mutilate/output.go
@@ -0,0 +1,87 @@
+package mutilate
+
+import (
+	"bytes"
+	"encoding/csv"
+	"golang.org/x/exp/inotify"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	SPACE    rune = 0x0020
+	NEW_LINE byte = 10
+	NULL     int  = 0x00
+)
+
+type MutilateRow struct {
+	time    time.Time
+	latency float64
+}
+
+func parse_mutilate_output(event inotify.Event, baseTime time.Time) ([]MutilateRow, error) {
+	csvFile, readError := os.Open(event.Name)
+	defer csvFile.Close()
+	if readError != nil {
+		var output []MutilateRow
+		return output, readError
+	}
+	csvReader := csv.NewReader(csvFile)
+	csvReader.Comma = SPACE
+	startTime := get_first_row_time(csvFile, baseTime)
+	output := get_structs_from_file(csvReader, startTime)
+
+	return output, nil
+}
+
+func get_first_row_time(file *os.File, baseTime time.Time) time.Time {
+	file.Seek(0, os.SEEK_END)
+	defer file.Seek(0, os.SEEK_SET)
+
+	lastLineBytes := find_last_row(file)
+	reader := csv.NewReader(bytes.NewReader(lastLineBytes))
+	reader.Comma = SPACE
+	row, _ := reader.Read()
+	row[0] = strings.Trim(row[0], string(NULL))
+	seconds, _ := strconv.ParseFloat(row[0], 64)
+	lastRowUnix := baseTime.Unix() - int64(seconds)
+
+	return time.Unix(int64(lastRowUnix), 0)
+}
+
+func get_structs_from_file(csvReader *csv.Reader, startTime time.Time) []MutilateRow {
+	var output []MutilateRow
+	for true {
+		row, error := csvReader.Read()
+		if error == io.EOF {
+			break
+		}
+		floatOffset, _ := strconv.ParseFloat(row[0], 64)
+		offset := time.Duration(time.Duration(floatOffset) * time.Second)
+		eventTime := startTime.Add(offset)
+		latency, _ := strconv.ParseFloat(row[1], 64)
+		output = append(output, MutilateRow{eventTime, latency})
+	}
+
+	return output
+}
+
+func find_last_row(file *os.File) []byte {
+	var i int64
+	var line []byte
+	singleByte := make([]byte, 1)
+	for true {
+		file.Seek(i, os.SEEK_END)
+		file.Read(singleByte)
+		if singleByte[0] == NEW_LINE {
+			break
+		}
+		line = append(singleByte, line...)
+		i--
+	}
+
+	return line
+}
---
+++ misc/snap-plugin-collector-mutilate/mutilate/output_integration_test.go
@@ -0,0 +1,90 @@
+package mutilate
+
+import (
+	"bytes"
+	"fmt"
+	. "github.com/smartystreets/goconvey/convey"
+	"golang.org/x/exp/inotify"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestOutputParser(t *testing.T) {
+	Convey("Opening non-existing file should fail", t, func() {
+		event := inotify.Event{Name: "/non/existing/file"}
+		baseTime := time.Now()
+
+		data, error := parse_mutilate_output(event, baseTime)
+
+		So(data, ShouldBeZeroValue)
+		So(error.Error(), ShouldEqual, "open /non/existing/file: no such file or directory")
+
+	})
+
+	Convey("Opening non-readable file should fail", t, func() {
+		event := inotify.Event{Name: "/etc/shadow"}
+		baseTime := time.Now()
+
+		data, error := parse_mutilate_output(event, baseTime)
+
+		So(data, ShouldBeZeroValue)
+		So(error.Error(), ShouldEqual, "open /etc/shadow: permission denied")
+	})
+
+	Convey("Opening readable and correct file should provide meaningful results", t, func() {
+		event := inotify.Event{Name: get_current_dir_file("/mutilate.out")}
+		cest, _ := time.LoadLocation("Europe/Warsaw")
+		baseTime := time.Date(2010, time.April, 10, 8, 41, 0, 0, cest)
+		zeroTime := time.Date(2010, time.April, 10, 8, 10, 26, 0, cest)
+
+		data, error := parse_mutilate_output(event, baseTime)
+
+		So(error, ShouldBeNil)
+		So(data, ShouldHaveLength, 10)
+		So(data[0].latency, ShouldEqual, 70.095062)
+		So(data[0].time.Unix(), ShouldEqual, zeroTime.Unix())
+		So(data[1].latency, ShouldEqual, 28.848648)
+		So(data[1].time.Unix(), ShouldEqual, zeroTime.Unix())
+		So(data[2].latency, ShouldEqual, 14.781952)
+		So(data[2].time.Unix(), ShouldEqual, zeroTime.Unix())
+		So(data[3].latency, ShouldEqual, 14.066696)
+		So(data[3].time.Unix(), ShouldEqual, zeroTime.Unix())
+		So(data[4].latency, ShouldEqual, 14.066696)
+		So(data[4].time.Unix(), ShouldEqual, zeroTime.Unix())
+		So(data[5].latency, ShouldEqual, 14.066696)
+		So(data[5].time.Unix(), ShouldEqual, zeroTime.Unix())
+		So(data[6].latency, ShouldEqual, 12.874603)
+		So(data[6].time.Unix(), ShouldEqual, zeroTime.Unix())
+		So(data[7].latency, ShouldEqual, 12.874603)
+		So(data[7].time.Unix(), ShouldEqual, zeroTime.Unix())
+		So(data[8].latency, ShouldEqual, 14.066696)
+		So(data[8].time.Unix(), ShouldEqual, zeroTime.Unix())
+		So(data[9].latency, ShouldEqual, 13.828278)
+		So(data[9].time.Unix(), ShouldEqual, baseTime.Unix())
+	})
+}
+
+func TestGettingFirstRowTime(t *testing.T) {
+	Convey("We should get correct timestamp for first row", t, func() {
+		file, _ := os.Open(get_current_dir_file("/mutilate.out"))
+		defer file.Close()
+		cest, _ := time.LoadLocation("Europe/Warsaw")
+		baseTime := time.Date(2010, time.April, 10, 8, 41, 0, 0, cest)
+		expectedFirstRowTime := time.Date(2010, time.April, 10, 8, 10, 26, 0, cest)
+
+		realFirstRowTime := get_first_row_time(file, baseTime)
+		fmt.Printf("\n%v\n%v", expectedFirstRowTime, realFirstRowTime)
+
+		So(realFirstRowTime.Equal(expectedFirstRowTime), ShouldBeTrue)
+	})
+}
+
+func get_current_dir_file(name string) string {
+	var path bytes.Buffer
+	gwd, _ := os.Getwd()
+	path.WriteString(gwd)
+	path.WriteString(name)
+
+	return path.String()
+}
