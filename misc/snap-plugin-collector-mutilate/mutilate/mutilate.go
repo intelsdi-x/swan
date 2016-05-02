fe20af79855d43768d192c9334555f84dc7ccdc7
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-05-02 15:28:53 +0200

    SCE-250 - docker support for snap (master), mutilate collector can be run now (some tests must have been deleted though)

--- misc/snap-plugin-collector-mutilate/mutilate/mutilate.go
+++ misc/snap-plugin-collector-mutilate/mutilate/mutilate.go
@@ -1,6 +1,7 @@
 package mutilate
 
 import (
+	"fmt"
 	"os"
 	"time"
 
@@ -30,17 +31,15 @@ func NewMutilate(now time.Time) *mutilate {
 // GetMetricTypes implements plugin.PluginCollector interface
 func (mutilate *mutilate) GetMetricTypes(configType plugin.ConfigType) ([]plugin.MetricType, error) {
 	var metrics []plugin.MetricType
-	phaseName, _ := config.GetConfigItem(configType, "phase_name")
-	tags := map[string]string{"phase_name": phaseName.(string)}
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("avg"), Tags_: tags, Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("std"), Tags_: tags, Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("min"), Tags_: tags, Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "5th"), Tags_: tags, Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "10th"), Tags_: tags, Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "90th"), Tags_: tags, Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "95th"), Tags_: tags, Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "99th"), Tags_: tags, Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "99.999th"), Tags_: tags, Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("avg"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("std"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("min"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "5th"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "10th"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "90th"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "95th"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "99th"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "99_999th"), Unit_: UNIT, Version_: VERSION})
 
 	return metrics, nil
 }
@@ -59,13 +58,21 @@ func createNewMetricNamespace(metricName ...string) core.Namespace {
 func (mutilate *mutilate) CollectMetrics(metricTypes []plugin.MetricType) ([]plugin.MetricType, error) {
 	var metrics []plugin.MetricType
 	// TODO - pass file path in configuration
-	//configuration, _ := config.GetConfigItem(metricTypes[0], "stdout_file")
-	rawMetrics, _ := parseMutilateStdout("/home/iwan/go_workspace/src/github.com/intelsdi-x/swan/misc/snap-plugin-collector-mutilate/mutilate/mutilate.stdout")
+	//fmt.Printf("\nmetricTypes: %v\n", metricTypes[0])
+	sourceFilePath, _ := config.GetConfigItem(metricTypes[0], "stdout_file")
+	phaseName, err0 := config.GetConfigItem(metricTypes[0], "phase_name")
+	experimentName, err1 := config.GetConfigItem(metricTypes[0], "experiment_name")
+	fmt.Printf("%v, %v", err0, err1)
+	//fmt.Printf("\nsourceFilePath: %v, %v\n", sourceFilePath, confErr)
+	rawMetrics, err := parseMutilateStdout(sourceFilePath.(string))
+	fmt.Printf("\nrawMetrics: %v\nerror: %v\n", rawMetrics, err)
 	hostname, _ := os.Hostname()
 	for key, metricType := range metricTypes {
 		metricType.Data_ = rawMetrics[key].value
 		metricType.Namespace_[3].Value = hostname
 		metricType.Timestamp_ = mutilate.now
+		metricType.Tags_["phase"] = phaseName.(string)
+		metricType.Tags_["experiment"] = experimentName.(string)
 		metrics = append(metrics, metricType)
 	}
 
@@ -76,11 +83,13 @@ func (mutilate *mutilate) CollectMetrics(metricTypes []plugin.MetricType) ([]plu
 func (mutilate *mutilate) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
 	phaseName, _ := cpolicy.NewStringRule("phase_name", true)
 	stdoutFile, _ := cpolicy.NewStringRule("stdout_file", true)
+	experimentName, _ := cpolicy.NewStringRule("experiment_name", true)
 	experiment := cpolicy.NewPolicyNode()
 	experiment.Add(phaseName)
 	experiment.Add(stdoutFile)
+	experiment.Add(experimentName)
 	policy := cpolicy.New()
-	policy.Add([]string{"experiment"}, experiment)
+	policy.Add([]string{""}, experiment)
 
 	return policy, nil
 }
