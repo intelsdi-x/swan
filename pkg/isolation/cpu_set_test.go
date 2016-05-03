bfb9507a5b99603afa97b5599011485ec39c7e68
MrigankAtGitHub <mrigank.shekhar@intel.com>
2016-05-03 11:54:53 -0700

    Included additional test using Convey

--- pkg/isolation/cpu_set_test.go
+++ pkg/isolation/cpu_set_test.go
@@ -2,7 +2,10 @@ package isolation
 
 import (
 	"fmt"
+	. "github.com/smartystreets/goconvey/convey"
+	"io/ioutil"
 	"os/exec"
+	"strconv"
 	"testing"
 )
 
@@ -15,10 +18,34 @@ func TestCpuSet(t *testing.T) {
 		panic(err)
 	}
 
-	cpuset.Create()
-
-	cpuset.Isolate(cmd.Process.Pid)
-
 	fmt.Printf(cpuset.cgroupName)
+
+	Convey("Should provide cpuset Create() to return and correct cpu set shares", t, func() {
+		So(cpuset.Create(), ShouldBeNil)
+		data, err := ioutil.ReadFile("/sys/fs/cgroup/cpuset/" + cpuset.cgroupName + "/cpuset.cpus")
+		if err != nil {
+			panic(err)
+		}
+		inputFmt := data[:len(data)-1]
+		So(string(inputFmt), ShouldEqual, cpuset.cpuSetShares)
+	})
+
+	Convey("Should provide cpuset Isolate() to return and correct process id", t, func() {
+		So(cpuset.Isolate(cmd.Process.Pid), ShouldBeNil)
+		data, err := ioutil.ReadFile("/sys/fs/cgroup/cpuset/" + cpuset.cgroupName + "/tasks")
+		if err != nil {
+			panic(err)
+		}
+		inputFmt := data[:len(data)-1]
+		strPID := strconv.Itoa(cmd.Process.Pid)
+		d := []byte(strPID)
+
+		So(string(inputFmt), ShouldContainSubstring, string(d))
+
+	})
+
+	Convey("Should provide Clean() to return", t, func() {
+		So(cpuset.Clean(), ShouldBeNil)
+	})
 
 }
--- pkg/isolation/cpu_test.go
+++ pkg/isolation/cpu_test.go
@@ -2,7 +2,10 @@ package isolation
 
 import (
 	"fmt"
+	. "github.com/smartystreets/goconvey/convey"
+	"io/ioutil"
 	"os/exec"
+	"strconv"
 	"testing"
 )
 
@@ -14,10 +17,35 @@ func TestCpu(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	cpu.Create()
-
-	cpu.Isolate(cmd.Process.Pid)
 
 	fmt.Printf(cpu.cgroupName)
+
+	Convey("Should provide Create() to return and correct cpu shares", t, func() {
+		So(cpu.Create(), ShouldBeNil)
+		data, err := ioutil.ReadFile("/sys/fs/cgroup/cpu/" + cpu.cgroupName + "/cpu.shares")
+		if err != nil {
+			panic(err)
+		}
+		inputFmt := data[:len(data)-1]
+		So(string(inputFmt), ShouldEqual, cpu.cpuShares)
+	})
+
+	Convey("Should provide Isolate() to return and correct process id", t, func() {
+		So(cpu.Isolate(cmd.Process.Pid), ShouldBeNil)
+		data, err := ioutil.ReadFile("/sys/fs/cgroup/cpu/" + cpu.cgroupName + "/tasks")
+		if err != nil {
+			panic(err)
+		}
+		inputFmt := data[:len(data)-1]
+		strPID := strconv.Itoa(cmd.Process.Pid)
+		d := []byte(strPID)
+
+		So(string(inputFmt), ShouldEqual, string(d))
+
+	})
+
+	Convey("Should provide Clean() to return", t, func() {
+		So(cpu.Clean(), ShouldBeNil)
+	})
 
 }
