734f5962b50cb900369e58d779fbe5c00f172585
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-05-04 15:51:44 +0200

    SCE-250 - fixing tests and static analysis

--- misc/snap-plugin-collector-mutilate/mutilate/mutilate.go
+++ misc/snap-plugin-collector-mutilate/mutilate/mutilate.go
@@ -21,34 +21,47 @@ const (
 	UNIT    = "ns"
 )
 
-type mutilate struct {
+// This is evil, I know. As of now we can't define Snap task that will be run only once
+var alreadyRun bool
+
+type mutilatePlugin struct {
 	now time.Time
 }
 
 // NewMutilate creates new mutilate collector
-func NewMutilate(now time.Time) *mutilate {
-	return &mutilate{now}
+func NewMutilate(now time.Time) plugin.CollectorPlugin {
+	return &mutilatePlugin{now}
 }
 
 // GetMetricTypes implements plugin.PluginCollector interface
-func (mutilate *mutilate) GetMetricTypes(configType plugin.ConfigType) ([]plugin.MetricType, error) {
+func (mutilate *mutilatePlugin) GetMetricTypes(configType plugin.ConfigType) ([]plugin.MetricType, error) {
 	var metrics []plugin.MetricType
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("avg"), Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("std"), Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("min"), Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "5th"), Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "10th"), Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "90th"), Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "95th"), Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "99th"), Unit_: UNIT, Version_: VERSION})
-	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("percentile", "99_999th"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("avg"),
+		Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("std"),
+		Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace("min"),
+		Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace(
+		"percentile", "5th"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace(
+		"percentile", "10th"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace(
+		"percentile", "90th"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace(
+		"percentile", "95th"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace(
+		"percentile", "99th"), Unit_: UNIT, Version_: VERSION})
+	metrics = append(metrics, plugin.MetricType{Namespace_: createNewMetricNamespace(
+		"percentile", "99_999th"), Unit_: UNIT, Version_: VERSION})
 
 	return metrics, nil
 }
 
 func createNewMetricNamespace(metricName ...string) core.Namespace {
 	namespace := core.NewNamespace("intel", "swan", "mutilate")
-	namespace = namespace.AddDynamicElement("hostname", "Name of the host that reports the metric")
+	namespace = namespace.AddDynamicElement("hostname",
+		"Name of the host that reports the metric")
 	for _, value := range metricName {
 		namespace = namespace.AddStaticElement(value)
 	}
@@ -57,8 +70,11 @@ func createNewMetricNamespace(metricName ...string) core.Namespace {
 }
 
 // CollectMetrics implements plugin.PluginCollector interface
-func (mutilate *mutilate) CollectMetrics(metricTypes []plugin.MetricType) ([]plugin.MetricType, error) {
+func (mutilate *mutilatePlugin) CollectMetrics(metricTypes []plugin.MetricType) ([]plugin.MetricType, error) {
 	var metrics []plugin.MetricType
+	if alreadyRun {
+		return metrics, errors.New("Subsequent calls to CollectMetrics() are ignored")
+	}
 	sourceFilePath, sFPErr := config.GetConfigItem(metricTypes[0], "stdout_file")
 	if sFPErr != nil {
 		logger.LogError("No file path set - no metrics are collected", sFPErr)
@@ -86,19 +102,22 @@ func (mutilate *mutilate) CollectMetrics(metricTypes []plugin.MetricType) ([]plu
 	}
 
 	for key, metricType := range metricTypes {
-		metric := plugin.MetricType{Namespace_: metricType.Namespace_, Unit_: metricType.Unit_, Version_: metricType.Version_}
+		metric := plugin.MetricType{Namespace_: metricType.Namespace_,
+			Unit_: metricType.Unit_, Version_: metricType.Version_}
 		metric.Data_ = rawMetrics[key].value
 		metric.Namespace_[3].Value = hostname
 		metric.Timestamp_ = mutilate.now
-		metric.Tags_ = map[string]string{"phase": phaseName.(string), "experiment": experimentName.(string)}
+		metric.Tags_ = map[string]string{"phase": phaseName.(string),
+			"experiment": experimentName.(string)}
 		metrics = append(metrics, metric)
 	}
+	alreadyRun = true
 
 	return metrics, nil
 }
 
 // GetConfigPolicy implements plugin.PluginCollector interface
-func (mutilate *mutilate) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
+func (mutilate *mutilatePlugin) GetConfigPolicy() (*cpolicy.ConfigPolicy, error) {
 	phaseName, _ := cpolicy.NewStringRule("phase_name", true)
 	stdoutFile, _ := cpolicy.NewStringRule("stdout_file", true)
 	experimentName, _ := cpolicy.NewStringRule("experiment_name", true)
