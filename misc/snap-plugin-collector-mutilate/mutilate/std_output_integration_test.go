30af775234def5fb056da9a84833ada9cce4391a
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-05-05 17:58:42 +0200

    SCE-250 - renaming variables, making closure a named function

--- misc/snap-plugin-collector-mutilate/mutilate/std_output_integration_test.go
+++ misc/snap-plugin-collector-mutilate/mutilate/std_output_integration_test.go
@@ -9,29 +9,25 @@ import (
 
 func TestStdoutParser(t *testing.T) {
 	Convey("Opening non-existing file should fail", t, func() {
-		data, error := parseMutilateStdout("/non/existing/file")
+		data, err := parseMutilateStdout("/non/existing/file")
 
 		So(data, ShouldBeZeroValue)
-		So(error.Error(), ShouldEqual,
+		So(err.Error(), ShouldEqual,
 			"open /non/existing/file: no such file or directory")
 
 	})
-
 	Convey("Opening non-readable file should fail", t, func() {
-		data, error := parseMutilateStdout("/etc/shadow")
+		data, err := parseMutilateStdout("/etc/shadow")
 
 		So(data, ShouldBeZeroValue)
-		So(error.Error(), ShouldEqual, "open /etc/shadow: permission denied")
+		So(err.Error(), ShouldEqual, "open /etc/shadow: permission denied")
 	})
 
 	Convey("Opening readable and correct file should provide meaningful results", t, func() {
-		data, error := parseMutilateStdout(GetCurrentDirFilePath("/mutilate.stdout"))
+		data, err := parseMutilateStdout(GetCurrentDirFilePath("/mutilate.stdout"))
 
-		So(error, ShouldBeNil)
+		So(err, ShouldBeNil)
 		So(data, ShouldHaveLength, 9)
-		soMetric := func(mutilateMetric metric, name string, value float64) {
-			So(mutilateMetric, ShouldResemble, metric{name, value})
-		}
 		soMetric(data[0], "avg", 20.8)
 		soMetric(data[1], "std", 23.1)
 		soMetric(data[2], "min", 11.9)
@@ -45,60 +41,60 @@ func TestStdoutParser(t *testing.T) {
 
 	Convey("Attempting to read file with wrong number of read columns should return"+
 		" an error and no metrics", t, func() {
-		data, error := parseMutilateStdout(GetCurrentDirFilePath(
+		data, err := parseMutilateStdout(GetCurrentDirFilePath(
 			"/mutilate_incorrect_count_of_columns.stdout"))
 
 		So(data, ShouldHaveLength, 0)
-		So(error.Error(), ShouldEqual,
+		So(err.Error(), ShouldEqual,
 			"Incorrect column count (got: 3, expected: 9) in QPS read row")
 	})
 
 	Convey("Attempting to read a file with no read row at all should return"+
 		" an error and no metrics", t, func() {
-		data, error := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_missing_read_row.stdout"))
+		data, err := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_missing_read_row.stdout"))
 
 		So(data, ShouldHaveLength, 0)
-		So(error.Error(), ShouldEqual, "No default mutilate statistics found")
+		So(err.Error(), ShouldEqual, "No default mutilate statistics found")
 	})
 
 	Convey("Attempting to read a file with no swan-specific row at all"+
 		" should return an error and no metrics", t, func() {
-		data, error := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_missing_swan_row.stdout"))
+		data, err := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_missing_swan_row.stdout"))
 
 		So(data, ShouldHaveLength, 0)
-		So(error.Error(), ShouldEqual, "No swan-specific statistics found")
+		So(err.Error(), ShouldEqual, "No swan-specific statistics found")
 	})
 
 	Convey("Attempting to read a file with malformed swan-specific row should return"+
 		" an error and no metrics", t, func() {
-		data, error := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_malformed_swan_row.stdout"))
+		data, err := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_malformed_swan_row.stdout"))
 
 		So(data, ShouldHaveLength, 0)
-		So(error.Error(), ShouldEqual, "Swan-specific row malformed")
+		So(err.Error(), ShouldEqual, "Swan-specific row malformed")
 	})
 
 	Convey("Attempting to read a file with swan-specific row missing metric"+
 		" value should return an error and no metrics", t, func() {
-		data, error := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_missing_metric_in_swan_row.stdout"))
+		data, err := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_missing_metric_in_swan_row.stdout"))
 
 		So(data, ShouldHaveLength, 0)
-		So(error.Error(), ShouldEqual, "Swan-specific row is missing metric value")
+		So(err.Error(), ShouldEqual, "Swan-specific row is missing metric value")
 	})
 
 	Convey("Attempting to read a file with swan-specific row missing percentile value"+
 		" should return an error and no metrics", t, func() {
-		data, error := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_swan_row_missing_percentile_in_description.stdout"))
+		data, err := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_swan_row_missing_percentile_in_description.stdout"))
 
 		So(data, ShouldHaveLength, 0)
-		So(error.Error(), ShouldEqual, "Swan-specific row is missing percentile value")
+		So(err.Error(), ShouldEqual, "Swan-specific row is missing percentile value")
 	})
 
 	Convey("Attempting to read a file with read row containing incorrect values should return"+
 		" en error and no results", t, func() {
-		data, error := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_non_numeric_default_metric_value.stdout"))
+		data, err := parseMutilateStdout(GetCurrentDirFilePath("/mutilate_non_numeric_default_metric_value.stdout"))
 
 		So(data, ShouldHaveLength, 0)
-		So(error.Error(), ShouldEqual,
+		So(err.Error(), ShouldEqual,
 			"Non-numeric value in read row (value: \"thisIsNotANumber\", column: 5)")
 	})
 }
@@ -110,4 +106,8 @@ func GetCurrentDirFilePath(name string) string {
 	path.WriteString(name)
 
 	return path.String()
+}
+
+func soMetric(mutilateMetric metric, name string, value float64) {
+	So(mutilateMetric, ShouldResemble, metric{name, value})
 }
