b0907373e8b448ee65840f6c77074b7b74b8805c
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-05-13 16:03:33 +0200

    SCE-259 - improving the script

---
+++ scripts/isolate-pid.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# See: https://intelsdi.atlassian.net/browse/SCE-259
+# This script allows us to isolate PID namespace while running experiments
+# Usage: isolate-pid.sh path/to/experiment/binary
+# The script uses sudo so you should not be suprised
+
+function display_error {
+	echo -e "\e[41m$1\e[0m"
+	exit 255
+}
+
+if [ "$#" -lt "1" ]; then
+	display_error "Missing path to experiment binary. Example usage: \e[7m$0 path/to/experiment/binary\e[27ms"
+        exit 251
+fi
+
+BINARY=$1
+type -p $1 2>/dev/null 1>/dev/null
+if [ "$?" -ne "0" ]; then
+	display_error "$1 not found in PATH"
+	exit 250
+fi
+BINARY=`type -p $1`
+
+if [ ! -f "$BINARY" ]; then
+	display_error "$BINARY does not exist or is not a file; it needs to be path to an existing file"
+	exit 252
+fi
+
+if [ ! -x "$BINARY" ]; then
+	display_error "$BINARY is not executable"
+	exit 253
+fi
+
+read -ra ARGS <<< "$@"
+ARGS[0]=$BINARY
+
+echo -e "\e[32mRunning \e[7msudo -E unshare --pid --fork --mount-proc ${ARGS[*]}\e[27m\e[0m"
+sudo -E unshare --pid --fork --mount-proc ${ARGS[*]} 
+exit $?
