f8af4cb4c05b2c3a5addb46cc1099c8e479cfd18
MrigankAtGitHub <mrigank.shekhar@intel.com>
2016-05-18 04:57:35 -0700

    Created CPU topology and discovered cache sizes

---
+++ pkg/isolation/discover.go
@@ -0,0 +1,116 @@
+package isolation
+
+import "os/exec"
+import "strconv"
+import "strings"
+import "errors"
+
+// CPUInfo defines data needed for CPU topology
+type CPUInfo struct {
+	sockets        int
+	physicalCores  int
+	threadsPerCore int
+	cacheL1i       int
+	cacheL1d       int
+	cacheL2        int
+	cacheL3        int
+}
+
+// const for human readable byte sizes
+const (
+	BYTE     = 1.0
+	KILOBYTE = 1024 * BYTE
+	MEGABYTE = 1024 * KILOBYTE
+	GIGABYTE = 1024 * MEGABYTE
+	TERABYTE = 1024 * GIGABYTE
+)
+
+// NewCPUInfo instance creation.
+func NewCPUInfo(cores int, threads int, l1i int, l1d int, l2 int, l3 int) *CPUInfo {
+	return &CPUInfo{physicalCores: cores, threadsPerCore: threads, cacheL1i: l1i, cacheL1d: l1d, cacheL2: l2, cacheL3: l3}
+}
+
+// UnitsOfBytes returns KILOBYTES, MEGHABYTES, etc as detected in the input string
+func UnitsOfBytes(s string) (int, error) {
+	if strings.Contains(s, "K") {
+		return KILOBYTE, nil
+	} else if strings.Contains(s, "M") {
+		return MEGABYTE, nil
+	} else if strings.Contains(s, "G") {
+		return GIGABYTE, nil
+	} else if strings.Contains(s, "T") {
+		return TERABYTE, nil
+	} else {
+		err := errors.New("Unexpected input error")
+		return BYTE, err
+	}
+}
+
+// DiscoverCPU removes the specified cgroup
+func (cputopo *CPUInfo) Discover() error {
+
+	out, err := exec.Command("lscpu").Output()
+	outstring := strings.TrimSpace(string(out))
+	lines := strings.Split(outstring, "\n")
+
+	for _, line := range lines {
+		fields := strings.Split(line, ":")
+		if len(fields) < 2 {
+			continue
+		}
+		key := strings.TrimSpace(fields[0])
+		value := strings.TrimSpace(fields[1])
+
+		switch key {
+		case "Socket(s)":
+			t, _ := strconv.Atoi(value)
+			cputopo.sockets = int(t)
+		case "Core(s) per socket":
+			t, _ := strconv.Atoi(value)
+			cputopo.physicalCores = int(t)
+		case "Thread(s) per core":
+			t, _ := strconv.Atoi(value)
+			cputopo.threadsPerCore = int(t)
+		case "L1d cache":
+			tFmt := value[:len(value)-1]
+			t, _ := strconv.Atoi(tFmt)
+
+			multiplier, err := UnitsOfBytes(value)
+			if err == nil {
+				cputopo.cacheL1d = int(t) * multiplier
+			}
+		case "L1i cache":
+			tFmt := value[:len(value)-1]
+			t, _ := strconv.Atoi(tFmt)
+
+			multiplier, err := UnitsOfBytes(value)
+			if err == nil {
+				cputopo.cacheL1i = int(t) * multiplier
+			}
+		case "L2 cache":
+			tFmt := value[:len(value)-1]
+			t, _ := strconv.Atoi(tFmt)
+
+			multiplier, err := UnitsOfBytes(value)
+			if err == nil {
+				cputopo.cacheL2 = int(t) * multiplier
+			}
+		case "L3 cache":
+			tFmt := value[:len(value)-1]
+			t, _ := strconv.Atoi(tFmt)
+
+			multiplier, err := UnitsOfBytes(value)
+			if err == nil {
+				cputopo.cacheL3 = int(t) * multiplier
+			}
+
+		}
+
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/isolation/discover_test.go
@@ -0,0 +1,49 @@
+// +build integration
+
+package isolation
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"os/exec"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestDiscover(t *testing.T) {
+	cpuTopo := CPUInfo{physicalCores: 4, threadsPerCore: 2, cacheL1i: 8192, cacheL1d: 8192, cacheL2: 262144, cacheL3: 8388608}
+
+	Convey("Should provide Discover() to return and correct cpu topology", t, func() {
+		So(cpuTopo.Discover(), ShouldBeNil)
+
+		out, err := exec.Command("lscpu").Output()
+		outstring := strings.TrimSpace(string(out))
+		lines := strings.Split(outstring, "\n")
+
+		numaNodes := 0
+		numCPUs := 0
+		for _, line := range lines {
+			fields := strings.Split(line, ":")
+			if len(fields) < 2 {
+				continue
+			}
+			key := strings.TrimSpace(fields[0])
+			value := strings.TrimSpace(fields[1])
+
+			switch key {
+			case "NUMA node(s)":
+				t, _ := strconv.Atoi(value)
+				numaNodes = int(t)
+
+			case "CPU(s)":
+				t, _ := strconv.Atoi(value)
+				numCPUs = int(t)
+			}
+
+		}
+		So(err, ShouldBeNil)
+		So(cpuTopo.sockets, ShouldEqual, numaNodes)
+		So(cpuTopo.physicalCores*cpuTopo.sockets*cpuTopo.threadsPerCore, ShouldEqual, numCPUs)
+	})
+
+}
