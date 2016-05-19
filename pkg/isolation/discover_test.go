2896f833d4f0cbcd835e058270f9afedfd4caa61
MrigankAtGitHub <mrigank.shekhar@intel.com>
2016-05-18 20:36:53 -0700

    Incorporated more code review suggestions PR-114

--- pkg/isolation/discover_test.go
+++ pkg/isolation/discover_test.go
@@ -1,5 +1,3 @@
-// +build integration
-
 package isolation
 
 import (
@@ -16,34 +14,39 @@ func TestDiscover(t *testing.T) {
 	Convey("Should provide Discover() to return and correct cpu topology", t, func() {
 		So(cpuTopo.Discover(), ShouldBeNil)
 
-		out, err := exec.Command("lscpu").Output()
-		outstring := strings.TrimSpace(string(out))
-		lines := strings.Split(outstring, "\n")
+		Convey("Should provide lscpu output without error", func() {
+			out, err := exec.Command("lscpu").Output()
+			So(err, ShouldBeNil)
+			So(out, ShouldNotBeNil)
 
-		numaNodes := 0
-		numCPUs := 0
-		for _, line := range lines {
-			fields := strings.Split(line, ":")
-			if len(fields) < 2 {
-				continue
+			outstring := strings.TrimSpace(string(out))
+			lines := strings.Split(outstring, "\n")
+
+			numaNodes := 0
+			numCPUs := 0
+			for _, line := range lines {
+				fields := strings.Split(line, ":")
+				if len(fields) < 2 {
+					continue
+				}
+				key := strings.TrimSpace(fields[0])
+				value := strings.TrimSpace(fields[1])
+
+				switch key {
+				case "NUMA node(s)":
+					t, _ := strconv.Atoi(value)
+					numaNodes = int(t)
+
+				case "CPU(s)":
+					t, _ := strconv.Atoi(value)
+					numCPUs = int(t)
+				}
+
 			}
-			key := strings.TrimSpace(fields[0])
-			value := strings.TrimSpace(fields[1])
-
-			switch key {
-			case "NUMA node(s)":
-				t, _ := strconv.Atoi(value)
-				numaNodes = int(t)
-
-			case "CPU(s)":
-				t, _ := strconv.Atoi(value)
-				numCPUs = int(t)
-			}
-
-		}
-		So(err, ShouldBeNil)
-		So(cpuTopo.sockets, ShouldEqual, numaNodes)
-		So(cpuTopo.physicalCores*cpuTopo.sockets*cpuTopo.threadsPerCore, ShouldEqual, numCPUs)
+			So(err, ShouldBeNil)
+			So(cpuTopo.sockets, ShouldEqual, numaNodes)
+			So(cpuTopo.physicalCores*cpuTopo.sockets*cpuTopo.threadsPerCore, ShouldEqual, numCPUs)
+		})
 	})
 
 }
