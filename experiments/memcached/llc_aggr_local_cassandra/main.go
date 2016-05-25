3e431089361c1e5badcf078617d1eca02e75c6be
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-05-25 14:59:34 +0200

    Added Experiment with Cassandra publisher.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

---
+++ experiments/memcached/llc_aggr_local_cassandra/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/intelsdi-x/snap/mgmt/rest/client"
+	"github.com/intelsdi-x/snap/scheduler/wmap"
+	"github.com/intelsdi-x/swan/pkg/executor"
+	"github.com/intelsdi-x/swan/pkg/experiment/sensitivity"
+	"github.com/intelsdi-x/swan/pkg/snap"
+	"github.com/intelsdi-x/swan/pkg/snap/sessions"
+	"github.com/intelsdi-x/swan/pkg/swan"
+	"github.com/intelsdi-x/swan/pkg/workloads/low_level/l3data"
+	"github.com/intelsdi-x/swan/pkg/workloads/memcached"
+	"github.com/intelsdi-x/swan/pkg/workloads/mutilate"
+	"github.com/shopspring/decimal"
+	"io/ioutil"
+	"os"
+	"path"
+	"time"
+)
+
+// This Experiments contains:
+// - memcached as LC task on localhost
+// - mutilate as loadGenerator on localhost
+// - Snap Session: Mutilate -> Cassandra Publisher
+// - LLC aggressor
+func main() {
+	logLevel := logrus.DebugLevel
+	logrus.SetLevel(logLevel)
+
+	local := executor.NewLocal()
+
+	// Initialize Memcached Launcher.
+	memcachedLauncher := memcached.New(local, memcached.DefaultMemcachedConfig())
+
+	// Initialize Mutilate Launcher.
+	percentile, _ := decimal.NewFromString("99.9")
+	mutilateConfig := mutilate.Config{
+		MutilatePath:      mutilate.GetPathFromEnvOrDefault(),
+		MemcachedHost:     "127.0.0.1",
+		LatencyPercentile: percentile,
+		TuningTime:        1 * time.Second,
+	}
+
+	mutilateLoadGenerator := mutilate.New(local, mutilateConfig)
+
+	// Create connection with Snap.
+	logrus.Debug("Connecting to Snap")
+	snapConnection, err := client.New("http://127.0.0.1:8181", "v1", true)
+	if err != nil {
+		panic(err)
+	}
+
+	// Load the snap cassandra publisher plugin if not yet loaded.
+	// TODO(bp): Make helper for that.
+	logrus.Debug("Checking if publisher cassandra is loaded.")
+	plugins := snap.NewPlugins(snapConnection)
+	loaded, err := plugins.IsLoaded("publisher", "cassandra")
+	if err != nil {
+		panic(err)
+	}
+
+	if !loaded {
+		pluginPath := []string{path.Join(
+			os.Getenv("GOPATH"), "bin", "snap-plugin-publisher-cassandra")}
+		err = plugins.Load(pluginPath)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Define publisher.
+	publisher := wmap.NewPublishNode("cassandra", 2)
+	if publisher == nil {
+		panic("Failed to create Publish Node for cassandra")
+	}
+
+	// TODO(bp): Get cassandra host from cmdLine.
+	cassandraHostName := "127.0.0.1"
+	publisher.AddConfigItem("server", cassandraHostName)
+
+	// Initialize Mutilate Snap Session.
+	mutilateSnapSession := sessions.NewMutilateSnapSessionLauncher(
+		swan.GetSwanBuildPath(),
+		1*time.Second,
+		snapConnection,
+		publisher)
+
+	// Initialize LLC aggressor.
+	llcAggressorLauncher := l3data.New(local, l3data.DefaultL3Config())
+
+	// Create Experiment configuration.
+	configuration := sensitivity.Configuration{
+		SLO:             500,             // us
+		LoadDuration:    1 * time.Second, //10 * time.Second,
+		LoadPointsCount: 1,               //10,
+		Repetitions:     1,               //3,
+	}
+
+	sensitivityExperiment := sensitivity.NewExperiment(
+		"MemcachedWithLocalMutilateToCSV",
+		logLevel,
+		configuration,
+		sensitivity.NewLauncherWithoutSession(memcachedLauncher),
+		sensitivity.NewMonitoredLoadGenerator(mutilateLoadGenerator, mutilateSnapSession),
+		[]sensitivity.LauncherSessionPair{
+			sensitivity.NewLauncherWithoutSession(llcAggressorLauncher),
+		},
+	)
+
+	// Run Experiment.
+	err = sensitivityExperiment.Run()
+	if err != nil {
+		panic(err)
+	}
+}
