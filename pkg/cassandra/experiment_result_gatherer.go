c2ae6178305eda52526987742246080b9d79ba35
akwasnie <alicja.kwasniewska@intel.com>
2016-05-25 08:24:24 -0500

    SCE-361 Add results gatherer for given experiment (#138)
    * SCE-361 Add results gatherer for given experiment
    
    Include:
    dbConnector
    experimentResultsGatherer
    
    Tests:
    test connecting to casandra
    test getting values and tags for given experiment
    
    * SCE-361 Add results gatherer for given experiment
    
    Include:
    dbConnector
    experimentResultsGatherer
    
    Tests:
    test connecting to casandra
    test getting values and tags for given experiment
    
    * Move integration tests to proper dir
    
    Also rename db to cassandra
    
    * Add Cassandra Config struct with session field
    
    Add session field which allows to interact with data stored in Cassandra.
    
    * Move Config from bottom of cassandraConnector.go
    
    * Change name of main struct to Connection
    
    Add function for closing session.
    
    * Add model for Metrics get from cassandra
    
    Testing done:
    golint
    integration tests (integration_tests/pkg/cassandra)
    
    * Small fixes: comment plus new line

---
+++ pkg/cassandra/experiment_result_gatherer.go
@@ -0,0 +1,28 @@
+package cassandra
+
+import "time"
+
+// GetValuesForGivenExperiment returns list of Metrics structs based on experiment ID.
+func (cassandraConfig *Connection) GetValuesForGivenExperiment(experimentID string) []*Metrics {
+	metricsList := []*Metrics{}
+	// We look for following values for each Metric.
+	var doubleval float64
+	var namespace, host, strval, valtype string
+	var version int
+	var time time.Time
+	var boolval bool
+	labels := []string{}
+	tags := make(map[string]string)
+
+	// Get current cassandra session and select values.
+	session := cassandraConfig.CassandraSession()
+	iter := session.Query(`SELECT ns, ver, host, time, boolval, doubleval, labels, strval, tags, valtype FROM snap.metrics
+	WHERE tags CONTAINS '` + experimentID + `'ALLOW FILTERING`).Iter()
+
+	// Iterate through all gathered row, create Metrics struct for each row and add it to a list.
+	for iter.Scan(&namespace, &version, &host, &time, &boolval, &doubleval, &labels, &strval, &tags, &valtype) {
+		metric := NewMetrics(namespace, version, host, time, boolval, doubleval, labels, strval, tags, valtype)
+		metricsList = append(metricsList, metric)
+	}
+	return metricsList
+}
