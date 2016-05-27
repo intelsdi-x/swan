f474f8425ef8eeb7d83b9fd84f938242ff90719c
Szymon Konefal <szymon.konefal@intel.com>
2016-05-27 15:10:07 +0200

    Centos/RHEL dependencies script and set default BLAS to OpenBLAS.

---
+++ workloads/deep_learning/caffe_deps_centos.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+yum install -y epel-release
+yum groupinstall -y 'Development Tools'
+
+yum install -y protobuf protobuf-devel leveldb leveldb-devel snappy \
+    snappy-devel opencv opencv-devel boost boost-devel hdf5 hdf5-devel \
+    gflags glog lmdb gflags-devel glog-devel lmdb-devel
+
+# OpenBLAS shoudl be used for local developement, when not using MKL
+yum install -y openblas openblas-devel
