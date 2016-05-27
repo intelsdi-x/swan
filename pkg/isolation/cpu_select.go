9e5664adae813c9f7164c6f02ab32f4fce34fcf9
MrigankAtGitHub <mrigank.shekhar@intel.com>
2016-05-27 12:40:33 -0700

    Addressed review comments

--- pkg/isolation/cpu_select.go
+++ pkg/isolation/cpu_select.go
@@ -8,6 +8,9 @@ import (
 const ShareLLCButNotL1L2 uint = 1
 
 // CPUSelect with the desired criterion.
+// Select cores that share LLC but do not share L1, L2 cache.
+// Return error if we can not provide cores meeting criterion.
+// Scan all the sockets for set of core ids meeting filter
 func (threadset *IntSet) CPUSelect(count int, filters uint) error {
 
 	// cpuDiscovered collect CPU topology.
@@ -18,38 +21,39 @@ func (threadset *IntSet) CPUSelect(count int, filters uint) error {
 		return err
 	}
 
-	k := 0
-	// Select cores that share LLC but do not share L1, L2 cache.
-	// Return error if we can not provide cores meeting criterion.
+	corecount := 0
+
+	// Scan all the sockets for set of core ids meeting filter
 	if filters == ShareLLCButNotL1L2 {
 
+		// Return error if we can not provide cores meeting criterion.
 		// If count is more than cores per socket return error.
 		if count > cpuDiscovered.PhysicalCores {
-			return errors.New("Error - Insufficient cores")
+			return errors.New("Error - CPUSelect - insufficient cores")
 		}
 		// If count is more than cores per socket return error.
 		if count == 0 {
-			return errors.New("Error - Number of core requested is zero")
+			return errors.New("Error - CPUSelect- number of core requested is zero")
 		}
+		// Loop through all the sockets
+		for socketid := 0; socketid < cpuDiscovered.Sockets*2; socketid++ {
+			corecount = 0
+			// Loop through all the cores to find available core ids meeting filter
+			for cores := 0; cores < cpuDiscovered.PhysicalCores; cores++ {
+				threadset.Add(socketid*cpuDiscovered.PhysicalCores + cores)
+				corecount++
 
-		for i := 0; i < cpuDiscovered.Sockets*2; i++ {
-			k = 0
-
-			for j := 0; j < cpuDiscovered.PhysicalCores; j++ {
-				threadset.Add(i*cpuDiscovered.PhysicalCores + j)
-				k++
-
-				if k == count {
+				if corecount == count {
 					break
 				}
 			}
-			if k == count {
+			if corecount == count {
 				break
 			}
 		}
 	}
-	if k < count {
-		return errors.New("Error - Insufficient cores to meet selection criteria")
+	if corecount < count {
+		return errors.New("Error - CPUSelect - Insufficient cores to meet selection criteria")
 	}
 	return nil
 }
