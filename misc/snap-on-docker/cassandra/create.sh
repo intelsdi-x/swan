61270db0b48c423ec447ee8ca559ec00a4d910c3
akwasniewska <alicja.kwasniewska@intel.com>
2016-05-27 09:08:22 +0200

    SCE-392 Labels are deprecated in current snap
    Remove labels from create.sh snap-on-docker.

--- misc/snap-on-docker/cassandra/create.sh
+++ misc/snap-on-docker/cassandra/create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /usr/bin/cqlsh -e "CREATE KEYSPACE snap WITH replication = {'class': 'SimpleStrategy','replication_factor':1}"
-/usr/bin/cqlsh -e "CREATE TABLE snap.metrics (ns text, ver int, host text, time timestamp, valtype text, doubleVal double, boolVal boolean, strVal text, labels list<text>, tags map<text,text>, PRIMARY KEY ((ns, ver, host), time)) WITH CLUSTERING ORDER BY (time DESC);"
+/usr/bin/cqlsh -e "CREATE TABLE snap.metrics (ns text, ver int, host text, time timestamp, valtype text, doubleVal double, boolVal boolean, strVal text, tags map<text,text>, PRIMARY KEY ((ns, ver, host), time)) WITH CLUSTERING ORDER BY (time DESC);"
