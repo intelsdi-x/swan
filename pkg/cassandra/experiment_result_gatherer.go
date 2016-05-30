0c3eb1feabf5b1362c89c5c3271e89c8db694839
akwasnie <alicja.kwasniewska@intel.com>
2016-05-30 16:29:10 +0200

    SCE-361: Viewer of data gathered from Cassandra for given experiment (#147)
    * Added drawing visualization table
    
    * Added Sensitivity Experiment Viewer
    
    * Added godeps.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Addressed comments about map rendering and error handling
    
    * Added short script sceleton.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Pair programming with Alicja: Added updated godeps and removed unnecessary test.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed small issue with Cassandra query. No label column.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Small fix for help message.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added drawing visualization table
    
    * Added Sensitivity Experiment Viewer
    
    * Added short script sceleton.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added godeps.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Pair programming with Alicja: Added updated godeps and removed unnecessary test.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed small issue with Cassandra query. No label column.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Small fix for help message.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Addressed comments about map rendering and error handling
    
    * Comments addressed
    
    TODO: filter columns to show only some of them in table, based on decision
    which columns should be shown.
    
    * Filter returned values based on valtype.
    
    Added golint to Makefile for scripts.
    
    * Fix error for doubleval mixed with boolval
    
    * Remove space
    
    * Comments addressed.

--- pkg/cassandra/experiment_result_gatherer.go
+++ pkg/cassandra/experiment_result_gatherer.go
@@ -1,9 +1,11 @@
 package cassandra
 
-import "time"
+import (
+	"time"
+)
 
 // GetValuesForGivenExperiment returns list of Metrics structs based on experiment ID.
-func (cassandraConfig *Connection) GetValuesForGivenExperiment(experimentID string) []*Metrics {
+func (cassandraConfig *Connection) GetValuesForGivenExperiment(experimentID string) ([]*Metrics, error) {
 	metricsList := []*Metrics{}
 	// We look for following values for each Metric.
 	var doubleval float64
@@ -16,12 +18,17 @@ func (cassandraConfig *Connection) GetValuesForGivenExperiment(experimentID stri
 	// Get current cassandra session and select values.
 	session := cassandraConfig.CassandraSession()
 	iter := session.Query(`SELECT ns, ver, host, time, boolval, doubleval, strval, tags, valtype FROM snap.metrics
-	WHERE tags CONTAINS '` + experimentID + `'ALLOW FILTERING`).Iter()
+	WHERE tags CONTAINS ? ALLOW FILTERING`, experimentID).Iter()
 
 	// Iterate through all gathered row, create Metrics struct for each row and add it to a list.
 	for iter.Scan(&namespace, &version, &host, &time, &boolval, &doubleval, &strval, &tags, &valtype) {
 		metric := NewMetrics(namespace, version, host, time, boolval, doubleval, strval, tags, valtype)
 		metricsList = append(metricsList, metric)
 	}
-	return metricsList
+
+	if err := iter.Close(); err != nil {
+		return nil, err
+	}
+
+	return metricsList, nil
 }
---
+++ scripts/sensitivity_viewer/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/intelsdi-x/swan/pkg/cassandra"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"os"
+)
+
+var (
+	viewer          = kingpin.New("SensitivityViewer", "Simple command-line tool for viewing Sensitivity experiment results.")
+	cassandraServer = viewer.Flag("cassandra_host", "host for Cassandra DB with results.").Default("127.0.0.1").String()
+
+	listExperimentsCmd = viewer.Command("list", "List all experiment UUIDs")
+
+	showExperimentDataCmd = viewer.Command("show", "Get Experiment Results for specific experiment UUID")
+	experimentID          = showExperimentDataCmd.Arg("experiment_uuid", "Experiment UUID").Required().String()
+)
+
+func listExperiments() {
+	err := cassandra.DrawList(*cassandraServer)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func showExperiment() {
+	err := cassandra.DrawTable(*experimentID, *cassandraServer)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Run via: go run scripts/sensitivity_viewer/main.go
+func main() {
+	switch kingpin.MustParse(viewer.Parse(os.Args[1:])) {
+	// List experiments.
+	case listExperimentsCmd.FullCommand():
+		listExperiments()
+
+	// Show experiment data for specified experimentID.
+	case showExperimentDataCmd.FullCommand():
+		showExperiment()
+	}
+}
