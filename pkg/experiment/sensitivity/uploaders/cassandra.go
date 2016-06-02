5d29a0c113a6d54031e8a0291930b650ccf5425c
mstachowski <michal.stachowski@outlook.com>
2016-06-02 15:20:54 +0200

    Apply Metadata PR changes

--- pkg/experiment/sensitivity/uploaders/cassandra.go
+++ pkg/experiment/sensitivity/uploaders/cassandra.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/gocql/gocql"
 	"github.com/hailocab/gocassa"
-	"github.com/intelsdi-x/swan/pkg/metrics"
+	"github.com/intelsdi-x/swan/pkg/experiment/sensitivity"
 )
 
 const experimentTablePrefix = "experiment"
@@ -58,7 +58,7 @@ type Config struct {
 }
 
 // NewCassandra created new Cassandra Uploader
-func NewCassandra(config Config) (metrics.Uploader, error) {
+func NewCassandra(config Config) (sensitivity.Uploader, error) {
 	gocql := gocql.NewCluster(config.Host...)
 	gocql.ProtoVersion = 4
 	session, err := gocql.CreateSession()
@@ -82,7 +82,7 @@ func NewCassandra(config Config) (metrics.Uploader, error) {
 }
 
 //SendMetrics implements metrics.Uploader interface
-func (c cassandra) SendMetrics(metrics metrics.Swan) error {
+func (c cassandra) SendMetrics(metrics sensitivity.Metadata) error {
 	experimentMetrics := c.buildExperimentMetadata(metrics)
 	err := c.experiment.Set(experimentMetrics).Run()
 	if err != nil {
@@ -103,38 +103,38 @@ func (c cassandra) SendMetrics(metrics metrics.Swan) error {
 
 }
 
-func (c cassandra) buildExperimentMetadata(metrics metrics.Swan) experiment {
+func (c cassandra) buildExperimentMetadata(metrics sensitivity.Metadata) experiment {
 	experimentMetrics := experiment{}
-	experimentMetrics.ID = metrics.Tags.ExperimentID
-	experimentMetrics.TestingDuration = metrics.Metrics.LoadDuration
-	experimentMetrics.LcName = metrics.Metrics.LCName
-	experimentMetrics.LcParameters = metrics.Metrics.LCParameters
-	experimentMetrics.LcIsolation = metrics.Metrics.LCIsolation
-	experimentMetrics.LgParameters = metrics.Metrics.LGParameters
-	experimentMetrics.LgName = metrics.Metrics.LGName
-	experimentMetrics.LgIsolation = metrics.Metrics.LGIsolation
-	experimentMetrics.LoadPointsNumber = metrics.Metrics.LoadPointsNumber
-	experimentMetrics.Repetitions = metrics.Metrics.RepetitionsNumber
-	experimentMetrics.SLO = metrics.Metrics.SLO
+	experimentMetrics.ID = metrics.ExperimentID
+	experimentMetrics.TestingDuration = metrics.LoadDuration
+	experimentMetrics.LcName = metrics.LCName
+	experimentMetrics.LcParameters = metrics.LCParameters
+	experimentMetrics.LcIsolation = metrics.LCIsolation
+	experimentMetrics.LgParameters = metrics.LGParameters
+	experimentMetrics.LgName = metrics.LGName
+	experimentMetrics.LgIsolation = metrics.LGIsolation
+	experimentMetrics.LoadPointsNumber = metrics.LoadPointsNumber
+	experimentMetrics.Repetitions = metrics.RepetitionsNumber
+	experimentMetrics.SLO = metrics.SLO
 
 	return experimentMetrics
 }
 
-func (c cassandra) buildPhaseMetadata(metrics metrics.Swan) phase {
+func (c cassandra) buildPhaseMetadata(metrics sensitivity.Metadata) phase {
 	phaseMetrics := phase{}
-	phaseMetrics.ID = metrics.Tags.PhaseID
-	phaseMetrics.ExperimentID = metrics.Tags.ExperimentID
-	phaseMetrics.AggressorName = metrics.Metrics.AggressorName
+	phaseMetrics.ID = metrics.PhaseID
+	phaseMetrics.ExperimentID = metrics.ExperimentID
+	phaseMetrics.AggressorName = metrics.AggressorName
 
 	return phaseMetrics
 }
 
-func (c cassandra) buildMeasurementMetadata(metrics metrics.Swan) measurement {
+func (c cassandra) buildMeasurementMetadata(metrics sensitivity.Metadata) measurement {
 	measurementMetrics := measurement{}
-	measurementMetrics.ID = metrics.Tags.RepetitionID
-	measurementMetrics.ExperimentID = metrics.Tags.ExperimentID
-	measurementMetrics.PhaseID = metrics.Tags.PhaseID
-	measurementMetrics.AggressorParameters = metrics.Metrics.AggressorParameters
+	measurementMetrics.ID = metrics.RepetitionID
+	measurementMetrics.ExperimentID = metrics.ExperimentID
+	measurementMetrics.PhaseID = metrics.PhaseID
+	measurementMetrics.AggressorParameters = metrics.AggressorParameters
 
 	return measurementMetrics
 }
