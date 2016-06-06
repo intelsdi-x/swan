87e11d316bf7df11bff8e601c26a56e31d17bed3
Szymon Konefal <szymon.konefal@intel.com>
2016-06-06 16:44:39 +0200

    Memcached + Synthethics + Caffe experiment

---
+++ experiments/memcached/synthetics_caffe/README.md
@@ -0,0 +1,43 @@
+# Sensitivity Experiment: Memcached + Caffe + Synthethic aggressors
+
+Sensitivity experiment runs different measurements to test the performance of co-located workloads on a single node.
+It executes workloads and triggers gathering of certain metrics like latency (SLI) and the achieved number of Request per Second (QPS/RPS)
+
+Every experiment includes:
+- Latency critical / Production workload (LC/PR)
+- Load Generator to generate simulated load on the LC (LG)
+- Optional aggressors to be co-located on node.
+
+## Experiment Enviroment Configuration
+
+Experiment makes use of enviroment variables:
+
+- SWAN_MEMCAHED_HOST (default: 127.0.0.1)
+- SWAN_CASSANDRA_HOST (default: 127.0.0.1)
+- SWAN_SNAP_ADDRESS (default: 127.0.0.1:8181)
+
+## Workload details
+
+| Type | Name  | Source | Execution | Isolation | APMs |
+| --- | --- | --- | --- | --- | --- |
+| *Latency Critical* | Memcached | [Readme](../../../workloads/data_caching/memcached) | Local | None | None |
+| *Load Generator* | Mutilate | [Readme](../../../workloads/data_caching/memcached) | Local | None | `Latency` and `QPS` via Snap to `Cassandra` |
+| *Aggressor* | Last-Level cache synthetic | [Code](../../../workloads/low-level-aggressors/l3.c) | Local | None | None |
+
+## Prerequisites
+- Running `snapd`
+- Running Cassandra (NOTE: Cassandra publisher is required but it's private currently)
+- `make build_workloads`
+- `make build`
+
+**NOTE**: It is recommended to ensure that all integration test are working on your machine before running experiment.
+
+## Running
+
+`./build/experiments/<path_to_experiment>/<name_of_experiment>`
+
+## Cassandra Result Viewer
+
+After test execution, you can see the results using following script:
+
+`go run ./script/sensitivity_viewer/main.go `
---
+++ experiments/memcached/synthetics_caffe/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/intelsdi-x/snap/mgmt/rest/client"
+	"github.com/intelsdi-x/snap/scheduler/wmap"
+	"github.com/intelsdi-x/swan/pkg/executor"
+	"github.com/intelsdi-x/swan/pkg/experiment/sensitivity"
+	"github.com/intelsdi-x/swan/pkg/osutil"
+	//"github.com/intelsdi-x/swan/pkg/snap"
+	"github.com/intelsdi-x/swan/pkg/snap/sessions"
+	"github.com/intelsdi-x/swan/pkg/swan"
+	"github.com/intelsdi-x/swan/pkg/workloads/caffe"
+	"github.com/intelsdi-x/swan/pkg/workloads/low_level/l1data"
+	"github.com/intelsdi-x/swan/pkg/workloads/low_level/l1instruction"
+	"github.com/intelsdi-x/swan/pkg/workloads/low_level/l3data"
+	"github.com/intelsdi-x/swan/pkg/workloads/low_level/memoryBandwidth"
+	"github.com/intelsdi-x/swan/pkg/workloads/memcached"
+	"github.com/intelsdi-x/swan/pkg/workloads/mutilate"
+	"github.com/shopspring/decimal"
+	//"os"
+	//"path"
+	"time"
+)
+
+// Check README.md for details of this experiment.
+func main() {
+	logLevel := logrus.InfoLevel
+	logrus.SetLevel(logLevel)
+
+	local := executor.NewLocal()
+
+	// Initialize Memcached Launcher.
+	memcachedLauncher := memcached.New(local, memcached.DefaultMemcachedConfig())
+
+	// Initialize Mutilate Launcher.
+	percentile, _ := decimal.NewFromString("99.9")
+
+	memcachedHost := osutil.GetEnvOrDefault("SWAN_MEMCAHED_HOST", "127.0.0.1")
+	mutilateConfig := mutilate.Config{
+		MutilatePath:      mutilate.GetPathFromEnvOrDefault(),
+		MemcachedHost:     memcachedHost,
+		LatencyPercentile: percentile,
+		TuningTime:        1 * time.Second,
+	}
+
+	mutilateLoadGenerator := mutilate.New(local, mutilateConfig)
+
+	// Create connection with Snap.
+	logrus.Debug("Connecting to Snap")
+
+	snapAddress := osutil.GetEnvOrDefault("SWAN_SNAP_ADDRESS", "http://127.0.0.1:8181")
+	snapConnection, err := client.New(snapAddress, "v1", true)
+	if err != nil {
+		panic(err)
+	}
+
+	// Load the snap cassandra publisher plugin if not yet loaded.
+	// TODO(bp): Make helper for that.
+	logrus.Debug("Checking if publisher cassandra is loaded.")
+	//plugins := snap.NewPlugins(snapConnection)
+	//loaded, err := plugins.IsLoaded("publisher", "cassandra")
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	//if !loaded {
+	//	pluginPath := []string{path.Join(
+	//		os.Getenv("GOPATH"), "bin", "snap-plugin-publisher-cassandra")}
+	//	err = plugins.Load(pluginPath)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+
+	// Define publisher.
+	publisher := wmap.NewPublishNode("cassandra", 2)
+	if publisher == nil {
+		panic("Failed to create Publish Node for cassandra")
+	}
+
+	cassandraHostName := osutil.GetEnvOrDefault("SWAN_CASSANDRA_HOST", "127.0.0.1")
+	publisher.AddConfigItem("server", cassandraHostName)
+
+	// Initialize Mutilate Snap Session.
+	mutilateSnapSession := sessions.NewMutilateSnapSessionLauncher(
+		swan.GetSwanBuildPath(),
+		1*time.Second,
+		snapConnection,
+		publisher)
+
+	// Initialize aggressors.
+	llcAggressorLauncher := l3data.New(local, l3data.DefaultL3Config())
+	memBwAggressorLauncher := memoryBandwidth.New(local, memoryBandwidth.DefaultMemBwConfig())
+	l1iAggressorLauncher := l1instruction.New(local, l1instruction.DefaultL1iConfig())
+	lidAggressorLauncher := l1data.New(local, l1data.DefaultL1dConfig())
+	caffeAggressorLauncher := caffe.New(local, caffe.DefaultConfig())
+
+	// Create Experiment configuration.
+	configuration := sensitivity.Configuration{
+		SLO:             500,             // us
+		LoadDuration:    1 * time.Second, //10 * time.Second,
+		LoadPointsCount: 1,               //10,
+		Repetitions:     1,               //3,
+	}
+
+	sensitivityExperiment := sensitivity.NewExperiment(
+		"MemcachedWithLocalMutilateToCassandra",
+		logLevel,
+		configuration,
+		sensitivity.NewLauncherWithoutSession(memcachedLauncher),
+		sensitivity.NewMonitoredLoadGenerator(mutilateLoadGenerator, mutilateSnapSession),
+		[]sensitivity.LauncherSessionPair{
+			sensitivity.NewLauncherWithoutSession(l1iAggressorLauncher),
+			sensitivity.NewLauncherWithoutSession(lidAggressorLauncher),
+			sensitivity.NewLauncherWithoutSession(llcAggressorLauncher),
+			sensitivity.NewLauncherWithoutSession(memBwAggressorLauncher),
+			sensitivity.NewLauncherWithoutSession(caffeAggressorLauncher),
+		},
+	)
+
+	// Run Experiment.
+	err = sensitivityExperiment.Run()
+	if err != nil {
+		panic(err)
+	}
+}
