63b25307b8ce9fdc59e8080437ff1c7401ff7f78
akwasniewska <alicja.kwasniewska@intel.com>
2016-06-13 04:26:15 +0200

    Clean caffe experiment.

--- experiments/memcached/synthetics_caffe/main.go
+++ experiments/memcached/synthetics_caffe/main.go
@@ -27,6 +27,16 @@ import (
 	"time"
 )
 
+const (
+	defaultSSHPort            = 22
+	defaultMemcachedThreads   = 10
+	defaultMutilatePercentile = "99"
+	defaultTuningTime         = 1
+	defaultLoadDuration       = 1
+	defaultSLO                = 500
+	defaultRepetitionsNumber  = 10
+)
+
 // Check README.md for details of this experiment.
 func main() {
 	logLevel := logrus.InfoLevel
@@ -35,29 +45,26 @@ func main() {
 	numaZero := isolation.NewIntSet(0)
 	hpCpus := isolation.NewIntSet(0, 1, 2, 3, 4, 20, 21, 22, 23, 24)
 	beCpus := isolation.NewIntSet(5, 6, 7, 8, 9, 25, 26, 27, 28, 29)
-	//
+
+	// Create new Cpu sets.
 	hpIsolation, err := cgroup.NewCPUSet("hp", hpCpus, numaZero, true, false)
 	beIsolation, err := cgroup.NewCPUSet("be", beCpus, numaZero, true, false)
-	//
-	//hpIsolation, err := NewCPUSetWithExecutor()
-
 	hpIsolation.Create()
 	defer hpIsolation.Clean()
-	localHPIsolated := executor.NewLocalIsolated(hpIsolation)
-
 	beIsolation.Create()
 	defer beIsolation.Clean()
-	localBEIsolated := executor.NewLocalIsolated(beIsolation)
 
-	//local := executor.NewLocal()
+	// Create local executors with given isolation.
+	localHPIsolated := executor.NewLocalIsolated(hpIsolation)
+	localBEIsolated := executor.NewLocalIsolated(beIsolation)
 
 	// Initialize Memcached Launcher.
 	conf := memcached.DefaultMemcachedConfig()
-	conf.NumThreads = 5
+	conf.NumThreads = defaultMemcachedThreads
 	memcachedLauncher := memcached.New(localHPIsolated, conf)
 
 	// Initialize Mutilate Launcher.
-	percentile, err := decimal.NewFromString("99")
+	percentile, err := decimal.NewFromString(defaultMutilatePercentile)
 	if err != nil {
 		panic(err)
 	}
@@ -68,46 +75,32 @@ func main() {
 		MutilatePath:      mutilate.GetPathFromEnvOrDefault(),
 		MemcachedHost:     memcachedHost,
 		LatencyPercentile: percentile,
-		TuningTime:        1 * time.Second,
+		TuningTime:        defaultTuningTime * time.Second,
 	}
 
+	// Get current user for ssh config.
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
-	sshConfig, err := executor.NewSSHConfig(mutilateHost, 22, user)
+
+	// Create ssh config and remote executor with this config.
+	sshConfig, err := executor.NewSSHConfig(mutilateHost, defaultSSHPort, user)
 	if err != nil {
 		panic(err)
 	}
 	remote := executor.NewRemote(*sshConfig)
+
+	// Create mutilate.
 	mutilateLoadGenerator := mutilate.New(remote, mutilateConfig)
 
 	// Create connection with Snap.
 	logrus.Debug("Connecting to Snap")
-
 	snapAddress := os.GetEnvOrDefault("SWAN_SNAP_ADDRESS", "http://127.0.0.1:8181")
 	snapConnection, err := client.New(snapAddress, "v1", true)
 	if err != nil {
 		panic(err)
 	}
-
-	// Load the snap cassandra publisher plugin if not yet loaded.
-	// TODO(bp): Make helper for that.
-	//logrus.Debug("Checking if publisher cassandra is loaded.")
-	//plugins := snap.NewPlugins(snapConnection)
-	//loaded, err := plugins.IsLoaded("publisher", "cassandra")
-	//if err != nil {
-	//	panic(err)
-	//}
-
-	//if !loaded {
-	//	pluginPath := []string{path.Join(
-	//		os.Getenv("GOPATH"), "bin", "snap-plugin-publisher-cassandra")}
-	//	err = plugins.Load(pluginPath)
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//}
 
 	// Define publisher.
 	publisher := wmap.NewPublishNode("cassandra", 2)
@@ -134,10 +127,10 @@ func main() {
 
 	// Create Experiment configuration.
 	configuration := sensitivity.Configuration{
-		SLO:             500,             // us
-		LoadDuration:    1 * time.Second, //10 * time.Second,
-		LoadPointsCount: 10,              //10,
-		Repetitions:     1,               //3,
+		SLO:             defaultSLO,
+		LoadDuration:    defaultLoadDuration * time.Second,
+		LoadPointsCount: defaultRepetitionsNumber,
+		Repetitions:     defaultRepetitionsNumber,
 	}
 
 	sensitivityExperiment := sensitivity.NewExperiment(
