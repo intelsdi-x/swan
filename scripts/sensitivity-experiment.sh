3e628c250546603b4e15521f3b39cb94eff9e599
akwasniewska <alicja.kwasniewska@intel.com>
2016-06-13 09:16:08 +0200

    Added script for running experiment end-to-end.

---
+++ scripts/sensitivity-experiment.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+usage="$(basename "$0") [-h] [-p] -- program to run sensitivity experiment
+
+where:
+    -h Print usage
+    -p Path to experiment binary"
+
+while getopts ':hp:' opt; do
+  case "$opt" in
+    h)
+      echo "$usage"
+      exit
+      ;;
+    p)
+      path=$OPTARG
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    esac
+  done
+
+ID=$($GOPATH/src/github.com/intelsdi-x/swan/$path)
+if [ -z ${ID+x} ];
+  then echo "could not retrieve experiment ID";
+  else go run $GOPATH/src/github.com/intelsdi-x/swan/scripts/sensitivity_viewer/main.go sensitivity $ID;
+fi
