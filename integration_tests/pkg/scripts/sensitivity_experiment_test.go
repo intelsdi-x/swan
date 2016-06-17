6bdd1782266beba3e3b038b5157fe04ef1c8c09f
akwasniewska <alicja.kwasniewska@intel.com>
2016-06-17 07:38:08 +0200

    Added checking if Cassandra is running.

--- integration_tests/pkg/scripts/sensitivity_experiment_test.go
+++ integration_tests/pkg/scripts/sensitivity_experiment_test.go
@@ -2,6 +2,7 @@ package scripts
 
 import (
 	"fmt"
+	"github.com/intelsdi-x/swan/pkg/cassandra"
 	. "github.com/smartystreets/goconvey/convey"
 	"os"
 	"os/exec"
@@ -17,25 +18,35 @@ func runScript(parameter string) (string, error) {
 }
 
 func TestSensitivityExperimentScript(t *testing.T) {
-	Convey("After running a script with help option", t, func() {
-		output, err := runScript("-h")
-		Convey("There should be no error and output should not be nil", func() {
-			So(output, ShouldNotBeNil)
+	Convey("While connecting to Cassandra with proper parameters", t, func() {
+		cassandraConfig, err := cassandra.CreateConfigWithSession("127.0.0.1", "snap")
+		So(err, ShouldBeNil)
+		session := cassandraConfig.CassandraSession()
+		Convey("I should receive not empty session", func() {
+			So(session, ShouldNotBeNil)
 			So(err, ShouldBeNil)
-		})
-	})
-	Convey("After running a script with non-existing experiment binary", t, func() {
-		output, err := runScript("-p /nonExistingBinary")
-		Convey("There should be an error and output should be empty", func() {
-			So(output, ShouldEqual, "")
-			So(fmt.Sprintf("%s", err), ShouldEqual, "exit status 127")
-		})
-	})
-	Convey("After running a script with existing proper experiment", t, func() {
-		output, err := runScript("-p integration_tests/pkg/scripts/fakeExperiment.sh")
-		Convey("There should be no error and output should not be empty", func() {
-			So(output, ShouldNotBeNil)
-			So(err, ShouldBeNil)
+			Convey("After running a script with help option", func() {
+				output, err := runScript("-h")
+				Convey("There should be no error and output should not be nil", func() {
+					So(output, ShouldNotBeNil)
+					So(err, ShouldBeNil)
+				})
+			})
+			Convey("After running a script with non-existing experiment binary", func() {
+				output, err := runScript("-p /nonExistingBinary")
+				Convey("There should be an error and output should be empty", func() {
+					So(output, ShouldEqual, "")
+					So(fmt.Sprintf("%s", err), ShouldEqual, "exit status 127")
+				})
+			})
+			Convey("After running a script with existing proper experiment", func() {
+				output, err := runScript("-p integration_tests/pkg/scripts/fakeExperiment.sh")
+				Convey("There should be no error and output should not be empty", func() {
+					So(output, ShouldNotBeNil)
+					So(err, ShouldBeNil)
+				})
+			})
+
 		})
 	})
 }
