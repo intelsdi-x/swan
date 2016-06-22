f0ff80b3f934b459a877f4b7fde2685342bf003d
mstachowski <michal.stachowski@outlook.com>
2016-06-22 14:22:22 +0200

    SCe-459: Error in llc_aggr_cassandra experiment
    - Added detailed information about experiment failure
    - Fixed situation when snapd isn't able to read experiment details from
      files.

--- pkg/conf/flag.go
+++ pkg/conf/flag.go
@@ -122,3 +122,35 @@ func (s SliceFlag) Value() []string {
 
 	return *s.value
 }
+
+type BoolFlag struct {
+	flag
+	defaultValue bool
+	value        *bool
+}
+
+// NewBoolFlag is a constructor of IntFlag struct.
+func NewBoolFlag(flagName string, description string, defaultValue bool) BoolFlag {
+	boolFlag := BoolFlag{
+		flag: flag{
+			name:        flagName,
+			description: description,
+		},
+		defaultValue: defaultValue,
+	}
+
+	boolFlag.value = app.Flag(flagName, description).OverrideDefaultFromEnvar(boolFlag.envName()).Bool()
+	isEnvParsed = false
+
+	return boolFlag
+}
+
+// Value returns value of defined flag after parse.
+// NOTE: If conf is not parsed it returns default value (!)
+func (b BoolFlag) Value() bool {
+	if !isEnvParsed {
+		return b.defaultValue
+	}
+
+	return *b.value
+}
