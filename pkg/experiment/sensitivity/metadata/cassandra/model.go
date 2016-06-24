c7967b972224eed8d361586d4475b92e365758ce
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-06-24 19:08:21 +0200

    SCE-395 - general cleanup of file names and directory structure

--- pkg/experiment/sensitivity/metadata/cassandra/model.go
+++ pkg/experiment/sensitivity/metadata/cassandra/model.go
@@ -1,4 +1,4 @@
-package uploaders
+package cassandra
 
 import "time"
 
--- pkg/experiment/sensitivity/uploader.go
+++
@@ -1,9 +0,0 @@
-package sensitivity
-
-import "github.com/intelsdi-x/swan/pkg/experiment/sensitivity/metadata"
-
-// Uploader is a interface for SwanMetrics uploading into external media(like database or telemetry framework)
-type Uploader interface {
-	SendMetadata(metadata.Experiment) error
-	GetMetadata(experiment string) (metadata.Experiment, error)
-}
