ad8f0f751a4168f3b9b2a5648a218d1c24314377
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-06-27 15:00:38 +0200

    SCE-395 - improving test readability

--- pkg/experiment/sensitivity/metadata/cassandra/to_metadata_test.go
+++ pkg/experiment/sensitivity/metadata/cassandra/to_metadata_test.go
@@ -1,24 +1,64 @@
 package cassandra
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
-	"github.com/intelsdi-x/swan/pkg/experiment/sensitivity/metadata"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestCassandraModelTransformation(t *testing.T) {
-	Convey("When Cassandra model is transformed I should receive correct metadata model", t, func() {
-		experimentModel, phasesModel, measurementsModel := prepareCassandraModel()
+	experimentModel, phasesModel, measurementsModel := prepareCassandraModel()
+	mapper := newCassandraToMetadata()
+	experiment := mapper.fromCassandraModelToMetadata(experimentModel, phasesModel, measurementsModel)
 
-		mapper := newCassandraToMetadata()
-		experiment := mapper.transform(experimentModel, phasesModel, measurementsModel)
+	Convey("When Cassandra model is transformed into metadata", t, func() {
+		Convey("Experiment metadata should be correct", func() {
+			So(experiment.ID, ShouldEqual, "experiment")
+			So(experiment.LoadDuration, ShouldResemble, time.Second)
+			So(experiment.TuningDuration, ShouldResemble, time.Second)
+			So(experiment.LcName, ShouldEqual, "launch critical task")
+			So(experiment.LgNames, ShouldResemble, []string{"load generator one"})
+			So(experiment.RepetitionsNumber, ShouldEqual, 1)
+			So(experiment.LoadPointsNumber, ShouldEqual, 2)
+			So(experiment.SLO, ShouldEqual, 123)
+		})
 
-		soExperimentIsCorrect(experiment)
-		soPhasesAreCorrect(experiment)
-		soMeasurementsAreCorrect(experiment)
+		Convey("Phases metadata should be correct", func() {
+			So(experiment.Phases[0].ID, ShouldEqual, "phase one")
+			So(experiment.Phases[0].LCIsolation, ShouldEqual, "Latency critical isolation phase one")
+			So(experiment.Phases[0].LCParameters, ShouldEqual, "Latency critical parameters phase one")
+			So(experiment.Phases[0].AggressorNames, ShouldResemble, []string{"first aggressor phase one", "second aggressor phase one"})
+			So(experiment.Phases[0].AggressorParameters, ShouldResemble, []string{"first aggressor parameters phase one", "second aggressor parameters phase one"})
+			So(experiment.Phases[0].AggressorIsolations, ShouldResemble, []string{"first aggressor isolation phase one", "second aggressor isolation phase one"})
+
+			So(experiment.Phases[1].ID, ShouldEqual, "phase two")
+			So(experiment.Phases[1].LCIsolation, ShouldEqual, "Latency critical isolation phase two")
+			So(experiment.Phases[1].LCParameters, ShouldEqual, "Latency critical parameters phase two")
+			So(experiment.Phases[1].AggressorNames, ShouldResemble, []string{"first aggressor phase two", "second aggressor phase two"})
+			So(experiment.Phases[1].AggressorParameters, ShouldResemble, []string{"first aggressor parameters phase two", "second aggressor parameters phase two"})
+			So(experiment.Phases[1].AggressorIsolations, ShouldResemble, []string{"first aggressor isolation phase two", "second aggressor isolation phase two"})
+
+		})
+
+		Convey("Measurements metadata should be correct", func() {
+			So(experiment.Phases[0].Measurements, ShouldHaveLength, 2)
+
+			So(experiment.Phases[0].Measurements[0].Load, ShouldEqual, 0.5)
+			So(experiment.Phases[0].Measurements[0].LoadPointQPS, ShouldEqual, 303.0)
+			So(experiment.Phases[0].Measurements[0].LGParameters, ShouldResemble, []string{"Load generator parameters measurement 1.1"})
+
+			So(experiment.Phases[0].Measurements[1].Load, ShouldEqual, 0.7)
+			So(experiment.Phases[0].Measurements[1].LoadPointQPS, ShouldEqual, 666.6)
+			So(experiment.Phases[0].Measurements[1].LGParameters, ShouldResemble, []string{"Load generator parameters measurement 1.2"})
+
+			So(experiment.Phases[1].Measurements, ShouldHaveLength, 1)
+
+			So(experiment.Phases[1].Measurements[0].Load, ShouldEqual, 0.1)
+			So(experiment.Phases[1].Measurements[0].LoadPointQPS, ShouldEqual, 0.75)
+			So(experiment.Phases[1].Measurements[0].LGParameters, ShouldResemble, []string{"Load generator parameters measurement 2.1"})
+
+		})
 	})
 }
 
@@ -73,47 +113,4 @@ func prepareCassandraModel() (Experiment, []Phase, []Measurement) {
 	}
 
 	return experiment, []Phase{phaseOne, phaseTwo}, []Measurement{measurementTwoOne, measurementOneTwo, measurementOneOne}
-}
-
-func soExperimentIsCorrect(experiment metadata.Experiment) {
-	So(experiment.ID, ShouldEqual, "experiment")
-	So(experiment.LoadDuration, ShouldResemble, time.Second)
-	So(experiment.TuningDuration, ShouldResemble, time.Second)
-	So(experiment.LcName, ShouldEqual, "launch critical task")
-	So(experiment.LgNames, ShouldResemble, []string{"load generator one"})
-	So(experiment.RepetitionsNumber, ShouldEqual, 1)
-	So(experiment.LoadPointsNumber, ShouldEqual, 2)
-	So(experiment.SLO, ShouldEqual, 123)
-}
-
-func soPhasesAreCorrect(experiment metadata.Experiment) {
-	soPhaseIsCorrect(experiment.Phases[0], "one")
-	soPhaseIsCorrect(experiment.Phases[1], "two")
-}
-
-func soPhaseIsCorrect(phase metadata.Phase, name string) {
-	So(phase.ID, ShouldEqual, fmt.Sprintf("phase %s", name))
-	So(phase.LCIsolation, ShouldEqual, fmt.Sprintf("Latency critical isolation phase %s", name))
-	So(phase.LCParameters, ShouldEqual, fmt.Sprintf("Latency critical parameters phase %s", name))
-	So(phase.AggressorNames, ShouldResemble, []string{fmt.Sprintf("first aggressor phase %s", name), fmt.Sprintf("second aggressor phase %s", name)})
-	So(phase.AggressorParameters, ShouldResemble, []string{fmt.Sprintf("first aggressor parameters phase %s", name), fmt.Sprintf("second aggressor parameters phase %s", name)})
-	So(phase.AggressorIsolations, ShouldResemble, []string{fmt.Sprintf("first aggressor isolation phase %s", name), fmt.Sprintf("second aggressor isolation phase %s", name)})
-}
-
-func soMeasurementsAreCorrect(experiment metadata.Experiment) {
-	So(experiment.Phases[0].Measurements, ShouldHaveLength, 2)
-
-	So(experiment.Phases[0].Measurements[0].Load, ShouldEqual, 0.5)
-	So(experiment.Phases[0].Measurements[0].LoadPointQPS, ShouldEqual, 303.0)
-	So(experiment.Phases[0].Measurements[0].LGParameters, ShouldResemble, []string{"Load generator parameters measurement 1.1"})
-
-	So(experiment.Phases[0].Measurements[1].Load, ShouldEqual, 0.7)
-	So(experiment.Phases[0].Measurements[1].LoadPointQPS, ShouldEqual, 666.6)
-	So(experiment.Phases[0].Measurements[1].LGParameters, ShouldResemble, []string{"Load generator parameters measurement 1.2"})
-
-	So(experiment.Phases[1].Measurements, ShouldHaveLength, 1)
-
-	So(experiment.Phases[1].Measurements[0].Load, ShouldEqual, 0.1)
-	So(experiment.Phases[1].Measurements[0].LoadPointQPS, ShouldEqual, 0.75)
-	So(experiment.Phases[1].Measurements[0].LGParameters, ShouldResemble, []string{"Load generator parameters measurement 2.1"})
 }
