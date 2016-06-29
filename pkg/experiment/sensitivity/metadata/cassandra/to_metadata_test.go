796d91bf3ecf1785fe5bf3c3312a9b7cee243f29
iwankgb <maciej.iwanowski@critical.today>
2016-06-29 14:47:41 +0200

    SCE-395 - sensitivity profile experiment [1/2] (#217)
    * SCE-395 - adding sensitivity profile metadata, Cassandra model and model-to-metadata translation
    
    * SCE-395 - improving test readability
    
    * SCE-395 - removing redundant struct, applying suggested fixes to comments
    
    * SCE-395 - removing some files that reappeared mysteriously; using separate struct for aggressor metadata
    
    * SCE-395 - only one load generator is allowed
    
    * SCE-395 - more consistent field names
    
    * SCE-395 - applying code review suggestions to metadata and its cassandra model
    
    * SCE-395 - renaming some structs and functions
    
    * SCE-395 - further simplification
    
    * SCE-395 - fixing broken make lint
    
    * SCE-395 - float to int
    
    * SCE-395 - removing constructor

---
+++ pkg/experiment/sensitivity/metadata/cassandra/to_metadata_test.go
@@ -0,0 +1,139 @@
+package cassandra
+
+import (
+	"testing"
+	"time"
+
+	"github.com/intelsdi-x/swan/pkg/experiment/sensitivity/metadata"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestCassandraModelTransformation(t *testing.T) {
+	experimentModel, phasesModel, measurementsModel := prepareCassandraModel()
+	mapper := toMetadata{phaseNameToIndex: make(map[string]int)}
+	experiment := mapper.transform(experimentModel, phasesModel, measurementsModel)
+
+	Convey("When Cassandra model is transformed into metadata", t, func() {
+		Convey("Experiment metadata should be correct", func() {
+			So(experiment.ID, ShouldEqual, "experiment")
+			So(experiment.LoadDuration, ShouldResemble, time.Second)
+			So(experiment.TuningDuration, ShouldResemble, time.Second)
+			So(experiment.LCName, ShouldEqual, "launch critical task")
+			So(experiment.LGName, ShouldEqual, "load generator")
+			So(experiment.RepetitionsNumber, ShouldEqual, 1)
+			So(experiment.LoadPointsNumber, ShouldEqual, 2)
+			So(experiment.SLO, ShouldEqual, 123)
+		})
+
+		Convey("Phases metadata should be correct", func() {
+			So(experiment.Phases[0].ID, ShouldEqual, "phase one")
+			So(experiment.Phases[0].LCIsolation, ShouldEqual, "Latency critical isolation phase one")
+			So(experiment.Phases[0].LCParameters, ShouldEqual, "Latency critical parameters phase one")
+
+			So(experiment.Phases[1].ID, ShouldEqual, "phase two")
+			So(experiment.Phases[1].LCIsolation, ShouldEqual, "Latency critical isolation phase two")
+			So(experiment.Phases[1].LCParameters, ShouldEqual, "Latency critical parameters phase two")
+		})
+
+		Convey("Aggressors metadata should be correct", func() {
+			So(experiment.Phases[0].Aggressors, ShouldHaveLength, 2)
+			So(experiment.Phases[0].Aggressors[0].Name, ShouldEqual, "first aggressor phase one")
+			So(experiment.Phases[0].Aggressors[0].Parameters, ShouldEqual, "first aggressor parameters phase one")
+			So(experiment.Phases[0].Aggressors[0].Isolation, ShouldEqual, "first aggressor isolation phase one")
+			So(experiment.Phases[0].Aggressors[1].Name, ShouldEqual, "second aggressor phase one")
+			So(experiment.Phases[0].Aggressors[1].Parameters, ShouldEqual, "second aggressor parameters phase one")
+			So(experiment.Phases[0].Aggressors[1].Isolation, ShouldEqual, "second aggressor isolation phase one")
+
+			So(experiment.Phases[1].Aggressors, ShouldHaveLength, 2)
+			So(experiment.Phases[1].Aggressors[0].Name, ShouldEqual, "first aggressor phase two")
+			So(experiment.Phases[1].Aggressors[0].Parameters, ShouldEqual, "first aggressor parameters phase two")
+			So(experiment.Phases[1].Aggressors[0].Isolation, ShouldEqual, "first aggressor isolation phase two")
+			So(experiment.Phases[1].Aggressors[1].Name, ShouldEqual, "second aggressor phase two")
+			So(experiment.Phases[1].Aggressors[1].Parameters, ShouldEqual, "second aggressor parameters phase two")
+			So(experiment.Phases[1].Aggressors[1].Isolation, ShouldEqual, "second aggressor isolation phase two")
+		})
+
+		Convey("Measurements metadata should be correct", func() {
+			So(experiment.Phases[0].Measurements, ShouldHaveLength, 2)
+
+			So(experiment.Phases[0].Measurements[0].Load, ShouldEqual, 5)
+			So(experiment.Phases[0].Measurements[0].LoadPointQPS, ShouldEqual, 303)
+			So(experiment.Phases[0].Measurements[0].LGParameters, ShouldEqual, "Load generator parameters measurement 1.1")
+
+			So(experiment.Phases[0].Measurements[1].Load, ShouldEqual, 7)
+			So(experiment.Phases[0].Measurements[1].LoadPointQPS, ShouldEqual, 666)
+			So(experiment.Phases[0].Measurements[1].LGParameters, ShouldEqual, "Load generator parameters measurement 1.2")
+
+			So(experiment.Phases[1].Measurements, ShouldHaveLength, 1)
+
+			So(experiment.Phases[1].Measurements[0].Load, ShouldEqual, 1)
+			So(experiment.Phases[1].Measurements[0].LoadPointQPS, ShouldEqual, 75)
+			So(experiment.Phases[1].Measurements[0].LGParameters, ShouldEqual, "Load generator parameters measurement 2.1")
+		})
+	})
+}
+
+func prepareCassandraModel() (Experiment, []Phase, []Measurement) {
+	experiment := Experiment{
+		BaseExperiment: metadata.BaseExperiment{
+			ID:                "experiment",
+			LoadDuration:      time.Second,
+			TuningDuration:    time.Second,
+			LCName:            "launch critical task",
+			LGName:            "load generator",
+			RepetitionsNumber: 1,
+			LoadPointsNumber:  2,
+			SLO:               123,
+		},
+	}
+	phaseOne := Phase{
+		BasePhase: metadata.BasePhase{
+			ID:           "phase one",
+			LCParameters: "Latency critical parameters phase one",
+			LCIsolation:  "Latency critical isolation phase one",
+		},
+		AggressorNames:      []string{"first aggressor phase one", "second aggressor phase one"},
+		AggressorParameters: []string{"first aggressor parameters phase one", "second aggressor parameters phase one"},
+		AggressorIsolations: []string{"first aggressor isolation phase one", "second aggressor isolation phase one"},
+	}
+	phaseTwo := Phase{
+		BasePhase: metadata.BasePhase{
+			ID:           "phase two",
+			LCParameters: "Latency critical parameters phase two",
+			LCIsolation:  "Latency critical isolation phase two",
+		},
+		ExperimentID:        "experiment",
+		AggressorNames:      []string{"first aggressor phase two", "second aggressor phase two"},
+		AggressorParameters: []string{"first aggressor parameters phase two", "second aggressor parameters phase two"},
+		AggressorIsolations: []string{"first aggressor isolation phase two", "second aggressor isolation phase two"},
+	}
+	measurementOneOne := Measurement{
+		Measurement: metadata.Measurement{
+			Load:         5,
+			LoadPointQPS: 303,
+			LGParameters: "Load generator parameters measurement 1.1",
+		},
+		PhaseID:      "phase one",
+		ExperimentID: "experiment",
+	}
+	measurementOneTwo := Measurement{
+		Measurement: metadata.Measurement{
+			Load:         7,
+			LoadPointQPS: 666,
+			LGParameters: "Load generator parameters measurement 1.2",
+		},
+		PhaseID:      "phase one",
+		ExperimentID: "experiment",
+	}
+	measurementTwoOne := Measurement{
+		Measurement: metadata.Measurement{
+			Load:         1,
+			LoadPointQPS: 75,
+			LGParameters: "Load generator parameters measurement 2.1",
+		},
+		PhaseID:      "phase two",
+		ExperimentID: "experiment",
+	}
+
+	return experiment, []Phase{phaseOne, phaseTwo}, []Measurement{measurementTwoOne, measurementOneTwo, measurementOneOne}
+}
