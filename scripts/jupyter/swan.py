b393c443e0a99b88f073389d27c53369c6f3521c
Niklas Q. Nielsen <nik@qni.dk>
2016-06-29 09:54:04 -0700

    Added colors to sensitivity table

--- scripts/jupyter/swan.py
+++ scripts/jupyter/swan.py
@@ -23,6 +23,8 @@ class Experiment:
 
         self.samples = []
         self.phases = {}
+        self.sensivity_rows = {}
+        self.phase_to_aggressor = {}
 
         lookup = self.session.prepare('SELECT ns, ver, host, time, boolval, doubleval, strval, tags, valtype FROM snap.metrics WHERE tags CONTAINS ? ALLOW FILTERING')
         sample_rows = self.session.execute(lookup, [self.id])
@@ -35,10 +37,28 @@ class Experiment:
                     self.phases[sample_row.tags['swan_phase']] = []
                 self.phases[sample_row.tags['swan_phase']].append(sample)
 
-            self.samples.append(sample)
+                # Categorize for sensitivity profile.
+                if 'swan_loadpoint_qps' in sample_row.tags:
+                    phase = sample_row.tags['swan_phase']
 
-    def samples(self):
-        return self.sample_rows
+                    # Ugly hack. Phase names are repeated and specialized from Swan side.
+                    # Should be fixed when we introduce configuration unfolding.
+                    phase = phase.split('_id_')[0]
+
+                    if 'swan_aggressor_name' in sample_row.tags:
+                        self.phase_to_aggressor[phase] = sample_row.tags['swan_aggressor_name']
+
+                    load_point = int(sample_row.tags['swan_loadpoint_qps'])
+
+                    if phase not in self.sensivity_rows:
+                        self.sensivity_rows[phase] = {}
+
+                    if load_point not in self.sensivity_rows[phase]:
+                        self.sensivity_rows[phase][load_point] = {}
+
+                    self.sensivity_rows[phase][load_point][sample_row.ns] = sample
+
+            self.samples.append(sample)
 
     def show_samples(self):
         html_out = ""
@@ -59,8 +79,54 @@ class Experiment:
 
         display(HTML(html_out))
 
-    def show_sensitivity_profile(self):
-        display(HTML('<table><tr><th>Scenario / Load</th><th>5%</th><th>10%</th><th>15%</th><th>15%</th><th>20%</th></tr></table>'))
+    # SLO should be read from database.
+    def show_sensitivity_profile(self, SLO):
+        html_out = ''
+        html_out += '<table>'
+        html_out += '<tr>'
+        html_out += '<th>Scenario / Load</th>'
+
+        for load_percentage in range(5, 100, 10):
+            html_out += '<th>%s%%</th>' % load_percentage
+
+        html_out += '</tr>'
+
+        for phase in self.sensivity_rows:
+            html_out += '<tr>'
+
+            aggressor = self.phase_to_aggressor[phase]
+            if aggressor == "None":
+                label = "Baseline"
+            else:
+                label = aggressor
+
+            html_out += '<td>%s</td>' % label
+
+            # Yet another hack. We have to sort the load points from lowest to highest.
+            sorted_loadpoints = []
+            for load_points in self.sensivity_rows[phase]:
+                sorted_loadpoints.append(load_points)
+            sorted_loadpoints.sort()
+
+            for load_points in sorted_loadpoints:
+                samples = self.sensivity_rows[phase][load_points]
+                latency = samples["/intel/swan/mutilate/localhost.localdomain/percentile/99th"]
+                violation = ((latency.doubleval / SLO) * 100)
+
+                if violation > 150:
+                    color = "red"
+                elif violation > 100:
+                    color = "orange"
+                else:
+                    color = "green"
+
+                html_out += '<td style="background-color: %s;">%.1f%%</td>' % (color, violation)
+
+            html_out += '</tr>'
+
+        html_out += '</table>'
+
+        display(HTML(html_out))
 
 class Experiments:
     def __init__(self, cassandra_cluster):
