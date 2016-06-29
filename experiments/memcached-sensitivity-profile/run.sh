add76541d7ccb94e02d5abdc5817f464a71380d8
Pawel Palucki <pawel.palucki@intel.com>
2016-06-29 10:04:03 -0700

    exp: run.sh WIP

--- experiments/memcached-sensitivity-profile/run.sh
+++ experiments/memcached-sensitivity-profile/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 export SWAN_LOAD_GENERATOR_ADDR=10.5.3.9
 #export SWAN_L1D_PATH=/home/ppalucki/golang/src/github.com/intelsdi-x/swan/workloads/low-level-aggressors/l1d
 export SWAN_CASSANDRA_ADDR=10.4.3.2
@@ -8,17 +9,28 @@ export SWAN_MEMCACHED_PATH=/home/ppalucki/gopath/src/github.com/intelsdi-x/swan/
 
 export SWAN_MUTILATE_PATH=/root/sce362/mutilate
 export SWAN_MUTILATE_MASTER=10.4.3.1
-export SWAN_PEAKLOAD=1000000
-export SWAN_MUTILATE_AGENT=
+export SWAN_SLO=500
+export SWAN_LOAD_POINTS=2
+export SWAN_REPS=1
+export SWAN_PEAK_LOAD=1000000
+#export SWAN_MUTILATE_AGENT=
+export SWAN_SNAPD_ADDR=10.4.3.2
+export SWAN_SNAPD_ADDR="none"
+# go get github.com/intelsdi-x/snap-plugin-publisher-cassandra
+export SWAN_SNAP_CASSANDRA_PLUGIN_PATH=/home/ppalucki/gopath/bin/snap-plugin-publisher-cassandra
+
 
 pkill memcached
-pkill -9 mutilate
-pstree
-env | grep SWAN_
+pkill -e -9 mutilate
+pssh -P -H 10.4.3.3 -H 10.4.3.4 -H 10.4.3.5 -H 10.4.3.6 -H 10.4.3.7 -H 10.4.3.8 -H 10.4.3.1 pkill -e -9 mutilate
 
-echo ready to run ... press a key
-read
+#pstree
+#env | grep SWAN_
+#echo ready to run ... press a key
+#read
 
 echo --- go ---
-/home/ppalucki/gopath/src/github.com/intelsdi-x/swan/build/experiments/memcached/memcached-sensitivity-profile --mutilate_agent=10.4.3.3 --mutilate_agent=10.4.3.4 --mutilate_agent=10.4.3.5 --mutilate_agent=10.4.3.6 --mutilate_agent=10.4.3.7 --mutilate_agent=10.4.3.8 
+export GOPATH=/home/ppalucki/gopath
+go run /home/ppalucki/gopath/src/github.com/intelsdi-x/swan/experiments/memcached-sensitivity-profile/*.go --lg_agent=10.4.3.3 --lg_agent=10.4.3.4 --lg_agent=10.4.3.5 --lg_agent=10.4.3.6 --lg_agent=10.4.3.7 --lg_agent=10.4.3.8  --log debug
+#/home/ppalucki/gopath/src/github.com/intelsdi-x/swan/build/experiments/memcached/memcached-sensitivity-profile --lg_agent=10.4.3.3 --lg_agent=10.4.3.4 --lg_agent=10.4.3.5 --lg_agent=10.4.3.6 --lg_agent=10.4.3.7 --lg_agent=10.4.3.8 
 echo --- done ---
