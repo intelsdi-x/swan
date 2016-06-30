0da70e8fb60985f8f46852940cca374891386123
iwankgb <maciej.iwanowski@critical.today>
2016-06-30 15:45:23 +0200

    SCE-395 - saving metadata to Cassandra [2/2] (#227)
    * SCE-395 - storing metadata in Cassandra without utilizing Snap workflow
    
    * SCE-395 - missing dependencies
    
    * Apply Metadata PR changes
    
    * SCE-395 - fixing interface implementation
    
    * SCE-395 - making sure that all necessary metadata are saved
    
    * SCE-395 - getting rid of SLO, adding some new lines to increase code readability
    
    * SCE-395 - enhanced experiment metadata, method for retriving metadata from the database
    
    * SCE-395 - adding missing comments
    
    * SCE-395 - better DI, measurement metadata being saved
    
    * SCE-395 - moving common code that breakes state of the database to a function
    
    * SCE-395 - fetching measurements metadata; moving some code to separate classes
    
    * SCE-395 - general cleanup of file names and directory structure
    
    * SCE-395 - fixing all the tests, using embedded structs with gocassa
    
    * SCE-395 - some typos fixed
    
    * SCE-395 - race condition: seek and destroy; seeking phase
    
    * SCE-395 - asserting immediately
    
    * SCE-395 - renaming the function
    
    * SCE-395 - comparing data saved with data retrieved directly; adding some comments to the model; renaming helper function; renaming parsing function
    
    * SCE-395 - race condition: seek and destroy; destroying phase

---
+++ pkg/experiment/sensitivity/metadata/cassandra/from_metadata.go
@@ -0,0 +1,53 @@
+package cassandra
+
+import "github.com/intelsdi-x/swan/pkg/experiment/sensitivity/metadata"
+
+func parseFromMetadata(experiment metadata.Experiment) (Experiment, []Phase, []Measurement) {
+	experimentMetadata := buildExperimentMetadata(experiment)
+	phaseMetadata, measurementMetadata := buildPhaseMetadata(experiment)
+
+	return experimentMetadata, phaseMetadata, measurementMetadata
+
+}
+
+func buildExperimentMetadata(metadata metadata.Experiment) Experiment {
+	return Experiment{
+		BaseExperiment: metadata.BaseExperiment,
+	}
+}
+
+func buildPhaseMetadata(experiment metadata.Experiment) ([]Phase, []Measurement) {
+	var phasesMetadata []Phase
+	var measurementsMetadata []Measurement
+	for _, metadata := range experiment.Phases {
+		var names, isolations, parameters []string
+		for _, aggressor := range metadata.Aggressors {
+			names = append(names, aggressor.Name)
+			isolations = append(isolations, aggressor.Isolation)
+			parameters = append(parameters, aggressor.Parameters)
+		}
+		phasesMetadata = append(phasesMetadata, Phase{
+			BasePhase:           metadata.BasePhase,
+			ExperimentID:        experiment.ID,
+			AggressorNames:      names,
+			AggressorIsolations: isolations,
+			AggressorParameters: parameters,
+		})
+		measurementsMetadata = append(measurementsMetadata, buildMeasurementMetadata(metadata, experiment.ID)...)
+	}
+
+	return phasesMetadata, measurementsMetadata
+}
+
+func buildMeasurementMetadata(phase metadata.Phase, experiment string) []Measurement {
+	var measurementsMetadata []Measurement
+	for _, metadata := range phase.Measurements {
+		measurementsMetadata = append(measurementsMetadata, Measurement{
+			ExperimentID: experiment,
+			PhaseID:      phase.ID,
+			Measurement:  metadata,
+		})
+	}
+
+	return measurementsMetadata
+}
