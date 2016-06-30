3c04f14a5dc2c7b760d5c83f3191128039f4dec3
Bplotka <bwplotka@gmail.com>
2016-06-30 14:27:11 +0200

    SCE-500: Added Duration, File, StringList, IP flags and corresponded tests (#228)
    * SCE-500: Added Duration, File, StringList, IP flags and corresponded tests.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Used defined flags.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Addressed comments.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Addressed comment.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added tests for list_parser.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Removed unnecessary test.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- pkg/conf/conf_test.go
+++ pkg/conf/conf_test.go
@@ -1,7 +1,6 @@
 package conf
 
 import (
-	"fmt"
 	"github.com/Sirupsen/logrus"
 	"github.com/intelsdi-x/swan/pkg/utils/fs"
 	. "github.com/smartystreets/goconvey/convey"
@@ -15,12 +14,6 @@ const (
 	testAppName       = "testAppName"
 	testIPDefaultName = "127.0.0.1"
 )
-
-func TestEnvFlag(t *testing.T) {
-	Convey("While using Flag struct, it should construct proper swan environment var name", t, func() {
-		So(NewStringFlag("test_name", "", "").envName(), ShouldEqual, "SWAN_TEST_NAME")
-	})
-}
 
 func TestConf(t *testing.T) {
 	testReadmePath := path.Join(fs.GetSwanPath(), "pkg", "conf", "test_file.md")
@@ -56,84 +49,6 @@ func TestConf(t *testing.T) {
 
 			// Should be from environment.
 			So(LogLevel(), ShouldEqual, logrus.DebugLevel)
-		})
-
-		Convey("When some custom String Flag is defined", func() {
-			// Register custom flag.
-			customFlag := NewStringFlag("custom_string_arg", "help", "default")
-			customFlag.clear()
-			defer customFlag.clear()
-
-			Convey("Without parse it should be default", func() {
-				So(customFlag.Value(), ShouldEqual, "default")
-			})
-
-			Convey("When we not defined any environment variable we should have default value after parse", func() {
-				err := ParseEnv()
-				So(err, ShouldBeNil)
-				So(customFlag.Value(), ShouldEqual, customFlag.defaultValue)
-			})
-
-			Convey("When we define custom environment variable we should have custom value after parse", func() {
-				customValue := "customContent"
-				os.Setenv(customFlag.envName(), customValue)
-
-				err := ParseEnv()
-				So(err, ShouldBeNil)
-				So(customFlag.Value(), ShouldEqual, customValue)
-			})
-		})
-
-		Convey("When some custom Bool Flag is defined", func() {
-			// Register custom flag.
-			customFlag := NewBoolFlag("custom_bool_arg", "help", false)
-			customFlag.clear()
-			defer customFlag.clear()
-
-			Convey("Without parse it should be default", func() {
-				So(customFlag.Value(), ShouldEqual, false)
-			})
-
-			Convey("When we not defined any environment variable we should have default value after parse", func() {
-				err := ParseEnv()
-				So(err, ShouldBeNil)
-				So(customFlag.Value(), ShouldEqual, customFlag.defaultValue)
-			})
-
-			Convey("When we define custom environment variable we should have custom value after parse", func() {
-				customValue := true
-				os.Setenv(customFlag.envName(), fmt.Sprintf("%v", customValue))
-
-				err := ParseEnv()
-				So(err, ShouldBeNil)
-				So(customFlag.Value(), ShouldEqual, customValue)
-			})
-		})
-
-		Convey("When some custom Int Flag is defined", func() {
-			// Register custom flag.
-			customFlag := NewIntFlag("custom_int_arg", "help", 23424)
-			customFlag.clear()
-			defer customFlag.clear()
-
-			Convey("Without parse it should be default", func() {
-				So(customFlag.Value(), ShouldEqual, 23424)
-			})
-
-			Convey("When we not defined any environment variable we should have default value after parse", func() {
-				err := ParseEnv()
-				So(err, ShouldBeNil)
-				So(customFlag.Value(), ShouldEqual, customFlag.defaultValue)
-			})
-
-			Convey("When we define custom environment variable we should have custom value after parse", func() {
-				customValue := 12
-				os.Setenv(customFlag.envName(), fmt.Sprintf("%d", customValue))
-
-				err := ParseEnv()
-				So(err, ShouldBeNil)
-				So(customFlag.Value(), ShouldEqual, customValue)
-			})
 		})
 	})
 }
