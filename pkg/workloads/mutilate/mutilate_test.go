374a606e278dd6c7da557a4bd337afd854adbc73
Bplotka <bwplotka@gmail.com>
2016-06-30 08:36:30 +0200

    SCE-403: Moved latency to string from decimal. (#223)
    * Moved latency to string from decimal.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed integration test.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- pkg/workloads/mutilate/mutilate_test.go
+++ pkg/workloads/mutilate/mutilate_test.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/intelsdi-x/swan/pkg/executor"
 	"github.com/intelsdi-x/swan/pkg/executor/mocks"
-	"github.com/shopspring/decimal"
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
@@ -28,6 +27,7 @@ const (
 	masterConnectionsDepth = 1
 	keySize                = 3
 	valueSize              = 5
+	latencyPercentile      = "99.9234"
 
 	correctMutilateQPS    = 4450
 	correctMutilateSLI    = 75
@@ -71,7 +71,7 @@ func (s *MutilateTestSuite) SetupTest() {
 	s.config.PathToBinary = mutilatePath
 	s.config.MemcachedHost = memcachedHost
 	s.config.MemcachedPort = memcachedPort
-	s.config.LatencyPercentile, _ = decimal.NewFromString("99.9234")
+	s.config.LatencyPercentile = latencyPercentile
 	s.config.TuningTime = 934 * time.Second
 	s.config.WarmupTime = 1231 * time.Second
 	s.config.AgentThreads = agentThreads
