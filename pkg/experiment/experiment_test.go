bc28177f64e956f9abc759bfca6be3c6007c8aa4
Wlodzimierz Borkowski <wlodzimierz.borkowski@intel.com>
2016-07-06 16:50:28 +0200

    add additional flag for stop experiment on failure
    Experiment can be breake at the repetition level.
    Update tests as well. Add structure for experiment configuration
    and and change constructor signature.

--- pkg/experiment/experiment_test.go
+++ pkg/experiment/experiment_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/mock"
 	"os"
 	"testing"
+	"errors"
 )
 
 func TestExperiment(t *testing.T) {
@@ -15,7 +16,7 @@ func TestExperiment(t *testing.T) {
 		Convey("With proper configuration and empty phases", func() {
 			var phases []phase.Phase
 			exp, err := NewExperiment("example-experiment1", phases,
-				os.TempDir(), logrus.ErrorLevel)
+				os.TempDir(), Configuration{logrus.ErrorLevel, false, true})
 
 			Convey("Experiment should return with error", func() {
 				So(exp, ShouldBeNil)
@@ -31,15 +32,67 @@ func TestExperiment(t *testing.T) {
 
 			phases = append(phases, mockedPhase)
 
-			exp, err := NewExperiment("example-experiment", phases, os.TempDir(), logrus.ErrorLevel)
+			exp, err := NewExperiment("example-experiment", phases,
+				os.TempDir(), Configuration{logrus.ErrorLevel, false, true})
 			So(exp, ShouldNotBeNil)
 			So(err, ShouldBeNil)
 
-			Convey("While setting one repetition to phase", func() {
+			Convey("While setting ten repetition to phase", func() {
 				mockedPhase.On("Run", mock.AnythingOfType("phase.Session")).Return(nil).Times(10)
 				mockedPhase.On("Repetitions").Return(int(10))
 				mockedPhase.On("Finalize").Return(nil).Once()
 				Convey("Experiment should succeed with 10 phase repetitions", func() {
+					err := exp.Run()
+					So(err, ShouldBeNil)
+
+					mockedPhase.AssertExpectations(t)
+				})
+			})
+		})
+
+		Convey("With invalid configuration stop experiment if error", func() {
+			var phases []phase.Phase
+
+			mockedPhase := new(mocks.Phase)
+			mockedPhase.On("Name").Return("mock-phase01")
+
+			phases = append(phases, mockedPhase)
+
+			exp, err := NewExperiment("example-experiment", phases,
+				os.TempDir(), Configuration{logrus.ErrorLevel, true, true})
+			So(exp, ShouldNotBeNil)
+			So(err, ShouldBeNil)
+
+			Convey("While setting one repetition to phase", func() {
+				mockedPhase.On("Run", mock.AnythingOfType("phase.Session")).Return(errors.New("Production task can't be launched")).Once()
+				mockedPhase.On("Repetitions").Return(int(10))
+				Convey("Experiment should fail with error phase", func() {
+					err := exp.Run()
+					So(err, ShouldNotBeNil)
+
+					mockedPhase.AssertExpectations(t)
+				})
+			})
+		})
+
+		Convey("With invalid configuration and not empty phases", func() {
+			var phases []phase.Phase
+
+			mockedPhase := new(mocks.Phase)
+			mockedPhase.On("Name").Return("mock-phase01")
+
+			phases = append(phases, mockedPhase)
+
+			exp, err := NewExperiment("example-experiment", phases,
+				os.TempDir(), Configuration{logrus.ErrorLevel, false, true})
+			So(exp, ShouldNotBeNil)
+			So(err, ShouldBeNil)
+
+			Convey("While setting one repetition to phase", func() {
+				mockedPhase.On("Run", mock.AnythingOfType("phase.Session")).Return(errors.New("Production task can't be launched"))
+				mockedPhase.On("Repetitions").Return(int(10))
+				mockedPhase.On("Finalize").Return(nil)
+				Convey("Experiment should fail with error phase", func() {
 					err := exp.Run()
 					So(err, ShouldBeNil)
 
