383b4962180cb67bedf5195a28e462531e0532d9
mstachowski <michal.stachowski@outlook.com>
2016-07-08 11:53:16 +0200

    Switch to static links

--- workloads/deep_learning/caffe/caffe_deps_ubuntu.sh
+++ workloads/deep_learning/caffe/caffe_deps_ubuntu.sh
@@ -1,1 +1,10 @@
-./misc/dev/docker/workload_deps_ubuntu/caffe_deps.sh
+#!/usr/bin/env bash
+#
+# Prepares operating system for building Caffe Workload.
+# Read $SWAN_ROOT/workloads/deep_learning/caffe/README.md for details.
+set -e -o pipefail
+
+apt-get install -y libprotobuf-dev libleveldb-dev \
+    libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler \
+    libboost-all-dev libgflags-dev libgoogle-glog-dev liblmdb-dev \
+    libopenblas-base libopenblas-dev
