0e630718a1fdf0e66071cb98b87849e3983cfbe7
Bplotka <bwplotka@gmail.com>
2016-07-12 15:30:46 +0200

    SCE-504: Moved launcher interfaces to executor pkg (#264)
    It is needed since we will use these interfaces in different packages than workloads.
    
    - Moved launcher interfaces to executor.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    # Conflicts:
    #	pkg/workloads/caffe/caffe.go

---
+++ pkg/executor/load_generator.go
@@ -0,0 +1,20 @@
+package executor
+
+import (
+	"time"
+)
+
+// LoadGenerator launches stresser which generates load on specified workload.
+type LoadGenerator interface {
+	// Populate inserts initial data.
+	Populate() error
+
+	// Tune does the tuning phase which is a process of searching for a targetQPS
+	// for given SLO.
+	Tune(slo int) (achievedLoad int, achievedSLI int, err error)
+
+	// Load starts a load on the specific workload with the defined loadPoint (number of QPS).
+	// The task will do the load for specified amount of time.
+	// Note: Results from Load needs to be fetched out of band e.g using Snap.
+	Load(load int, duration time.Duration) (task TaskHandle, err error)
+}
