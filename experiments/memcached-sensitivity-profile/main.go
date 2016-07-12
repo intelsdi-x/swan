b71347ff611caf4b87618bf1f18afcd58bfb07ee
akwasniewska <alicja.kwasniewska@intel.com>
2016-07-12 05:09:10 +0200

    Testing done

--- experiments/memcached-sensitivity-profile/main.go
+++ experiments/memcached-sensitivity-profile/main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/intelsdi-x/swan/pkg/isolation"
 	"github.com/intelsdi-x/swan/pkg/snap"
 	"github.com/intelsdi-x/swan/pkg/snap/sessions"
-	"github.com/intelsdi-x/swan/pkg/utils/err_handle"
+	"github.com/intelsdi-x/swan/pkg/utils/errutil"
 	"github.com/intelsdi-x/swan/pkg/utils/fs"
 	"github.com/intelsdi-x/swan/pkg/workloads/memcached"
 	"github.com/intelsdi-x/swan/pkg/workloads/mutilate"
@@ -53,10 +53,10 @@ var (
 func newRemote(ip string) executor.Executor {
 	// TODO(bp): Have ability to choose user using parameter here.
 	user, err := user.Current()
-	err_handle.Check(err)
+	errutil.Check(err)
 
 	sshConfig, err := executor.NewSSHConfig(ip, executor.DefaultSSHPort, user)
-	err_handle.Check(err)
+	errutil.Check(err)
 
 	return executor.NewRemote(sshConfig)
 }
@@ -75,14 +75,14 @@ func prepareSnapSessionLauncher() snap.SessionLauncher {
 			"v1",
 			true,
 		)
-		err_handle.Check(err)
+		errutil.Check(err)
 
 		// Load the snap cassandra publisher plugin if not yet loaded.
 		// TODO(bp): Make helper for that.
 		logrus.Debug("Checking if publisher cassandra is loaded.")
 		plugins := snap.NewPlugins(snapConnection)
 		loaded, err := plugins.IsLoaded("publisher", "cassandra")
-		err_handle.Check(err)
+		errutil.Check(err)
 
 		if !loaded {
 			pluginPath := snapCassandraPluginPath.Value()
@@ -90,7 +90,7 @@ func prepareSnapSessionLauncher() snap.SessionLauncher {
 				logrus.Error("Cannot find snap cassandra plugin at %q", pluginPath)
 			}
 			err = plugins.Load([]string{pluginPath})
-			err_handle.Check(err)
+			errutil.Check(err)
 		}
 
 		// Define publisher.
@@ -130,7 +130,7 @@ func main() {
 It executes workloads and triggers gathering of certain metrics like latency (SLI) and the achieved number of Request per Second (QPS/RPS)`)
 
 	// Parse CLI.
-	err_handle.Check(conf.ParseFlags())
+	errutil.Check(conf.ParseFlags())
 
 	logrus.SetLevel(conf.LogLevel())
 
@@ -196,7 +196,7 @@ It executes workloads and triggers gathering of certain metrics like latency (SL
 	aggressors := []sensitivity.LauncherSessionPair{}
 	for _, aggressorName := range aggressorsFlag.Value() {
 		aggressor, err := aggressorFactory.Create(aggressorName)
-		err_handle.Check(err)
+		errutil.Check(err)
 
 		aggressors = append(aggressors, aggressor)
 	}
@@ -216,5 +216,5 @@ It executes workloads and triggers gathering of certain metrics like latency (SL
 
 	// Run Experiment.
 	err := sensitivityExperiment.Run()
-	err_handle.Check(err)
+	errutil.Check(err)
 }
