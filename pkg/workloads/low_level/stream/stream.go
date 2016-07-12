00405848f6abbf768e1528b67fd92fa97345209a
Pawel Palucki <pawel.palucki@intel.com>
2016-07-12 09:48:20 +0200

    stream aggressor thread environment variable fix

--- pkg/workloads/low_level/stream/stream.go
+++ pkg/workloads/low_level/stream/stream.go
@@ -61,7 +61,7 @@ func New(exec executor.Executor, config Config) workloads.Launcher {
 }
 
 func (l stream) buildCommand() string {
-	return fmt.Sprintf("OMP_NUM_THREADS=%d %s", l.conf.NumThreads, l.conf.Path)
+	return fmt.Sprintf("sh -c 'OMP_NUM_THREADS=%d %s'", l.conf.NumThreads, l.conf.Path)
 }
 
 // Launch starts a workload.
