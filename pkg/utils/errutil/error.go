1c38bd9cd544abd48105f674961abc05b324cae8
akwasniewska <alicja.kwasniewska@intel.com>
2016-07-13 06:13:54 +0200

    Added errutil pkg.

---
+++ pkg/utils/errutil/error.go
@@ -0,0 +1,13 @@
+package errutil
+
+import (
+	"github.com/Sirupsen/logrus"
+)
+
+// Check the supplied error, log and exit if non-nil.
+func Check(err error) {
+	if err != nil {
+		logrus.Debugf("%+v", err)
+		logrus.Fatalf("%v", err)
+	}
+}
