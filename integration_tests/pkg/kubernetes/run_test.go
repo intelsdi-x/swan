d2b853ba7b6e7501258d5371a4acf117bac355e1
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-07-13 13:41:39 +0200

    Added tmp script.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

---
+++ integration_tests/pkg/kubernetes/run_test.go
@@ -0,0 +1,49 @@
+package kubernetes
+
+import (
+	"testing"
+
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	"github.com/intelsdi-x/swan/pkg/executor"
+	"github.com/intelsdi-x/swan/pkg/kubernetes"
+	"github.com/intelsdi-x/swan/pkg/utils/fs"
+	. "github.com/smartystreets/goconvey/convey"
+	"io/ioutil"
+	"os"
+	"path"
+	"regexp"
+	"time"
+)
+
+var (
+	kubectlBinPath = path.Join(fs.GetSwanBinPath(), "kubectl")
+)
+
+func check(err error) {
+	if err != nil {
+		logrus.Debugf("%+v", err)
+		logrus.Fatalf("%v", err)
+	}
+}
+
+// Please see `pkg/kubernetes/README.md` for prerequisites for this test.
+func TestLocalKubernetesRun(t *testing.T) {
+	logrus.SetLevel(logrus.ErrorLevel)
+
+	local := executor.NewLocal()
+	k8sLauncher := kubernetes.New(local, local, kubernetes.DefaultConfig())
+	k8sHandle, err := k8sLauncher.Launch()
+	check(err)
+
+	defer func() {
+		err := k8sHandle.Stop()
+		So(err, ShouldBeNil)
+		err = k8sHandle.Clean()
+		So(err, ShouldBeNil)
+		err = k8sHandle.EraseOutput()
+		So(err, ShouldBeNil)
+	}()
+
+	k8sHandle.Wait(0 * time.Millisecond)
+}
