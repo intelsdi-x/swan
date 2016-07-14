4919ea31813267ef60ec1d8a7a5a1997e247f57b
Bplotka <bwplotka@gmail.com>
2016-07-14 16:05:53 +0200

    SCE-504: Kubernetes Launcher in Swan. (#236)
    * SCE-504: Kubernetes Launcher in Swan.
    
    - Moved launcher interfaces to exectutor.
    - Added kubernetes launcher with 5 services
    - Reusing cluster task handle.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * SCE-504: Added initial tests.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added checking if services are up in k8s launcher.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed minor errors.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Done checking if services are up.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed k8s cluster launcher, added integration test.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added proper unit test for K8s.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed typo.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed errors.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed missed log.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed lint issue.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Skipped k8s integration tests.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Updated gitignore, moved k8s binaries installation to local dir and addressed comments.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Addressed comments part2.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed error in install k8s script.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added tmp script.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed minor issues.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed minor issues v2.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed minor issues v3.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Moved to bin.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Debugging issue with not working lookup.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixing debug runner.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Removed debug file. Addressed comment.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added makefile for k8s integration test.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added chmod to install script.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added missed service.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Removed integration test since this ticket does not include it.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Explained unit tests in comment.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

---
+++ misc/kubernetes/install_binaries.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# NOTE: This is a script for manual deployment of k8s binaries. It should be replaced by Ansible
+# scripts.
+set -x
+
+K8S_VERSION="v1.3.0"
+
+pushd `dirname $0`
+    SWAN_ROOT=`pwd`/../../
+    SWAN_BIN=${SWAN_ROOT}misc/bin/
+
+    mkdir -p ${SWAN_BIN}
+
+    OPT=$1
+    if [ "${OPT}" = "--force" ] || [ ! -f  ${SWAN_BIN}.kube-services-${K8S_VERSION} ] ; then
+        pushd ${SWAN_BIN}
+            wget https://storage.googleapis.com/kubernetes-release/release/v1.3.0/bin/linux/amd64/kubectl
+            wget https://storage.googleapis.com/kubernetes-release/release/v1.3.0/bin/linux/amd64/kube-apiserver
+            wget https://storage.googleapis.com/kubernetes-release/release/v1.3.0/bin/linux/amd64/kube-controller-manager
+            wget https://storage.googleapis.com/kubernetes-release/release/v1.3.0/bin/linux/amd64/kube-proxy
+            wget https://storage.googleapis.com/kubernetes-release/release/v1.3.0/bin/linux/amd64/kube-scheduler
+            wget https://storage.googleapis.com/kubernetes-release/release/v1.3.0/bin/linux/amd64/kubelet
+            chmod +x ./*
+            touch .kube-services-${K8S_VERSION}
+        popd
+    fi
+popd
