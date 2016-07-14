c105120d09b02bbf7d36ac4424a151edfede7368
Bplotka <bwplotka@gmail.com>
2016-07-14 16:05:53 +0200

    SCE-504: Kubernetes Launcher in Swan. (#236)
    * SCE-504: Kubernetes Launcher in Swan.
    
    - Moved launcher interfaces to exectutor.
    - Added kubernetes launcher with 5 services
    - Reusing cluster task handle.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * SCE-504: Added initial tests.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added checking if services are up in k8s launcher.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed minor errors.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Done checking if services are up.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed k8s cluster launcher, added integration test.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added proper unit test for K8s.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed typo.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed errors.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed missed log.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed lint issue.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Skipped k8s integration tests.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Updated gitignore, moved k8s binaries installation to local dir and addressed comments.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Addressed comments part2.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed error in install k8s script.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added tmp script.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed minor issues.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed minor issues v2.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixed minor issues v3.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Moved to bin.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Debugging issue with not working lookup.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Fixing debug runner.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Removed debug file. Addressed comment.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added makefile for k8s integration test.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added chmod to install script.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Added missed service.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Removed integration test since this ticket does not include it.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>
    
    * Explained unit tests in comment.
    
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

---
+++ pkg/kubernetes/kubernetes_test.go
@@ -0,0 +1,171 @@
+package kubernetes
+
+import (
+	"errors"
+	"fmt"
+	"github.com/intelsdi-x/swan/pkg/executor/mocks"
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+var serviceNames = []string{
+	"kube-apiserver",
+	"kube-controller-manager",
+	"kube-scheduler",
+	"kube-proxy",
+	"kubelet",
+}
+
+type KubernetesTestSuite struct {
+	suite.Suite
+
+	mExecutor    *mocks.Executor
+	mTaskHandles []*mocks.TaskHandle
+	k8sLauncher  *kubernetes
+
+	outputFile *os.File
+
+	isListeningIteration int
+}
+
+func (s *KubernetesTestSuite) TestKubernetesLauncher() {
+	Convey("While having mocked executor", s.T(), func() {
+		s.mExecutor = new(mocks.Executor)
+		// Create taskHandles with labels corresponding to service names.
+		s.mTaskHandles = []*mocks.TaskHandle{}
+		for _ = range serviceNames {
+			mTaskHandle := new(mocks.TaskHandle)
+			// TODO(bp): Add that after https://github.com/intelsdi-x/swan/pull/263 merge.
+			//mTaskHandle.Label(serviceName)
+
+			s.mTaskHandles = append(s.mTaskHandles, mTaskHandle)
+		}
+
+		var err error
+		s.outputFile, err = ioutil.TempFile(os.TempDir(), "k8s")
+		if err != nil {
+			s.Fail(err.Error())
+			return
+		}
+		defer s.outputFile.Close()
+
+		Convey("While launching k8s cluster with default configuration", func() {
+			k8sLauncher, ok := New(s.mExecutor, s.mExecutor, DefaultConfig()).(kubernetes)
+			So(ok, ShouldBeTrue)
+
+			s.k8sLauncher = &k8sLauncher
+
+			// Start from first service (service[0]).
+			s.testServiceCasesRecursively(0)
+
+			So(s.mExecutor.AssertExpectations(s.T()), ShouldBeTrue)
+			for _, mTaskHandle := range s.mTaskHandles {
+				So(mTaskHandle.AssertExpectations(s.T()), ShouldBeTrue)
+			}
+		})
+	})
+}
+
+// testServiceCasesRecursively tests three cases which can happen for single service during
+// Launcher work:
+// a) failed `Execute` execution.
+// b) failed `IsListening` function (checking the case, where the service started but is not listening.
+// c) successful service creation
+// 		In this particular case we can move to another service, so having mocked successful case
+// 		for service 1 we can run the same function `testServiceCasesRecursively` to test service1+1.
+//
+// Since our launcher just spawn 5 services, to save LOC we test each of them using the this function.
+func (s *KubernetesTestSuite) testServiceCasesRecursively(serviceIterator int) {
+	Convey(fmt.Sprintf("When %q fails to execute, we expect error", serviceNames[serviceIterator]), func() {
+		// Mock current service's executor failure.
+		s.mExecutor.On(
+			"Execute", mock.AnythingOfType("string")).Return(nil, errors.New("executor-fail")).Once()
+
+		// Mock successful connection verifier for `iteration+1` iteration.
+		// It will succeed for current service.
+		s.isListeningIteration = 0
+		s.k8sLauncher.isListening = func(string, time.Duration) bool {
+			s.isListeningIteration++
+			return s.isListeningIteration <= (serviceIterator + 1)
+		}
+
+		k8sHandle, err := s.k8sLauncher.Launch()
+		So(k8sHandle, ShouldBeNil)
+		So(err, ShouldNotBeNil)
+		// Error should wrap the initial reason.
+		So(err.Error(), ShouldContainSubstring, serviceNames[serviceIterator])
+		So(err.Error(), ShouldContainSubstring, "Execution of service failed")
+		So(err.Error(), ShouldContainSubstring, "executor-fail")
+	})
+
+	Convey(fmt.Sprintf("When %q is not listening on the endpoint, we expect error and it's handle stopped", serviceNames[serviceIterator]), func() {
+		// Mock current service's executor success.
+		s.mExecutor.On(
+			"Execute", mock.AnythingOfType("string")).Return(s.mTaskHandles[serviceIterator], nil).Once()
+
+		s.mTaskHandles[serviceIterator].On("StderrFile").Return(s.outputFile, nil).Once()
+		s.mTaskHandles[serviceIterator].On("Address").Return(serviceNames[serviceIterator]).Once()
+		s.mTaskHandles[serviceIterator].On("Stop").Return(nil).Once()
+		s.mTaskHandles[serviceIterator].On("Clean").Return(nil).Once()
+
+		// Mock successful connection verifier for `iteration` iteration.
+		// It will fail for current service.
+		s.isListeningIteration = 0
+		s.k8sLauncher.isListening = func(string, time.Duration) bool {
+			s.isListeningIteration++
+			return s.isListeningIteration <= (serviceIterator)
+		}
+
+		k8sHandle, err := s.k8sLauncher.Launch()
+		So(k8sHandle, ShouldBeNil)
+		So(err, ShouldNotBeNil)
+		So(err.Error(), ShouldContainSubstring, serviceNames[serviceIterator])
+		So(err.Error(), ShouldContainSubstring, "Failed to connect to service on instance.")
+	})
+
+	Convey(fmt.Sprintf("When %q execute successfully", serviceNames[serviceIterator]), func() {
+		// Mock current service's executor success.
+		s.mExecutor.On(
+			"Execute", mock.AnythingOfType("string")).Return(s.mTaskHandles[serviceIterator], nil).Once()
+
+		s.mTaskHandles[serviceIterator].On("Address").Return(serviceNames[serviceIterator]).Once()
+		s.mTaskHandles[serviceIterator].On("Stop").Return(nil).Once()
+		s.mTaskHandles[serviceIterator].On("Clean").Return(nil).Once()
+
+		// Check if it is the last service.
+		if serviceIterator < len(serviceNames)-1 {
+			// kube-apiserver's Address is passed to other services so we need to add that as well.
+			s.mTaskHandles[0].On("Address").Return(serviceNames[serviceIterator]).Once()
+
+			// We did not test all of them yet. Go to another service.
+			s.testServiceCasesRecursively(serviceIterator + 1)
+		} else {
+			// It is the last service so check the launcher's successful case.
+			Convey("We expect launcher to return cluster handle and no error", func() {
+				// Mock successful connection verifier for `iteration+1` iteration.
+				// It will succeed for current service.
+				s.isListeningIteration = 0
+				s.k8sLauncher.isListening = func(string, time.Duration) bool {
+					s.isListeningIteration++
+					return s.isListeningIteration <= (serviceIterator + 1)
+				}
+
+				k8sHandle, err := s.k8sLauncher.Launch()
+				So(k8sHandle, ShouldNotBeNil)
+				So(err, ShouldBeNil)
+
+				So(k8sHandle.Stop(), ShouldBeNil)
+				So(k8sHandle.Clean(), ShouldBeNil)
+			})
+		}
+	})
+}
+
+func TestKubernetesLauncherSuite(t *testing.T) {
+	suite.Run(t, new(KubernetesTestSuite))
+}
