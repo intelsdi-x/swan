7aaba65b4e044ff474705864121ded7a03eebd23
Michał Stachowski <mstachowski@users.noreply.github.com>
2016-07-19 10:21:15 +0200

    Speed-up workloads execution (#279)

--- misc/dev/docker/inside-docker-tests.sh
+++ misc/dev/docker/inside-docker-tests.sh
@@ -12,16 +12,18 @@ if [[ ${GIT_TOKEN} != "" ]]; then
     GIT_TOKEN_ENV="-e GIT_TOKEN=${GIT_TOKEN}"
 fi
 
+cd ../../../
+
 echo "Building up docker images"
 # Skip showing output for clean output on CI
 echo "* Building up Centos based image"
-docker build -t swan_centos_tests -f Dockerfile_centos . > /dev/null
+docker build -t misc/dev/docker/swan_centos_tests -f Dockerfile_centos . > /dev/null
 echo "* Building up Ubuntu based image"
-docker build -t swan_ubuntu_tests -f Dockerfile_ubuntu . > /dev/null
+docker build -t misc/dev/docker/swan_ubuntu_tests -f Dockerfile_ubuntu . > /dev/null
 
 echo "Running up tests"
 echo "* Running Centos based image"
-docker run --privileged $GIT_TOKEN_ENV -t -v $(pwd)/../../:/swan -v /sys/fs/cgroup:/sys/fs/cgroup:rw --net=host swan_centos_tests
+docker run --privileged $GIT_TOKEN_ENV -t -v $(pwd):/swan -v /sys/fs/cgroup:/sys/fs/cgroup:rw --net=host swan_centos_tests
 echo "* Running Ubuntu based image"
-docker run --privileged $GIT_TOKEN_ENV -t -v $(pwd)/../../:/swan -v /sys/fs/cgroup:/sys/fs/cgroup:rw --net=host swan_ubuntu_tests
+docker run --privileged $GIT_TOKEN_ENV -t -v $(pwd):/swan -v /sys/fs/cgroup:/sys/fs/cgroup:rw --net=host swan_ubuntu_tests
 
