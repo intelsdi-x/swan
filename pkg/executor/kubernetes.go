4e18202df5be407033eace9049ab7a7cfc05d1f8
Pawel Palucki <pawel.palucki@intel.com>
2016-07-20 19:21:47 +0200

    WIP

--- pkg/executor/kubernetes.go
+++ pkg/executor/kubernetes.go
@@ -2,13 +2,16 @@ package executor
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"time"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/intelsdi-x/swan/pkg/isolation"
 	"github.com/pkg/errors"
 
 	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/resource"
 	"k8s.io/kubernetes/pkg/api/unversioned"
 	"k8s.io/kubernetes/pkg/client/restclient"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
@@ -16,169 +19,274 @@ import (
 	"k8s.io/kubernetes/pkg/watch"
 )
 
-// -------------------- options -------------------------
-// http://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
-type kubernetesExecutorOption func(_ *kubernetesExecutor)
+const (
+	swanKubernetesNamespace = api.NamespaceDefault
+	defaultContainerName    = "swan"
+)
 
-func CPURequest(request int) kubernetesExecutorOption {
-	return func(k *kubernetesExecutor) {
-		k.cpuRequest = request
-	}
+// -----------------------------
+// kuberentes and Pod config
+type KubernetesConfig struct {
+	PodName    string // unique pod identifier.
+	Address    string // no default must be specifed or TODO: DefaultKubenertesConfig
+	CpuRequest int64  // k8s/.../resource.DecimalSi unit
+	CpuLimit   int64  // k8s/.../resouce.DecimalSi unit
+	Decorators isolation.Decorators
 }
 
-func CPULimit(limit int) kubernetesExecutorOption {
-	return func(k *kubernetesExecutor) {
-		k.cpuLimit = limit
-	}
-}
-
-func Decorator(decorator isolation.Decorator) kubernetesExecutorOption {
-	return func(k *kubernetesExecutor) {
-		k.decorators = append(k.decorators, decorator)
-	}
-}
-
-// ----------------------- executor ------------------------
-
+// -----------------------------
+// Executor
 type kubernetesExecutor struct {
-	podName    string // unique pod identifier.
-	address    string
-	cpuRequest int
-	cpuLimit   int
-	decorators isolation.Decorators
-
+	config KubernetesConfig
 	client *client.Client
 }
 
-func NewKubernetesExectuor(address, podName string, options ...kubernetesExecutorOption) (*kubernetesExecutor, error) {
+func NewKubernetesExectuor(config KubernetesConfig) (*kubernetesExecutor, error) {
 
 	// new
-	k8s := &kubernetesExecutor{address: address, podName: podName}
-
-	// apply options
-	for _, option := range options {
-		option(k8s)
+	k8s := &kubernetesExecutor{
+		config: config,
 	}
 
-	config := &restclient.Config{
-		Host: address,
+	restClientConfig := &restclient.Config{
+		Host: config.Address,
 		// Username: "test", // TODO authorization
 		// Password: "password",
 	}
 	var err error
-	k8s.client, err = client.New(config)
+	k8s.client, err = client.New(restClientConfig)
 	if err != nil {
-		return nil, errors.Wrapf(err, "can't initilize kubernetes Client for host = %q", address)
+		return nil, errors.Wrapf(err, "can't initilize kubernetes Client for host = %q", config.Address)
 	}
 
 	return k8s, nil
 }
 
-func (k8s *kubernetesExecutor) Execute(command string) *kubernetesTaskHandle {
+// prepareContainerResources helper to create ResourceRequirments for container.
+func prepareContainerResources(CPULimit, CPURequest int64) api.ResourceRequirements {
+	resourceListLimits := api.ResourceList{}
+	resourceListRequests := api.ResourceList{}
+	if CPULimit > 0 {
+		resourceListRequests[api.ResourceCPU] = *resource.NewQuantity(CPULimit, resource.DecimalSI)
+	}
+	if CPURequest > 0 {
+		resourceListRequests[api.ResourceCPU] = *resource.NewQuantity(CPURequest, resource.DecimalSI)
+	}
+	return api.ResourceRequirements{
+		Limits:   resourceListLimits,
+		Requests: resourceListRequests,
+	}
 
-	// TODO: do we want own namespace ?
-	ns := api.NamespaceDefault
+}
 
-	podsapi := k8s.client.Pods(ns)
+func (k8s *kubernetesExecutor) Execute(command string) (*kubernetesTaskHandle, error) {
 
-	pod, err := podsapi.Create(&api.Pod{
+	podsAPI := k8s.client.Pods(swanKubernetesNamespace)
+
+	for _, decorator := range k8s.config.Decorators {
+		command = decorator.Decorate(command)
+	}
+
+	// Definition of Pod http://kubernetes.io/docs/api-reference/v1/definitions/.
+	pod, err := podsAPI.Create(&api.Pod{
 		TypeMeta: unversioned.TypeMeta{},
 		ObjectMeta: api.ObjectMeta{
-			Name:      k8s.podName,
-			Namespace: ns,
-			Labels:    map[string]string{"name": k8s.podName},
+			Name:      k8s.config.PodName,
+			Namespace: swanKubernetesNamespace,
+			Labels:    map[string]string{"name": k8s.config.PodName},
 		},
-
 		Spec: api.PodSpec{
 			Containers: []api.Container{
 				api.Container{
-					Name:    k8s.podName,
-					Image:   "jess/stress", // replace with image of swan
-					Command: []string{"sh", "-c", command},
+					Name:      defaultContainerName,
+					Image:     "jess/stress", // replace with image of swan
+					Command:   []string{"sh", "-c", command},
+					Resources: prepareContainerResources(k8s.config.CpuLimit, k8s.config.CpuRequest),
 				},
 			},
 		},
 	})
 
 	if err != nil {
-		panic(fmt.Sprintf("can't schedule pod %q ns=%q: %s", k8s.podName, ns, err))
+		return nil, errors.Wrapf(err, "cannot schedule pod %q ns=%q: %s", k8s.config.PodName, swanKubernetesNamespace, err)
 	}
 
-	// Wait.
 	// req, err := labels.NewRequirement("name", labels.EqualsOperator, sets.NewString("jebac"))
 	// selector := labels.NewSelector()
 	// selector.Add(*req)
 	//// for some reasons it doesn't work
 
-	selector, err := labels.Parse(fmt.Sprintf("name=%s", k8s.podName))
+	selectorRaw := fmt.Sprintf("name=%s", k8s.config.PodName)
+	selector, err := labels.Parse(selectorRaw)
 	if err != nil {
-		panic(err)
+		return nil, errors.Wrapf(err, "cannot create an selector err:%s", selectorRaw)
 	}
-	watch, err := podsapi.Watch(api.ListOptions{LabelSelector: selector})
+
+	// Prepare events watcher.
+	watcher, err := podsAPI.Watch(api.ListOptions{LabelSelector: selector})
 	if err != nil {
-		panic(err)
+		return nil, errors.Wrapf(err, "cannot create a watcher over selector: %v", selector)
 	}
 
-	for event := range watch.ResultChan() {
-		// fmt.Printf("event = %+v\n", event)
-		// log.Println(event.Object)
+	// One goroutine to watch over events.
+	started, stopped := startWatching(watcher)
+	<-started
 
-		pod, ok := event.Object.(*api.Pod)
-		if ok {
-			if pod.Status.Phase == api.PodRunning {
-				break
-			}
-		}
+	logsRequest := podsAPI.GetLogs(k8s.config.PodName, &api.PodLogOptions{
+		Container: defaultContainerName,
+	})
+	logStream, err := logsRequest.Stream()
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot create a stream to get logsRequest selector: %+v", logsRequest)
 	}
 
-	// start a gorutines that copies logs from kuberntes to local file
+	// TODO: move that to constructor
+	th := &kubernetesTaskHandle{
+		podsAPI: podsAPI,
+		pod:     pod,
+		stopped: stopped}
+
+	// Prepare local files
+	stdoutFile, stderrFile, err := createExecutorOutputFiles(command, "local")
+	if err != nil {
+		return nil, errors.Wrapf(err, "createExecutorOutputFiles for command %q failed", command)
+	}
+	log.Debug("Created temporary files ",
+		"stdout path:  ", stdoutFile.Name(), ", stderr path:  ", stderrFile.Name())
+
+	th.stdout = stdoutFile
+	th.stderr = stderrFile
+	th.errCh = make(chan error)
+
+	// a gourtine that copies data from streamer to local files.
 	go func() {
-		podsapi.GetLogs(k8s.podName, nil)
-
+		mw := io.MultiWriter(stdoutFile, stderrFile)
+		_, err := io.Copy(mw, logStream)
+		if err != nil {
+			th.errCh <- err
+		}
 	}()
 
-	return &kubernetesTaskHandle{pod, watch}
+	return th, nil
 }
 
-// --------------------  task handle -------------------------
+// ------------------------------------------------------
+// TaskHandle
 type kubernetesTaskHandle struct {
-	pod   *api.Pod
-	watch watch.Interface // TODO: use that to spot our PodTerminated (in background gorountien)
+	podsAPI client.PodInterface
+	pod     *api.Pod
+	stopped chan struct{} // channel that is closed when Pod finished execution.
+	stdout  *os.File
+	stderr  *os.File
+	errCh   chan error
 }
 
-func (k8s *kubernetesTaskHandle) Stop() error {
+func (th *kubernetesTaskHandle) isTerminated() bool {
+	select {
+	case <-th.stopped:
+		return true
+	default:
+		return false
+	}
+}
+
+func (th *kubernetesTaskHandle) Stop() error {
+	if th.isTerminated() {
+		return nil
+	}
+
+	var GracePeriodSeconds int64 = 0 // need an address
+
+	log.Debugf("delete pod...")
+	err := th.podsAPI.Delete(th.pod.Name, &api.DeleteOptions{
+		GracePeriodSeconds: &GracePeriodSeconds,
+	})
+	if err != nil {
+		return errors.Wrapf(err, "cannot delete pod %q", th.pod.Name)
+	}
+	<-th.stopped
+	return nil
+}
+
+func (th *kubernetesTaskHandle) Status() TaskState {
 	panic("not implemented")
 }
 
-func (k8s *kubernetesTaskHandle) Status() TaskState {
+func (th *kubernetesTaskHandle) ExitCode() (int, error) {
 	panic("not implemented")
 }
 
-func (k8s *kubernetesTaskHandle) ExitCode() (int, error) {
+func (th *kubernetesTaskHandle) Wait(timeout time.Duration) bool {
+	if th.isTerminated() {
+		return true
+	}
+
+	var timeoutChannel <-chan time.Time
+	if timeout != 0 {
+		// In case of wait with timeout set the timeout channel.
+		timeoutChannel = time.After(timeout)
+	}
+	select {
+	case <-th.stopped:
+		return true
+	case <-timeoutChannel:
+		return false
+	}
+}
+
+func (th *kubernetesTaskHandle) Clean() error {
 	panic("not implemented")
 }
 
-func (k8s *kubernetesTaskHandle) Wait(timeout time.Duration) bool {
+func (th *kubernetesTaskHandle) EraseOutput() error {
 	panic("not implemented")
 }
 
-func (k8s *kubernetesTaskHandle) Clean() error {
+func (th *kubernetesTaskHandle) Address() string {
 	panic("not implemented")
 }
 
-func (k8s *kubernetesTaskHandle) EraseOutput() error {
+// ---------------------
+// task info
+
+// StdoutFile return local file from stream
+func (th *kubernetesTaskHandle) StdoutFile() (*os.File, error) {
 	panic("not implemented")
 }
 
-func (k8s *kubernetesTaskHandle) Address() string {
+func (th *kubernetesTaskHandle) StderrFile() (*os.File, error) {
 	panic("not implemented")
 }
 
-// --------------------- task info ----------------------------------
-func (k8s *kubernetesTaskHandle) StdoutFile() (*os.File, error) {
-	panic("not implemented")
-}
+// ---------------------
+// status watcher
+func startWatching(watcher watch.Interface) (started, stopped chan struct{}) {
+	started, stopped = make(chan struct{}), make(chan struct{})
 
-func (k8s *kubernetesTaskHandle) StderrFile() (*os.File, error) {
-	panic("not implemented")
+	go func() {
+		var startedAlreadyClosed bool //guard to not close "started" channel twice.
+		for event := range watcher.ResultChan() {
+			pod, ok := event.Object.(*api.Pod)
+			// http://kubernetes.io/docs/user-guide/pod-states/
+			if ok {
+				log.Debugf("pod event: %v", pod)
+				switch pod.Status.Phase {
+				case api.PodPending:
+					log.Debugf("pod %q is pending", pod.Name)
+				case api.PodRunning:
+					log.Debugf("pod %q running (closed=%v)", pod.Name, startedAlreadyClosed)
+					if !startedAlreadyClosed { // we get two events about running, but I can close channel only once.
+						close(started)
+					} else {
+						log.Fatal("WTF?")
+					}
+				case api.PodFailed, api.PodSucceeded, api.PodUnknown:
+					log.Debugf("pod %q stopped: with status: %+v", pod.Name, pod.Status)
+					close(stopped)
+					return
+				}
+			}
+		}
+	}()
+	return started, stopped
 }
--- pkg/executor/kubernetes_test.go
+++ pkg/executor/kubernetes_test.go
@@ -4,23 +4,27 @@ cd kubernetes
 # https://github.com/kubernetes/kubernetes/blob/master/docs/devel/running-locally.md
 
 j kubernetes
-sudo -s
-export GOPATH="/home/ppalucki/work/gopath"
+sudo -s GOPATH=$GOPATH PATH=$PATH
 export PATH=${PATH}:/home/ppalucki/work/gopath/src/k8s.io/kubernetes/third_party/etcd
-hack/local-up-cluster.sh
 hack/local-up-cluster.sh -o _output/bin
 
 go test -run K8s -v github.com/intelsdi-x/swan/pkg/executor
 
 cluster/kubectl.sh get pod stress1
 cluster/kubectl.sh delete pod stress1
+cluster/kubectl.sh delete pods --all
+cluster/kubectl.sh get pod stress1
 */
 package executor_test
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"testing"
+	"time"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/intelsdi-x/swan/pkg/executor"
 )
 
@@ -31,29 +35,42 @@ func check(err error) {
 }
 
 func TestK8s(t *testing.T) {
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetLevel(log.DebugLevel)
 
-	println("BLE !!!")
-	fmt.Println("client....")
-
-	k8s, err := executor.NewKubernetesExectuor("http://127.0.0.1:8080", "stress1")
+	fmt.Println("new executor")
+	k8s, err := executor.NewKubernetesExectuor(
+		executor.KubernetesConfig{
+			Address: "http://127.0.0.1:8080",
+			PodName: "stress1",
+		},
+	)
 	check(err)
-	println("BLE !!!")
+	log.Printf("k8s = %+v\n", k8s)
 
-	fmt.Printf("k8s = %+v\n", k8s)
+	log.Println("exectute...")
+	th, err := k8s.Execute("stress -c 1 -t 15")
+	check(err)
+	log.Printf("th = %+v\n", th)
 
-	fmt.Println("schedule a pod....")
-	th := k8s.Execute("stress -c 1")
-	println("BLE !!!")
+	log.Println("wait...")
 
-	fmt.Println("scheduled")
-	fmt.Printf("th = %+v\n", th)
+	log.Println("wait result:", th.Wait(5*time.Second))
 
-	th.Wait(0)
-	// play with
-	/*
-		./cluster/kubectl.sh delete pods --all
-		./cluster/kubectl.sh get pod stress1
-	*/
-	th.Stop()
+	err = th.Stop() // delete pod
+	check(err)
 
+	// just helper function
+	pr := func(r io.Reader, err error) string {
+		check(err)
+		data, err := ioutil.ReadAll(r)
+		check(err)
+		return string(data)
+	}
+
+	log.Println("stderr: ", pr(th.StderrFile()))
+	log.Println("stdout: ", pr(th.StdoutFile()))
+
+	// missing
+	th.ExitCode()
 }
