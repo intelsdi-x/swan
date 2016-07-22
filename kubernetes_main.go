7052f066d2f944306329a4c08892343f66368c3c
Niklas Q. Nielsen <nik@qni.dk>
2016-07-21 17:27:20 -0700

    Snapshot of pod status work

--- kubernetes_main.go
+++ kubernetes_main.go
@@ -1,25 +1,8 @@
-/* manuall run
+// THIS FILE WILL BE DELETED.
 
-cd kubernetes
-# https://github.com/kubernetes/kubernetes/blob/master/docs/devel/running-locally.md
-
-j kubernetes
-sudo -s GOPATH=$GOPATH PATH=$PATH
-export PATH=${PATH}:/home/ppalucki/work/gopath/src/k8s.io/kubernetes/third_party/etcd
-hack/local-up-cluster.sh -o _output/bin
-
-go test -run K8s -v github.com/intelsdi-x/swan/pkg/executor
-
-cluster/kubectl.sh get pod stress1
-cluster/kubectl.sh delete pod stress1
-cluster/kubectl.sh delete pods --all
-cluster/kubectl.sh get pod stress1
-*/
 package main
 
 import (
-	"io"
-	"io/ioutil"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -36,25 +19,21 @@ func main() {
 	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
 	log.SetLevel(log.DebugLevel)
 
-	log.Println("new executor")
-	k8s, err := executor.NewKubernetesExectuor(
+	k8s, err := executor.NewKubernetesExecutor(
 		executor.KubernetesConfig{
 			Address: "http://127.0.0.1:8080",
 			PodName: "stress1",
 		},
 	)
 	check(err)
-	log.Printf("k8s = %+v\n", k8s)
 
 	log.Println("execute...")
-	th, err := k8s.Execute("stress -c 1")
-	// th, err := k8s.Execute("stress -c 1 -t 10")
+	th, err := k8s.Execute("sleep 2 && exit 1")
 	check(err)
 
-	log.Printf("th = %+v\n", th)
 	log.Printf("th.Status() = %v\n", th.Status())
 
-	log.Println("wait for 2 seconds result:", th.Wait(2*time.Second))
+	log.Println("wait for 10 seconds result:", th.Wait(10*time.Second))
 
 	log.Println("stop ...")
 	// wait()
@@ -64,18 +43,8 @@ func main() {
 
 	log.Println("hostip:", th.Address())
 
-	log.Println("outputs")
-	// just helper function
-	pr := func(r io.Reader, err error) string {
-		check(err)
-		data, err := ioutil.ReadAll(r)
-		check(err)
-		return string(data)
-	}
-
-	log.Println("stderr: ", pr(th.StderrFile()))
-	log.Println("stdout: ", pr(th.StdoutFile()))
 
 	// missing
-	// th.ExitCode()
+	code, _ := th.ExitCode()
+	log.Printf("exit code: %d", code)
 }
--- pkg/executor/kubernetes.go
+++ pkg/executor/kubernetes.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"os"
 	"path"
-	"sync"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -26,26 +25,23 @@ const (
 	defaultContainerName    = "swan"
 )
 
-// -----------------------------
-// kuberentes and Pod config
+// KubernetesConfig describes the necessary information to connect to a Kubernetes cluster.
 type KubernetesConfig struct {
 	PodName    string // unique pod identifier.
 	Address    string // no default must be specifed or TODO: DefaultKubenertesConfig
-	CpuRequest int64  // k8s/.../resource.DecimalSi unit
-	CpuLimit   int64  // k8s/.../resouce.DecimalSi unit
+	CPURequest int64  // k8s/.../resource.DecimalSi unit
+	CPULimit   int64  // k8s/.../resouce.DecimalSi unit
 	Decorators isolation.Decorators
 }
 
-// -----------------------------
-// Executor
 type kubernetesExecutor struct {
 	config KubernetesConfig
 	client *client.Client
 }
 
-func NewKubernetesExectuor(config KubernetesConfig) (*kubernetesExecutor, error) {
-
-	// new
+// NewkubernetesExecutor returns an executor which lets the user run commands in pods in a
+// kubernetes cluster.
+func NewKubernetesExecutor(config KubernetesConfig) (Executor, error) {
 	k8s := &kubernetesExecutor{
 		config: config,
 	}
@@ -78,18 +74,18 @@ func prepareContainerResources(CPULimit, CPURequest int64) api.ResourceRequireme
 		Limits:   resourceListLimits,
 		Requests: resourceListRequests,
 	}
-
 }
 
-func (k8s *kubernetesExecutor) Execute(command string) (*kubernetesTaskHandle, error) {
-
+// Execute creates a pod and runs the provided command in it. When the command completes, the pod
+// is stopped i.e. the container is not restarted automatically.
+func (k8s *kubernetesExecutor) Execute(command string) (TaskHandle, error) {
 	podsAPI := k8s.client.Pods(swanKubernetesNamespace)
 
 	for _, decorator := range k8s.config.Decorators {
 		command = decorator.Decorate(command)
 	}
 
-	// Definition of Pod http://kubernetes.io/docs/api-reference/v1/definitions/.
+	// See http://kubernetes.io/docs/api-reference/v1/definitions/ for definition of the pod manifest.
 	pod, err := podsAPI.Create(&api.Pod{
 		TypeMeta: unversioned.TypeMeta{},
 		ObjectMeta: api.ObjectMeta{
@@ -98,12 +94,13 @@ func (k8s *kubernetesExecutor) Execute(command string) (*kubernetesTaskHandle, e
 			Labels:    map[string]string{"name": k8s.config.PodName},
 		},
 		Spec: api.PodSpec{
+			RestartPolicy: "Never",
 			Containers: []api.Container{
 				api.Container{
 					Name:      defaultContainerName,
 					Image:     "jess/stress", // replace with image of swan
 					Command:   []string{"sh", "-c", command},
-					Resources: prepareContainerResources(k8s.config.CpuLimit, k8s.config.CpuRequest),
+					Resources: prepareContainerResources(k8s.config.CPULimit, k8s.config.CPURequest),
 				},
 			},
 		},
@@ -113,54 +110,135 @@ func (k8s *kubernetesExecutor) Execute(command string) (*kubernetesTaskHandle, e
 		return nil, errors.Wrapf(err, "cannot schedule pod %q ns=%q: %s", k8s.config.PodName, swanKubernetesNamespace, err)
 	}
 
-	// req, err := labels.NewRequirement("name", labels.EqualsOperator, sets.NewString("jebac"))
-	// selector := labels.NewSelector()
-	// selector.Add(*req)
-	//// for some reasons it doesn't work
+	th := newKubernetesTaskHandle(command, podsAPI, pod)
 
-	selectorRaw := fmt.Sprintf("name=%s", k8s.config.PodName)
+	// NOTE: We should have timeout for the amount of time we want to wait for the pod to appear.
+	<-th.started
+
+	th.setupLogs()
+
+	return th, nil
+}
+
+// kubernetesTaskHandle implements the TaskHandle interface
+type kubernetesTaskHandle struct {
+	podsAPI  client.PodInterface
+	pod      *api.Pod
+	command  string
+	stopped  chan struct{}
+	started  chan struct{}
+	stdout   *os.File
+	stderr   *os.File
+	errCh    chan error
+	exitCode *int
+}
+
+func newKubernetesTaskHandle(command string, podsAPI client.PodInterface, pod *api.Pod) *kubernetesTaskHandle {
+	th := &kubernetesTaskHandle{
+		command: command,
+		podsAPI: podsAPI,
+		pod:     pod,
+	}
+	th.watch()
+	return th
+}
+
+func (th *kubernetesTaskHandle) watch() error {
+	selectorRaw := fmt.Sprintf("name=%s", th.pod.Name)
 	selector, err := labels.Parse(selectorRaw)
 	if err != nil {
-		return nil, errors.Wrapf(err, "cannot create an selector err:%s", selectorRaw)
+		return errors.Wrapf(err, "cannot create an selector err:%s", selectorRaw)
 	}
 
 	// Prepare events watcher.
-	watcher, err := podsAPI.Watch(api.ListOptions{LabelSelector: selector})
+	watcher, err := th.podsAPI.Watch(api.ListOptions{LabelSelector: selector})
 	if err != nil {
-		return nil, errors.Wrapf(err, "cannot create a watcher over selector: %v", selector)
+		return errors.Wrapf(err, "cannot create a watcher over selector: %v", selector)
 	}
 
-	// One goroutine to watch over events.
-	started, stopped := startWatching(watcher)
-	<-started
+	th.started = make(chan struct{})
+	th.stopped = make(chan struct{})
 
-	logsRequest := podsAPI.GetLogs(k8s.config.PodName, &api.PodLogOptions{
+	go func() {
+		// var once sync.Once
+		for event := range watcher.ResultChan() {
+			pod, ok := event.Object.(*api.Pod)
+			if ok {
+				// Update with latest status.
+				th.pod = pod
+
+				switch event.Type {
+				case watch.Added:
+					// NOTE: Replicate switch statement below.
+
+				case watch.Modified:
+					switch pod.Status.Phase {
+					case api.PodPending:
+						log.Debugf("modified event: '%s' in PodPending phase", pod.Name)
+					case api.PodFailed:
+						log.Debugf("modified event: '%s' in PodFailed phase", pod.Name)
+						exitCode := int(1)
+						th.exitCode = &exitCode
+						close(th.stopped)
+
+						// Try to delete the failed pod to avoid conflicts and having to call Stop()
+						// after the stopped channel has been closed.
+						var GracePeriodSeconds int64
+						th.podsAPI.Delete(th.pod.Name, &api.DeleteOptions{
+							GracePeriodSeconds: &GracePeriodSeconds,
+						})
+					case api.PodSucceeded:
+						log.Debugf("modified event: '%s' in PodSucceeded phase", pod.Name)
+						exitCode := int(0)
+						th.exitCode = &exitCode
+						close(th.stopped)
+
+					case api.PodRunning:
+						log.Debugf("modified event: '%s' in PodRunning phase", pod.Name)
+
+						if api.IsPodReady(pod) {
+							close(th.started)
+						}
+					default:
+						log.Debugf("unknown pod.Status.Phase '%d' for pod '%s'", pod.Status.Phase, pod.Name)
+					}
+				case watch.Deleted:
+					log.Debugf("pod '%s' deleted", pod.Name)
+					close(th.stopped)
+				default:
+					log.Debugf("unknown event.Type")
+				}
+			}
+		}
+	}()
+
+	return nil
+}
+
+// NOTE: That setupLogs can only be called when the pod is running i.e. wait until the started
+// channel has been closed by watch().
+func (th *kubernetesTaskHandle) setupLogs() error {
+	// Wire up logs to task handle stdout.
+	logsRequest := th.podsAPI.GetLogs(th.pod.Name, &api.PodLogOptions{
 		Container: defaultContainerName,
 	})
 	logStream, err := logsRequest.Stream()
 	if err != nil {
-		return nil, errors.Wrapf(err, "cannot create a stream to get logsRequest selector: %+v", logsRequest)
+		return errors.Wrapf(err, "cannot create a stream to get logsRequest selector: %+v", logsRequest)
 	}
-
-	// TODO: move that to constructor
-	th := &kubernetesTaskHandle{
-		podsAPI: podsAPI,
-		pod:     pod,
-		stopped: stopped}
 
 	// Prepare local files
-	stdoutFile, stderrFile, err := createExecutorOutputFiles(command, "local")
+	stdoutFile, stderrFile, err := createExecutorOutputFiles(th.command, "local")
 	if err != nil {
-		return nil, errors.Wrapf(err, "createExecutorOutputFiles for command %q failed", command)
+		return errors.Wrapf(err, "createExecutorOutputFiles for pod '%s' failed", th.pod.Name)
 	}
-	log.Debug("Created temporary files ",
-		"stdout path:  ", stdoutFile.Name(), ", stderr path:  ", stderrFile.Name())
+	log.Debug("created temporary files stdout path:  ", stdoutFile.Name(), ", stderr path:  ", stderrFile.Name())
 
 	th.stdout = stdoutFile
 	th.stderr = stderrFile
 	th.errCh = make(chan error)
 
-	// a gourtine that copies data from streamer to local files.
+	// a goroutine that copies data from streamer to local files.
 	go func() {
 		mw := io.MultiWriter(stdoutFile, stderrFile)
 		_, err := io.Copy(mw, logStream)
@@ -169,18 +247,7 @@ func (k8s *kubernetesExecutor) Execute(command string) (*kubernetesTaskHandle, e
 		}
 	}()
 
-	return th, nil
-}
-
-// ------------------------------------------------------
-// TaskHandle
-type kubernetesTaskHandle struct {
-	podsAPI client.PodInterface
-	pod     *api.Pod
-	stopped chan struct{} // channel that is closed when Pod finished execution.
-	stdout  *os.File
-	stderr  *os.File
-	errCh   chan error
+	return nil
 }
 
 func (th *kubernetesTaskHandle) isTerminated() bool {
@@ -192,36 +259,53 @@ func (th *kubernetesTaskHandle) isTerminated() bool {
 	}
 }
 
+// Stop will delete the pod and block caller until done.
 func (th *kubernetesTaskHandle) Stop() error {
 	if th.isTerminated() {
 		return nil
 	}
 
-	var GracePeriodSeconds int64 = 0 // need an address
+	log.Debugf("deleting pod '%s'", th.pod.Name)
 
-	log.Debugf("delete pod...")
+	var GracePeriodSeconds int64
 	err := th.podsAPI.Delete(th.pod.Name, &api.DeleteOptions{
 		GracePeriodSeconds: &GracePeriodSeconds,
 	})
 	if err != nil {
-		return errors.Wrapf(err, "cannot delete pod %q", th.pod.Name)
+		return errors.Wrapf(err, "cannot delete pod %s", th.pod.Name)
 	}
+
+	log.Debugf("waiting for pod '%s' to stop", th.pod.Name)
 	<-th.stopped
+	log.Debugf("pod '%s' stopped", th.pod.Name)
+
 	return nil
 }
 
+// Status returns the current task state in terms of RUNNING or TERMINATED.
 func (th *kubernetesTaskHandle) Status() TaskState {
 	if th.isTerminated() {
 		return TERMINATED
-	} else {
-		return RUNNING
 	}
+
+	return RUNNING
 }
 
+// ExitCode returns the exit code of the container running in the pod.
 func (th *kubernetesTaskHandle) ExitCode() (int, error) {
-	panic("not implemented")
+	if !th.isTerminated() {
+		return 0, errors.New("task is still running")
+	}
+
+	if th.exitCode == nil {
+		return 0, errors.New("exit code unknown")
+	}
+
+	return *th.exitCode, nil
 }
 
+// Wait blocks until the pod terminates _or_ if timeout is provided, will exit ealier with
+// false if the pod didn't terminate before the provided timeout.
 func (th *kubernetesTaskHandle) Wait(timeout time.Duration) bool {
 	if th.isTerminated() {
 		return true
@@ -240,6 +324,7 @@ func (th *kubernetesTaskHandle) Wait(timeout time.Duration) bool {
 	}
 }
 
+// Clean closes file descriptors but leaves stdout and stderr files intact.
 func (th *kubernetesTaskHandle) Clean() error {
 	for _, f := range []*os.File{th.stderr, th.stdout} {
 		if err := f.Close(); err != nil {
@@ -249,6 +334,7 @@ func (th *kubernetesTaskHandle) Clean() error {
 	return nil
 }
 
+// EraseOutput deletes the stdout and stderr files.
 func (th *kubernetesTaskHandle) EraseOutput() error {
 	outputDir, _ := path.Split(th.stderr.Name())
 	if err := os.RemoveAll(outputDir); err != nil {
@@ -257,48 +343,18 @@ func (th *kubernetesTaskHandle) EraseOutput() error {
 	return nil
 }
 
+// Address returns the host IP where the pod was scheduled.
 func (th *kubernetesTaskHandle) Address() string {
+	// NOTE: Could be th.pod.Status.PodIP as well.
 	return th.pod.Status.HostIP
 }
 
-// ---------------------
-// task info
-
-// StdoutFile return local file from stream
+// StdoutFile returns a file handle to the stdout file for the pod.
 func (th *kubernetesTaskHandle) StdoutFile() (*os.File, error) {
 	return th.stdout, nil
 }
 
+// StderrFile returns a file handle to the stderr file for the pod.
 func (th *kubernetesTaskHandle) StderrFile() (*os.File, error) {
 	return th.stderr, nil
-}
-
-// ---------------------
-// status watcher
-func startWatching(watcher watch.Interface) (started, stopped chan struct{}) {
-	started, stopped = make(chan struct{}), make(chan struct{})
-
-	go func() {
-		var once sync.Once
-		for event := range watcher.ResultChan() {
-			log.Debugf("event: %s", event.Type)
-
-			pod, ok := event.Object.(*api.Pod)
-			// http://kubernetes.io/docs/user-guide/pod-states/
-			if ok {
-				log.Debugf("pod event: %v '%s'", pod, pod.Status.Phase)
-				switch event.Type {
-				case watch.Added:
-				case watch.Modified:
-					if api.IsPodReady(pod) {
-						once.Do((func() { close(started) }))
-					}
-
-				case watch.Deleted:
-					close(stopped)
-				}
-			}
-		}
-	}()
-	return started, stopped
 }
