6e52aea61444db6072a306cb76a9adad37b4116f
Pawel Palucki <ppalucki@users.noreply.github.com>
2016-07-22 16:28:59 +0200

    SCE-566 validate cpupower: check existence of cpufreq system before validation (#286)
    * validate cpupower: check existence of cpufreq system before validation

--- experiments/memcached-sensitivity-profile/validate.go
+++ experiments/memcached-sensitivity-profile/validate.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"runtime"
 	"strconv"
 	"strings"
@@ -37,14 +38,19 @@ func checkTCPSyncookies() {
 // governor path: https://www.kernel.org/doc/Documentation/cpu-freq/user-guide.txt
 // performance,powersave constants: http://lxr.free-electrons.com/source/drivers/cpufreq/cpufreq.c#L484
 func checkCPUPowerGovernor() {
-	const PERFORMANCE = "performance"
+	const cpu0GovernorFile = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor" // Assume at least one CPU exists!.
+	if _, err := os.Stat(cpu0GovernorFile); os.IsNotExist(err) {
+		logrus.Warnf("Validation of CPU power governor failed! - %q not available (check `dmesg | grep acpi_cpufreq` entry for hardware support).", cpu0GovernorFile)
+		return
+	}
+	const performance = "performance"
 	for i := 0; i < runtime.NumCPU(); i++ {
 		cpuGovernorFile := fmt.Sprintf("/sys/devices/system/cpu/cpu%d/cpufreq/scaling_governor", i)
 		governorBytes, err := ioutil.ReadFile(cpuGovernorFile)
 		governor := strings.TrimSuffix(string(governorBytes), "\n")
 		errutil.Check(err)
 		logrus.Debugf("governor cpu%d: %q", i, governor)
-		if string(governor) != PERFORMANCE {
+		if string(governor) != performance {
 			logrus.Warnf("scaling_governor=%q (%q) should be set to 'performance' policy to mitigate wakeup penalty (causes variability in measurements at moderate load). You can change this value with 'cpupower frequency-set -g performance'as root.", governor, cpuGovernorFile)
 		}
 	}
