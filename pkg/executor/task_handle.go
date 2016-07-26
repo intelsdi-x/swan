6103783eadb49dac37d7b2182c1c9da5ed1ce6e9
Szymon Konefal <szymon.konefal@intel.com>
2016-07-26 11:50:06 +0200

    Decomposed TaskHandle interface

--- pkg/executor/task_handle.go
+++ pkg/executor/task_handle.go
@@ -15,24 +15,30 @@ const (
 	TERMINATED
 )
 
+// TaskHandle represents a process which can be stopped or monitored.
+type TaskHandle interface {
+	TaskInfo
+	TaskControl
+}
+
 // TaskInfo represents process from which information like Stdout can be retrieved.
 // NOTE: In future it can include other info like `Address`.
 type TaskInfo interface {
+	// Location returns address where task was located.
+	Address() string
+	// ExitCode returns a exitCode. If task is not terminated it returns error.
+	ExitCode() (int, error)
+	// Status returns a state of the task.
+	Status() TaskState
 	// StdoutFile returns a file handle for file to the task's stdout file.
 	StdoutFile() (*os.File, error)
 	// StderrFile returns a file handle for file to the task's stderr file.
 	StderrFile() (*os.File, error)
 }
 
-// TaskHandle represents a process which can be stopped or monitored.
-type TaskHandle interface {
-	TaskInfo
+type TaskControl interface {
 	// Stops a task.
 	Stop() error
-	// Status returns a state of the task.
-	Status() TaskState
-	// ExitCode returns a exitCode. If task is not terminated it returns error.
-	ExitCode() (int, error)
 	// Wait does the blocking wait. It is a helper for waiting with a given timeout time.
 	// It returns true if task is terminated.
 	// In case of '0*time.Nanoseconds' timeout it waits infinitely for task completion.
@@ -42,6 +48,4 @@ type TaskHandle interface {
 	Clean() error
 	// EraseOutput removes task's stdout & stderr files.
 	EraseOutput() error
-	// Location returns address where task was located.
-	Address() string
 }
