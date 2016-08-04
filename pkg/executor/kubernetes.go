fc94e8763125087e0e5fc200823df1f3b21ff9a9
akwasniewska <alicja.kwasniewska@intel.com>
2016-08-04 10:52:54 +0200

    Tests.

--- pkg/executor/kubernetes.go
+++ pkg/executor/kubernetes.go
@@ -218,7 +218,8 @@ func (th *kubernetesTaskHandle) watch() error {
 
 			// Try to delete the failed pod to avoid conflicts and having to call Stop()
 			// after the stopped channel has been closed.
-			GracePeriodSeconds := 0
+			var GracePeriodSeconds int64
+			GracePeriodSeconds = int64(0)
 			th.podsAPI.Delete(th.pod.Name, &api.DeleteOptions{
 				GracePeriodSeconds: &GracePeriodSeconds,
 			})
