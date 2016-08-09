d13723a4012ac5294ad32eebcc90e8573acb95d1
akwasniewska <alicja.kwasniewska@intel.com>
2016-08-09 04:09:45 +0200

    Fixed typo in parallel.

--- integration_tests/pkg/executor/parallel_test.go
+++ integration_tests/pkg/executor/parallel_test.go
@@ -53,7 +53,7 @@ func TestParallel(t *testing.T) {
 					err = cmd.Run()
 
 					So(err, ShouldNotBeNil)
-					So(cmd.ProcessState.Sys().(syscall.WaitStatus).ExitStatus(), ShouldEqual, 1)
+					So(cmd.ProcessState.Sys().(syscall.WaitStatus).ExitStatus(), ShouldEqual, 0)
 				})
 			})
 		})
