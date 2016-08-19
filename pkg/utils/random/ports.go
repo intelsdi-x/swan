0776f143f59e394a5245a96577e3d6b8051298e0
akwasniewska <alicja.kwasniewska@intel.com>
2016-08-19 03:04:48 +0200

    Added random seed.

--- pkg/utils/random/ports.go
+++ pkg/utils/random/ports.go
@@ -2,13 +2,22 @@ package random
 
 import (
 	"math/rand"
+	"time"
 )
+
+var source int64
 
 // Ports returns count random ports between start and end.
 func Ports(start int, end int, count int) []int {
+	if source == 0 {
+		source = time.Now().UnixNano()
+	} else {
+		source = source + 1
+	}
+	r := rand.New(rand.NewSource(source))
 	ports := map[int]struct{}{}
 	for len(ports) < count {
-		port := rand.Intn(end-start) + start
+		port := r.Intn(end-start) + start
 		ports[port] = struct{}{}
 	}
 
