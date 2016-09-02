42b9d213e4720fb2f23d509369fb90a789596630
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-09-02 11:16:03 +0200

    SCE-657 - getting rid of constructor and using composite literal instead

--- pkg/isolation/numa_test.go
+++ pkg/isolation/numa_test.go
@@ -9,23 +9,23 @@ import (
 func TestNumaDecorator(t *testing.T) {
 	Convey("When I want to use numa decorator", t, func() {
 		Convey("Without any additional parameters", func() {
-			decorator := NewNumactl(false, false, []int{}, []int{}, []int{}, []int{}, -1)
+			decorator := Numactl{PreferredNode: -1}
 			So(decorator.Decorate("test"), ShouldEqual, "numactl  -- test")
 		})
 		Convey("Which runs test command w/o CPU awareness", func() {
-			decorator := NewNumactl(true, false, []int{}, []int{}, []int{}, []int{}, -1)
+			decorator := Numactl{IsAll: true, PreferredNode: -1}
 			So(decorator.Decorate("test"), ShouldEqual, "numactl --all -- test")
 		})
 		Convey("Which is allocating memory for test command on valid nodes", func() {
-			decorator := NewNumactl(false, true, []int{1, 3, 4}, []int{}, []int{}, []int{}, -1)
+			decorator := Numactl{IsLocalalloc: true, InterleaveNodes: []int{1, 3, 4}}
 			So(decorator.Decorate("test"), ShouldEqual, "numactl --localalloc --interleave=1,3,4 -- test")
 		})
 		Convey("Which is allocating memory for test command on non-valid nodes", func() {
-			decorator := NewNumactl(false, false, []int{1, -3, 6}, []int{}, []int{}, []int{}, -1)
+			decorator := Numactl{InterleaveNodes: []int{1, -3, 6}, PreferredNode: -1}
 			So(decorator.Decorate("test"), ShouldEqual, "numactl --interleave=1,6 -- test")
 		})
 		Convey("With every possible parameter", func() {
-			decorator := NewNumactl(false, false, []int{4}, []int{1}, []int{2}, []int{3}, 3)
+			decorator := Numactl{InterleaveNodes: []int{4}, MembindNodes: []int{1}, CPUnodebindNodes: []int{2}, PhyscpubindCPUs: []int{3}, PreferredNode: 3}
 			So(decorator.Decorate("test"), ShouldEqual, "numactl --interleave=4 --membind=1 --physcpubind=3 --cpunodebind=2 --preferred=3 -- test")
 		})
 	})
