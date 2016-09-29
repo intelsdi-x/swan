19260303408582a3a7b1cc05b050d117613ed48d
Michał Stachowski <squall0gd@users.noreply.github.com>
2016-09-29 09:11:48 +0200

    Reworked k8s downloading (#20)
    * Reworked k8s downloading
    
    Changes:
    - hide output from wget
    - cache binaries into /cache dir
    - detect if /cache directory exists
    
    * remove old point into swan repo
    
    * Change k8s binaries to modified
    
    * Remove pointing into /tmp
    
    * New directory name

--- misc/kubernetes/install_binaries.sh
+++ misc/kubernetes/install_binaries.sh
@@ -4,26 +4,40 @@
 # scripts.
 set -x -e -o pipefail
 
-K8S_VERSION="v1.3.0"
+K8S_VERSION="v1.4.0-alpha.2-serenity"
+
+CACHE_DIRECTORY=/cache
+
+if [ ! -d /cache ]; then
+    mkdir ~/.cache || true
+    CACHE_DIRECTORY=~/.cache
+fi
+
+function downloadK8s() {
+    if [ ! -f ${CACHE_DIRECTORY}/.kube-services-${K8S_VERSION} ] || [ ! -f ${CACHE_DIRECTORY}/$1 ] ; then
+        wget -q https://s3-us-west-2.amazonaws.com/intel-sdi.eo.swan.kubernetes/${1}.${K8S_VERSION} -O ${CACHE_DIRECTORY}/$1
+    fi 
+    cp ${CACHE_DIRECTORY}/$1 ${ATHENA_BIN}
+}
 
 pushd `dirname $0`
     ATHENA_ROOT=`pwd`/../../
-    ATHENA_BIN=${ATHENA_ROOT}misc/bin/
+    ATHENA_BIN=${ATHENA_ROOT}misc/bin
 
     mkdir -p ${ATHENA_BIN}
 
     OPT=$1
-    if [ "${OPT}" = "--force" ] || [ ! -f  ${ATHENA_BIN}.kube-services-${K8S_VERSION} ] ; then
+    if [ "${OPT}" = "--force" ] || [ ! -f  ${ATHENA_BIN}/.kube-services-${K8S_VERSION} ] ; then
         pushd ${ATHENA_BIN}
-            wget https://s3-us-west-2.amazonaws.com/intel-sdi.eo.swan.kubernetes/kubectl.v1.4.0-alpha.2-serenity -O kubectl
-            wget https://s3-us-west-2.amazonaws.com/intel-sdi.eo.swan.kubernetes/kube-apiserver.v1.4.0-alpha.2-serenity -O kube-apiserver
-            wget https://s3-us-west-2.amazonaws.com/intel-sdi.eo.swan.kubernetes/kube-controller-manager.v1.4.0-alpha.2-serenity -O kube-controller-manager
-            wget https://s3-us-west-2.amazonaws.com/intel-sdi.eo.swan.kubernetes/kube-proxy.v1.4.0-alpha.2-serenity -O kube-proxy
-            wget https://s3-us-west-2.amazonaws.com/intel-sdi.eo.swan.kubernetes/kube-scheduler.v1.4.0-alpha.2-serenity -O kube-scheduler
-            wget https://s3-us-west-2.amazonaws.com/intel-sdi.eo.swan.kubernetes/kubelet.v1.4.0-alpha.2-serenity -O kubelet
-
+            downloadK8s kubectl
+            downloadK8s kube-apiserver
+            downloadK8s kube-controller-manager
+            downloadK8s kube-proxy
+            downloadK8s kube-scheduler
+            downloadK8s kubelet
             chmod +x ./*
             touch .kube-services-${K8S_VERSION}
+            touch ${CACHE_DIRECTORY}/.kube-services-${K8S_VERSION}
         popd
     fi
 popd
