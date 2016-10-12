39f08d7aa2be085c7c2fb66242ad2b978f96e8b8
Michał Stachowski <squall0gd@users.noreply.github.com>
2016-10-12 11:20:52 +0200

    Fixed snap plugins building (#379)
    * Fixed snap plugins building, use shipped makefiles instead of "go get"
    * build_plugins + install_plugin function with patch applying feature

--- misc/dev/vagrant/singlenode/resources/scripts/install_golang.sh
+++ misc/dev/vagrant/singlenode/resources/scripts/install_golang.sh
@@ -16,3 +16,12 @@ if [ ! -f /cache/glide-v0.12.2-linux-amd64.tar.gz ]; then
     mkdir -p $GOPATH/bin || true
     tar -xf /cache/glide-v0.12.2-linux-amd64.tar.gz linux-amd64/glide --strip-components=1 -C $GOPATH/bin/glide
 fi
+
+echo "Installing Godep..."
+if [ ! -f /cache/.godep_v74 ]; then
+    wget -q -P /cache https://github.com/tools/godep/releases/download/v74/godep_linux_amd64
+    mkdir -p $GOPATH/bin || true
+    chmod +x /cache/godep_linux_amd64
+    cp /cache/godep_linux_amd64 $GOPATH/bin/godep
+    touch /cache/.godep_v74
+fi
