759400c18da7633757ff963ec2adbf3fcf5d4f70
squall0gd <michal.stachowski@outlook.com>
2016-10-20 18:25:10 +0200

    Remove installing openblas on Vagrant

--- misc/dev/vagrant/singlenode/resources/scripts/install_packages.sh
+++ misc/dev/vagrant/singlenode/resources/scripts/install_packages.sh
@@ -9,30 +9,47 @@ yum update -y -q
 yum install -y -q epel-release  # Enables EPEL repo
 echo Installing packages
 yum install -y -q \
+    boost \
+    boost-devel \
     ccache \
     deltarpm \
     docker-engine \
     etcd \
     gcc-g++ \
     gengetopt \
+    gflags \
+    gflags-devel \
     git \
+    glog \
+    glog-devel \
+    hdf5 \
+    hdf5-devel \
     htop \
     iptables \
+    leveldb \
+    leveldb-devel \
     libcgroup-tools \
     libevent-devel \
+    lmdb \
+    lmdb-devel \
     moreutils-parallel \
     nmap-ncat \
     numactl \
+    opencv \
+    opencv-devel \
     perf \
+    protobuf \
+    protobuf-devel \
     psmisc \
     pssh \
     python-pip \
     python-devel \
+    snappy \
+    snappy-devel \
     scons \
     sudo \
     tree \
     vim \
     wget \
     zeromq-devel
-. $HOME_DIR/go/src/github.com/intelsdi-x/swan/workloads/deep_learning/caffe/caffe_deps_centos.sh
 yum clean all
