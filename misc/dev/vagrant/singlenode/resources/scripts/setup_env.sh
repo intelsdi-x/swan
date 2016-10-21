5b5d831d1bdd49f78b0aaec3148ac0810849a926
Michał Stachowski <squall0gd@users.noreply.github.com>
2016-10-21 13:52:10 +0200

    SCE-756: There is no way to run caffe on kubernetes/remote because it is based on CWD of experiment process. [3/3] (#387)
    * OpenBLAS submodule
    
    * Building mutithreaded caffe
    
    * 1st working version
    
    * Add end line in build_caffe.sh
    
    * Priviliged installation of Caffe
    
    * Speeding up OpenBLAS compilation
    
    * Apply @iwankgb review
    
    * Remove installing openblas on Vagrant
    
    * Updated Dockerfile
    
    * Updated Makefile
    
    * Refresh syntax
    
    * Build just openblas libs
    
    * Add additional binaries
    
    * Update Vagrantfile
    
    * Exit on error
    
    * Apply @akwasnie review
    
    * Rerun tests
    
    * Rerun tests
    
    * once again rerun tests
    
    * Dump file
    
    * Remove dump file

--- misc/dev/vagrant/singlenode/resources/scripts/setup_env.sh
+++ misc/dev/vagrant/singlenode/resources/scripts/setup_env.sh
@@ -12,6 +12,8 @@ addEnv "export GOPATH=\"$HOME_DIR/go\""
 addEnv 'export CCACHE_CONFIGPATH=/etc/ccache.conf'
 addEnv 'export PATH=/usr/lib64/ccache/:$PATH:/usr/local/go/bin:$GOPATH/bin'
 addEnv 'export ATHENA_DIR=$GOPATH/src/github.com/intelsdi-x/athena'
+addEnv 'export OPENBLAS_PATH=/opt/OpenBLAS'
+addEnv 'export LD_LIBRARY_PATH=$OPENBLAS_PATH/lib'
 
 ## Create convenient symlinks in the home directory
 ln -sf $HOME_DIR/go/src/github.com/intelsdi-x/swan $HOME_DIR
---
+++ workloads/deep_learning/caffe/build_caffe.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+CAFFE_ROOT_DIRECTORY=$(dirname ${BASH_SOURCE[0]})
+OPENBLAS_SRC_DIRECTORY="${CAFFE_ROOT_DIRECTORY}/openblas"
+CAFFE_SRC_DIRECTORY="${CAFFE_ROOT_DIRECTORY}/caffe_src"
+
+CPUS_NUMBER=$(grep -c ^processor /proc/cpuinfo)
+
+pushd ${CAFFE_ROOT_DIRECTORY}
+if [ "${OPENBLAS_PATH}" != "" ] && [ "${BUILD_OPENBLAS}" == "true" ]; then
+    sudo mkdir -p ${OPENBLAS_PATH}
+    pushd ${OPENBLAS_SRC_DIRECTORY}
+    make -j USE_OPENMP=1 --quiet libs
+    make -j USE_OPENMP=1 --quiet netlib
+    make -j USE_OPENMP=1 --quiet shared
+    sudo make PREFIX=${OPENBLAS_PATH} --quiet install
+    popd
+    cp ${CAFFE_ROOT_DIRECTORY}/Makefile.config_openblas ${CAFFE_SRC_DIRECTORY}/Makefile.config
+    export LD_LIBRARY_PATH=${OPENBLAS_PATH}/lib
+else
+    echo "To build multithreaded caffe you need to set \"OPENBLAS_PATH\" and \"BUILD_OPENBLAS\" envs first."
+    cp ${CAFFE_ROOT_DIRECTORY}/Makefile.config ${CAFFE_SRC_DIRECTORY}/Makefile.config
+fi
+
+cp ${CAFFE_ROOT_DIRECTORY}/caffe_cpu_solver.patch ${CAFFE_SRC_DIRECTORY}
+cp ${CAFFE_ROOT_DIRECTORY}/vagrant_vboxsf_workaround.patch ${CAFFE_SRC_DIRECTORY}
+cp ${CAFFE_ROOT_DIRECTORY}/get_cifar10.patch ${CAFFE_SRC_DIRECTORY}
+
+
+pushd ${CAFFE_SRC_DIRECTORY}
+patch -p1 --forward -s --merge < caffe_cpu_solver.patch
+patch -p1 --forward -s --merge < vagrant_vboxsf_workaround.patch
+patch -p1 --forward -s --merge < get_cifar10.patch
+export OMP_NUM_THREADS=${CPUS_NUMBER}
+make --quiet all
+popd
+${CAFFE_ROOT_DIRECTORY}/prepare_cifar10_dataset.sh
+popd
