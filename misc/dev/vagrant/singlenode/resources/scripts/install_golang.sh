f95237813e02bb343dd0d9931751a8afe55c583e
squall0gd <michal.stachowski@outlook.com>
2016-10-24 16:18:56 +0200

    Vagrant scripts: better expose of external artifacts version

--- misc/dev/vagrant/singlenode/resources/scripts/install_golang.sh
+++ misc/dev/vagrant/singlenode/resources/scripts/install_golang.sh
@@ -4,24 +4,28 @@ set -e
 
 source $HOME_DIR/.bash_profile
 
+GO_VERSION="1.6"
+GLIDE_VERSION="v0.12.2"
+GODEP_VERSION="v74"
+
 echo "Installing Go..."
-if [ ! -f /cache/go1.6.linux-amd64.tar.gz ]; then
-    wget -q -P /cache https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz
-    tar xf /cache/go1.6.linux-amd64.tar.gz -C /usr/local
+if [ ! -f /cache/go${GO_VERSION}.linux-amd64.tar.gz ]; then
+    wget -q -P /cache https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
+    tar xf /cache/go${GO_VERSION}.linux-amd64.tar.gz -C /usr/local
 fi
 
 echo "Installing Glide..."
-if [ ! -f /cache/glide-v0.12.2-linux-amd64.tar.gz ]; then
-    wget -q -P /cache https://github.com/Masterminds/glide/releases/download/v0.12.2/glide-v0.12.2-linux-amd64.tar.gz
+if [ ! -f /cache/glide-${GLIDE_VERSION}-linux-amd64.tar.gz ]; then
+    wget -q -P /cache https://github.com/Masterminds/glide/releases/download/${GLIDE_VERSION}/glide-${GLIDE_VERSION}-linux-amd64.tar.gz
     mkdir -p $GOPATH/bin || true
-    tar -xf /cache/glide-v0.12.2-linux-amd64.tar.gz linux-amd64/glide --strip-components=1 -C $GOPATH/bin/glide
+    tar -xf /cache/glide-${GLIDE_VERSION}-linux-amd64.tar.gz linux-amd64/glide --strip-components=1 -C $GOPATH/bin/glide
 fi
 
 echo "Installing Godep..."
-if [ ! -f /cache/.godep_v74 ]; then
-    wget -q -P /cache https://github.com/tools/godep/releases/download/v74/godep_linux_amd64
+if [ ! -f /cache/.godep_${GODEP_VERSION} ]; then
+    wget -q -P /cache https://github.com/tools/godep/releases/download/${GODEP_VERSION}/godep_linux_amd64
     mkdir -p $GOPATH/bin || true
     chmod +x /cache/godep_linux_amd64
     cp /cache/godep_linux_amd64 $GOPATH/bin/godep
-    touch /cache/.godep_v74
+    touch /cache/.godep_${GODEP_VERSION}
 fi
