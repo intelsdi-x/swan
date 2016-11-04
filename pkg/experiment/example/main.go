2eb606e73d64789b17b0e5448187a89f00d9b4b6
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-11-04 15:45:00 +0100

    SCE-785 - using same closure for Permutate() and Iterate()

--- pkg/experiment/example/main.go
+++ pkg/experiment/example/main.go
@@ -10,18 +10,17 @@ func main() {
 	fmt.Println("Nested Iterate() calls")
 
 	var iteration int
+	repetition := func(a string, b int) {
+		fmt.Printf("iteration: %d\tset: %v\trange: %v\n", iteration, a, b)
+		iteration++
+	}
+
 	experiment.Iterate("one,two,three", func() {
-		experiment.Iterate("1-3", func(a string, b int) {
-			fmt.Printf("iteration: %d\tset: %v\trange: %v\n", iteration, a, b)
-			iteration++
-		})
+		experiment.Iterate("1-3", repetition)
 	})
 
 	fmt.Println("Should produce identical result as a Permutate() call")
 
 	iteration = 0
-	experiment.Permutate("one,two,three", "1-3", func(a string, b int) {
-		fmt.Printf("iteration: %d\tset: %v\trange: %v\n", iteration, a, b)
-		iteration++
-	})
+	experiment.Permutate("one,two,three", "1-3", repetition)
 }
