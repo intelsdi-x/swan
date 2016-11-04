620ed327fb3857ee14cefeeace6ae95866c5151d
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-11-04 16:37:13 +0100

    SCE-785 - some reordering and renaming

--- pkg/experiment/functional.go
+++ pkg/experiment/functional.go
@@ -8,11 +8,24 @@ import (
 
 var experimentContext []interface{}
 
+// Permutate accepts slices of specs, tell set from range and run them recursively
+func Permutate(specs ...interface{}) {
+	if len(specs) > 2 {
+		recursive := func() {
+			Permutate(specs[1:]...)
+		}
+		Iterate(specs[0].(string), recursive)
+	} else {
+
+		Iterate(specs...)
+	}
+}
+
 // Iterate is a public interface of the experiment. It can recognize type of iteration and runs approptiate code.
 func Iterate(specs ...interface{}) {
 	if isSetSpec(specs[0]) {
 		set(specs...)
-	} else if isRangeSpec(specs[0]) {
+	} else if isIntervalSpec(specs[0]) {
 		interval(specs...)
 	}
 }
@@ -21,24 +34,20 @@ func isSetSpec(spec interface{}) bool {
 	return strings.Contains(spec.(string), ",")
 }
 
-func isRangeSpec(spec interface{}) bool {
-	return strings.Contains(spec.(string), "-")
-}
-
-// Range allows to run a function across a range of integers
-func interval(specs ...interface{}) {
-	from, to := parseRangeSpec(specs[0])
-	for i := from; i <= to; i++ {
-		call(specs[1], i)
+func set(specs ...interface{}) {
+	set := parseSetSpec(specs[0])
+	for _, v := range set {
+		call(specs[1], v)
 	}
 }
 
-func parseRangeSpec(rangeSpec interface{}) (from, to int) {
-	boundaries := strings.Split(rangeSpec.(string), "-")
-	from, _ = strconv.Atoi(boundaries[0])
-	to, _ = strconv.Atoi(boundaries[1])
+func parseSetSpec(setSpec interface{}) (set []interface{}) {
+	items := strings.Split(setSpec.(string), ",")
+	for _, v := range items {
+		set = append(set, v)
+	}
 
-	return from, to
+	return set
 }
 
 func call(r, localContext interface{}) {
@@ -57,32 +66,21 @@ func call(r, localContext interface{}) {
 	experimentContext = experimentContext[:len(experimentContext)-1]
 }
 
-// Set allows to run a function across set of values
-func set(specs ...interface{}) {
-	set := parseSetSpec(specs[0])
-	for _, v := range set {
-		call(specs[1], v)
+func isIntervalSpec(spec interface{}) bool {
+	return strings.Contains(spec.(string), "-")
+}
+
+func interval(specs ...interface{}) {
+	from, to := parseIntervalSpec(specs[0])
+	for i := from; i <= to; i++ {
+		call(specs[1], i)
 	}
 }
 
-func parseSetSpec(setSpec interface{}) (set []interface{}) {
-	items := strings.Split(setSpec.(string), ",")
-	for _, v := range items {
-		set = append(set, v)
-	}
+func parseIntervalSpec(rangeSpec interface{}) (from, to int) {
+	boundaries := strings.Split(rangeSpec.(string), "-")
+	from, _ = strconv.Atoi(boundaries[0])
+	to, _ = strconv.Atoi(boundaries[1])
 
-	return set
-}
-
-// Permutate accepts slices of specs, tell set from range and run them recursively
-func Permutate(specs ...interface{}) {
-	if len(specs) > 2 {
-		recursive := func() {
-			Permutate(specs[1:]...)
-		}
-		Iterate(specs[0].(string), recursive)
-	} else {
-
-		Iterate(specs...)
-	}
+	return from, to
 }
