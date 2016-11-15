630eede280fd6a30e200da8d87263c85dd82e235
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-11-15 11:18:32 +0100

    SCE-785 - we can use name Range now

--- pkg/experiment/range.go
+++ pkg/experiment/range.go
@@ -1,14 +1,14 @@
 package experiment
 
-// Interval allows to iterate over values it specifies
-type Interval struct {
+// Range allows to iterate over values it specifies
+type Range struct {
 	From float64
 	To   float64
 	Step float64
 }
 
 // Iterate implements Iterator interface
-func (r *Interval) Iterate(runnable interface{}) {
+func (r *Range) Iterate(runnable interface{}) {
 	for i := r.From; i < r.To; i = i + r.Step {
 		Call(runnable, i)
 	}
