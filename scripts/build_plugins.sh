31d84801dc9f55aaa4eb6b35dfebd4d8aed8cfc4
Michał Stachowski <squall0gd@users.noreply.github.com>
2016-11-17 15:55:53 +0100

    SCE-870: Update publisher-cassandra location (#407)
    * Update publisher-cassandra location
    
    * Checkout to plugin version
    
    * Revert plugin

--- scripts/build_plugins.sh
+++ scripts/build_plugins.sh
@@ -8,18 +8,19 @@ function installPlugin() {
     fi
     pushd $1
     git pull
+    git checkout $2
     if [ "$3" != "" ]; then
-        patch -p1 --forward -s --merge < $3 || true 
+        patch -p1 --forward -s --merge < $4 || true
     fi
     make
-    cp $2 $GOPATH/bin/$1
+    cp $3 $GOPATH/bin/$1
     popd
 }
 
 pushd $GOPATH/src/github.com/intelsdi-x
-installPlugin snap-plugin-publisher-cassandra build/rootfs/snap-plugin-publisher-cassandra
-installPlugin snap-plugin-processor-tag build/linux/x86_64/snap-plugin-processor-tag
-installPlugin kubesnap-plugin-collector-docker build/rootfs/snap-plugin-collector-docker $(pwd)/swan/misc/kubesnap_docker_collector.patch
+installPlugin snap-plugin-publisher-cassandra 188b78e3e6b29dc3b80d8a9e9dd21eb5a5a8a81a build/rootfs/snap-plugin-publisher-cassandra
+installPlugin snap-plugin-processor-tag 3ccdb7de499ff92d7b7c9812c497a6e6f124a64d build/linux/x86_64/snap-plugin-processor-tag
+installPlugin kubesnap-plugin-collector-docker 81a60d8276054a95dde4a72429bf320c89e31ded build/rootfs/snap-plugin-collector-docker $(pwd)/swan/misc/kubesnap_docker_collector.patch
 popd
 (go install ./misc/snap-plugin-collector-session-test)
 (go install ./misc/snap-plugin-publisher-session-test)
