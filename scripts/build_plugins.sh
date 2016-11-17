1f664ba850d813c1ce3095c5b8154c8f4aa7caaf
squall0gd <michal.stachowski@outlook.com>
2016-11-17 14:21:06 +0100

    Checkout to plugin version

--- scripts/build_plugins.sh
+++ scripts/build_plugins.sh
@@ -8,18 +8,19 @@ function installPlugin() {
     fi
     pushd $1
     git pull
+    git checkout $2
     if [ "$3" != "" ]; then
-        patch -p1 --forward -s --merge < $3 || true 
+        patch -p1 --forward -s --merge < $4 || true
     fi
     make
-    cp $2 $GOPATH/bin/$1
+    cp $3 $GOPATH/bin/$1
     popd
 }
 
 pushd $GOPATH/src/github.com/intelsdi-x
-installPlugin snap-plugin-publisher-cassandra build/linux/x86_64/snap-plugin-publisher-cassandra
-installPlugin snap-plugin-processor-tag build/linux/x86_64/snap-plugin-processor-tag
-installPlugin kubesnap-plugin-collector-docker build/rootfs/snap-plugin-collector-docker $(pwd)/swan/misc/kubesnap_docker_collector.patch
+installPlugin snap-plugin-publisher-cassandra d37b39d9c88af36480eca05c3d2da4b7a0554d16 build/rootfs/snap-plugin-publisher-cassandra
+installPlugin snap-plugin-processor-tag 3ccdb7de499ff92d7b7c9812c497a6e6f124a64d build/linux/x86_64/snap-plugin-processor-tag
+installPlugin kubesnap-plugin-collector-docker 81a60d8276054a95dde4a72429bf320c89e31ded build/rootfs/snap-plugin-collector-docker $(pwd)/swan/misc/kubesnap_docker_collector.patch
 popd
 (go install ./misc/snap-plugin-collector-session-test)
 (go install ./misc/snap-plugin-publisher-session-test)
