89c3f656f7f0218b0298978371ef68e7a70390d7
squall0gd <michal.stachowski@outlook.com>
2016-11-23 14:30:38 +0100

    Apply @mpatelcz review

--- workloads/deep_learning/caffe/caffe_wrapper.sh
+++ workloads/deep_learning/caffe/caffe_wrapper.sh
@@ -6,6 +6,7 @@ if [ "$1" == "init" ]; then
         mkdir -p /tmp/caffe/examples/cifar10/
         bash ./data/cifar10/get_cifar10.sh
         bash ./examples/cifar10/create_cifar10.sh
+        cp ./cifar10_quick_iter_5000.caffemodel.h5 /tmp/caffe
         exit 0
 fi
 ./bin/caffe "$@"
