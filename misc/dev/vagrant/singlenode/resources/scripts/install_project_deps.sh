c36e2fc646362ce248f675d96c78aba4f46e2ae0
squall0gd <michal.stachowski@outlook.com>
2016-11-28 14:58:30 +0100

    Download specjbb before building package

--- misc/dev/vagrant/singlenode/resources/scripts/install_project_deps.sh
+++ misc/dev/vagrant/singlenode/resources/scripts/install_project_deps.sh
@@ -14,6 +14,7 @@ echo "Installing project dependencies..."
 pushd $HOME_DIR/go/src/github.com/intelsdi-x/swan/
 executeAsVagrantUser make repository_reset
 executeAsVagrantUser make deps_all
+./scripts/get_specjbb.sh -s . -c $HOME_DIR/swan_s3_creds/.s3cfg -b swan-artifacts
 executeAsVagrantUser make BUILD_OPENBLAS=${BUILD_OPENBLAS} dist
 if [[ "$BUILD_DOCKER_IMAGE" == "true" ]]; then
         executeAsVagrantUser make build_image
@@ -23,5 +24,4 @@ make install
 # -b specifies bucket name.
 # By default bucket name value is read from SWAN_BUCKET_NAME env variable.
 # When we add this variable to jenkins/vagrant, we will be able to remove it from command below.
-./scripts/get_specjbb.sh -s . -c $HOME_DIR/swan_s3_creds/.s3cfg -b swan-artifacts
 popd
