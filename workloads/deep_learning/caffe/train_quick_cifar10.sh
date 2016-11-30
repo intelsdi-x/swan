61c09dce6d1697f7dd500f54848bead37267f86b
Michał Stachowski <squall0gd@users.noreply.github.com>
2016-11-30 17:39:38 +0100

    Delete train_quick_cifar10.sh

--- workloads/deep_learning/caffe/train_quick_cifar10.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Runs Caffe workload with CIFAR10 example solver.
-# You need to prepare the workload using `prepare_ciphar10_dataset.sh` first.
-
-caffe train --solver=examples/cifar10/cifar10_quick_solver.prototxt
-caffe train --solver=examples/cifar10/cifar10_quick_solver_lr1.prototxt --snapshot=examples/cifar10/cifar10_quick_iter_4000.solverstate.h5
