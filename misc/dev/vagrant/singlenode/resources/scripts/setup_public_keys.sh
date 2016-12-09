b2000c07331646d3d4177c2287ba34e54af521a5
squall0gd <michal.stachowski@outlook.com>
2016-12-09 12:19:44 +0100

    Always download s3cmd

--- misc/dev/vagrant/singlenode/resources/scripts/setup_public_keys.sh
+++ misc/dev/vagrant/singlenode/resources/scripts/setup_public_keys.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [ ! -x "$(which s3cmd)" ]; then
-    pip install s3cmd
-fi
-
 if [ -e "$HOME_DIR/swan_s3_creds/.s3cfg" ]; then
+    pip install s3cmd
     s3cmd sync -c $HOME_DIR/swan_s3_creds/.s3cfg s3://swan-artifacts/public_keys $HOME_DIR/.ssh/authorized_keys
 fi
