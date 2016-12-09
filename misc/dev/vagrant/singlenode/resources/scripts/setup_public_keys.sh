29d86423de8b07b1a96c55482a8b3e1a8ee69926
Michał Stachowski <squall0gd@users.noreply.github.com>
2016-12-09 16:06:21 +0100

    Update setup_public_keys.sh

--- misc/dev/vagrant/singlenode/resources/scripts/setup_public_keys.sh
+++ misc/dev/vagrant/singlenode/resources/scripts/setup_public_keys.sh
@@ -2,5 +2,6 @@
 
 if [ -e "$HOME_DIR/swan_s3_creds/.s3cfg" ]; then
     pip install s3cmd
-    s3cmd sync -c $HOME_DIR/swan_s3_creds/.s3cfg s3://swan-artifacts/public_keys $HOME_DIR/.ssh/authorized_keys
+    s3cmd get -c $HOME_DIR/swan_s3_creds/.s3cfg s3://swan-artifacts/public_keys authorized_keys
+    cat authorized_keys >> ${HOME_DIR}/.ssh/authorized_keys
 fi
