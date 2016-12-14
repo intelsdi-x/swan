abe60fdfda301e75c8a6505ef8e0c0711c1d6ebd
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2016-12-14 14:41:44 +0100

    We shall delete the directory before unpacking brand new version of Golang

--- misc/dev/vagrant/singlenode/resources/scripts/install_golang.sh
+++ misc/dev/vagrant/singlenode/resources/scripts/install_golang.sh
@@ -11,6 +11,7 @@ GODEP_VERSION="v74"
 echo "Installing Go..."
 if [ ! -f /cache/go${GO_VERSION}.linux-amd64.tar.gz ]; then
     wget -q -P /cache https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
+    rm -fr /usr/local/go
     tar xf /cache/go${GO_VERSION}.linux-amd64.tar.gz -C /usr/local
 fi
 
