2deea9034453a9cf87e97a9eeea2b2bee0a1f37f
Pawel Palucki <pawel.palucki@intel.com>
2017-02-20 17:11:35 +0100

    less verbose tests and fixed root PATH expansion

--- scripts/isolate-pid.sh
+++ scripts/isolate-pid.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Run command in isolated PID namespace with extended PATH as root, to
 #cleanup any left over procesess.
-sudo -E env PATH=$PATH unshare --pid --fork --mount-proc "$@"
+sudo -E env "PATH=$PATH" unshare --pid --fork --mount-proc "$@"
