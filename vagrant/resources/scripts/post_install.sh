abf1584cc2fd0f77a1e4770042f0352735bf7eab
Pawel Palucki <pawel.palucki@intel.com>
2017-02-23 21:12:48 +0100

    PATH in /etc/environment for all

--- vagrant/resources/scripts/post_install.sh
+++ vagrant/resources/scripts/post_install.sh
@@ -5,3 +5,5 @@ set -e
 echo "Rewriting permissions..."
 chown -R $VAGRANT_USER:$VAGRANT_USER $HOME_DIR
 chown -R $VAGRANT_USER:$VAGRANT_USER /cache
+
+echo "PATH=/usr/lib64/ccache:/sbin:/bin:/usr/sbin:/usr/bin:/opt/swan/bin" >> /etc/environment
