b3b96528d0ade6c1537c540e016582557cbbe41c
Pawel Palucki <pawel.palucki@intel.com>
2017-03-03 11:53:53 +0100

    drop integration tests run with binaries

--- scripts/integration_tests_build.sh
+++ scripts/integration_tests_build.sh
@@ -1,7 +1,11 @@
 # compile depedencies and compile all tests
-set -x 
+set -e 
+echo "install dependecies..."
+go test -v -i ./integration_tests/...
+echo "build tests..."
 mkdir -p build/tests
-pushd build/tests
-for i in `go list ../../integration_tests/...`; do go test -i $i; done
-for i in `go list ../../integration_tests/...`; do go test -c $i; done
-popd
+cd build/tests
+for i in `go list ../../integration_tests/...`; do 
+    echo building: $i
+    go test -c $i
+done
--- scripts/integration_tests_run.sh
+++
@@ -1,3 +0,0 @@
-# run all tests as bins
-set -x -e
-for i in `ls build/tests/`; do ./build/tests/$i; done
