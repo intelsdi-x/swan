42c0d6d7fb494f88a34c19524182eb7845f1a5ac
Stanisław Szydło <stanislaw.szydlo@intel.com>
2017-03-06 17:23:05 +0100

    Update install_golang.sh

--- vagrant/resources/scripts/install_golang.sh
+++ vagrant/resources/scripts/install_golang.sh
@@ -5,7 +5,7 @@ set -e
 source $HOME_DIR/.bash_profile
 
 GO_VERSION="1.7"
-GLIDE_VERSION="v0.12.2"
+GLIDE_VERSION="v0.12.3"
 GODEP_VERSION="v74"
 
 echo "Installing Go..."
@@ -19,7 +19,7 @@ echo "Installing Glide..."
 if [ ! -f /cache/glide-${GLIDE_VERSION}-linux-amd64.tar.gz ]; then
     wget -q -P /cache https://github.com/Masterminds/glide/releases/download/${GLIDE_VERSION}/glide-${GLIDE_VERSION}-linux-amd64.tar.gz
     mkdir -p $GOPATH/bin || true
-    tar -xf /cache/glide-${GLIDE_VERSION}-linux-amd64.tar.gz linux-amd64/glide --strip-components=1 -C $GOPATH/bin/glide
+    tar -xf /cache/glide-${GLIDE_VERSION}-linux-amd64.tar.gz --strip-components=1 -C $GOPATH/bin/ linux-amd64/glide
 fi
 
 echo "Installing Godep..."
