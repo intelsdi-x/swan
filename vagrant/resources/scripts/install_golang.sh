cceec1f7cbcdbf1345d665dd9cba980eff3c8ee9
Pawel Palucki <pawel.palucki@intel.com>
2017-03-07 10:07:41 +0100

    drop glide and godep from vagrant/scripts

--- vagrant/resources/scripts/install_golang.sh
+++ vagrant/resources/scripts/install_golang.sh
@@ -14,19 +14,3 @@ if [ ! -f /cache/go${GO_VERSION}.linux-amd64.tar.gz ]; then
     rm -fr /usr/local/go
     tar xf /cache/go${GO_VERSION}.linux-amd64.tar.gz -C /usr/local
 fi
-
-echo "Installing Glide..."
-if [ ! -f /cache/glide-${GLIDE_VERSION}-linux-amd64.tar.gz ]; then
-    wget -q -P /cache https://github.com/Masterminds/glide/releases/download/${GLIDE_VERSION}/glide-${GLIDE_VERSION}-linux-amd64.tar.gz
-    mkdir -p $GOPATH/bin || true
-    tar -xf /cache/glide-${GLIDE_VERSION}-linux-amd64.tar.gz --strip-components=1 -C $GOPATH/bin/ linux-amd64/glide
-fi
-
-echo "Installing Godep..."
-if [ ! -f /cache/.godep_${GODEP_VERSION} ]; then
-    wget -q -P /cache https://github.com/tools/godep/releases/download/${GODEP_VERSION}/godep_linux_amd64
-    mkdir -p $GOPATH/bin || true
-    chmod +x /cache/godep_linux_amd64
-    cp /cache/godep_linux_amd64 $GOPATH/bin/godep
-    touch /cache/.godep_${GODEP_VERSION}
-fi
