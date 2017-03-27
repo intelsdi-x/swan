c5ed82afd38bf522335efc03dd0b227b30bebf59
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2017-03-27 14:14:06 +0200

    adding rdtset-based isolation

---
+++ pkg/isolation/rdtset_test.go
@@ -0,0 +1,19 @@
+package isolation
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestRdtsetDecorator(t *testing.T) {
+	Convey("When I want to use rdtset decorator", t, func() {
+		Convey("It should parse configuration values as expected", func() {
+			decorator := &Rdtset{Mask: 2047, CPURange: "0-3"}
+			command := decorator.Decorate("ls -l")
+
+			So(command, ShouldEqual, "rdtset.sh -v -c 0-3 -t 'l3=0x7ff;cpu=0-3' ls -l")
+		})
+	})
+
+}
