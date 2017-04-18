068b0f9852b02e8745050a725c0b8ab392846db1
Szymon Konefal <szymon.konefal@intel.com>
2017-04-18 17:00:07 +0200

    Executors flag for experiment fix.

--- pkg/experiment/sensitivity/executors.go
+++ pkg/experiment/sensitivity/executors.go
@@ -21,14 +21,14 @@ var (
 	}()
 
 	// HPKubernetesCPUResourceFlag indicates CPU shares that HP task should be allowed to use.
-	HPKubernetesCPUResourceFlag = conf.NewIntFlag("hp_kubernetes_cpu_resource", "set limits and request for HP workloads pods run on kubernetes in CPU millis (default 1000 * number of CPU).", runtime.NumCPU()*1000)
+	HPKubernetesCPUResourceFlag = conf.NewIntFlag("hp_kubernetes_cpu_resource", "Sets CPU resource limit and request for HP workload on Kubernetes [CPU millis, default 1000 * number of CPU].", runtime.NumCPU()*1000)
 	// HPKubernetesMemoryResourceFlag indicates amount of memory that HP task can use.
-	HPKubernetesMemoryResourceFlag = conf.NewIntFlag("hp_kubernetes_memory_resource", "set memory limits and request for HP pods workloads run on kubernetes in bytes (default 1GB).", 1000000000)
+	HPKubernetesMemoryResourceFlag = conf.NewIntFlag("hp_kubernetes_memory_resource", "Sets memory limit and request for HP workloads on Kubernetes in bytes (default 1GB).", 1000000000)
 
 	// RunOnKubernetesFlag indicates that experiment is to be run on K8s cluster.
 	RunOnKubernetesFlag = conf.NewBoolFlag("kubernetes", "Launch HP and BE tasks on Kubernetes.", false)
 	// RunOnExistingKubernetesFlag indicates that experiment should not set up a Kubernetes cluster but use an existing one.
-	RunOnExistingKubernetesFlag = conf.NewBoolFlag("kubernetes_run_on_existing", "Launch HP and BE tasks on existing Kubernetes cluster. (can be use only with --kubernetes flag)", false)
+	RunOnExistingKubernetesFlag = conf.NewBoolFlag("kubernetes_run_on_existing", "Launch HP and BE tasks on existing Kubernetes cluster. (It can only be used with --kubernetes flag). User should provide 'kubernetes_kubeconfig' flag to kubeconfig to point proper API server.", false)
 
 	kubernetesNodeName = conf.NewStringFlag("kubernetes_nodename", "Experiment's Kubernetes pods will be run on this node.", hostname)
 )
@@ -37,12 +37,12 @@ var (
 func PrepareExecutors(hpIsolation isolation.Decorator) (hpExecutor executor.Executor, beExecutorFactory ExecutorFactoryFunc, cleanup func() error, err error) {
 	if RunOnKubernetesFlag.Value() {
 		k8sConfig := kubernetes.DefaultConfig()
-		masterExecutor, err := executor.NewRemoteFromIP(k8sConfig.KubeAPIAddr)
-		if err != nil {
-			return nil, nil, nil, err
-		}
 
 		if !RunOnExistingKubernetesFlag.Value() {
+			masterExecutor, err := executor.NewRemoteFromIP(k8sConfig.KubeAPIAddr)
+			if err != nil {
+				return nil, nil, nil, err
+			}
 			k8sLauncher := kubernetes.New(masterExecutor, executor.NewLocal(), k8sConfig)
 			k8sClusterTaskHandle, err := k8sLauncher.Launch()
 			if err != nil {
@@ -65,7 +65,6 @@ func PrepareExecutors(hpIsolation isolation.Decorator) (hpExecutor executor.Exec
 		hpExecutorConfig.Decorators = isolation.Decorators{hpIsolation}
 		hpExecutorConfig.HostNetwork = true // requied to have access from mutilate agents run outside a k8s cluster.
 		hpExecutorConfig.Address = k8sConfig.GetKubeAPIAddress()
-
 		hpExecutorConfig.CPULimit = int64(HPKubernetesCPUResourceFlag.Value())
 		hpExecutorConfig.MemoryLimit = int64(HPKubernetesMemoryResourceFlag.Value())
 		// "Guranteed" class is when both resources and set for request and limit and equal.
