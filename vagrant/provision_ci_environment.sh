62737cab87af3dda7f7199dea9ea88a30613edb6
iwankgb <maciej.iwanowski@critical.today>
2017-05-11 12:09:59 +0200

    Getting rid of "centos_swan_image" (#658)

--- vagrant/provision_ci_environment.sh
+++ vagrant/provision_ci_environment.sh
@@ -46,32 +46,15 @@ if [ -e "$HOME_DIR/swan_s3_creds/.s3cfg" ]; then
         chown -R ${VAGRANT_USER}:${VAGRANT_USER} ${HOME_DIR}/.glide
     fi
 
-    echo "Synchronize /opt/swan"
-    # For manuall installtion
-    # sudo sh -c "mkdir -p /opt/swan && chown -R $USER:$USER /opt/swan"
-    s3cmd sync s3://swan-artifacts/workloads/swan/ /opt/swan/
 
-    echo "Install binaries from /opt/swan"
-    ln -sv ${SWAN_BIN}/* /bin/
-
-    echo "Download centos_swan_image docker image"
-    s3cmd sync s3://swan-artifacts/workloads/centos_swan_image.tgz /tmp/centos_swan_image.tgz
-
-    
-    echo "Import centos_swan_image to docker"
-    gunzip /tmp/centos_swan_image.tgz 
-    docker image load -i /tmp/centos_swan_image.tar 
-    docker images centos_swan_image
-
-    echo "Check centos_swan_image docker"
-    docker run --rm centos_swan_image memcached -V
-    docker run --rm centos_swan_image mutilate --version
-    docker run --rm centos_swan_image caffe.sh --version
+    echo "Downloading private dependencies"
+    s3cmd cp --recursive s3://swan-artifacts/workloads/swan-private/  s3://swan-artifacts/workloads/swan/
 fi
 echo "--------------------------- Provisioning CI environment done (`date`)"
 
 echo "--------------------------- Post install (`date`)"
-ln -sf $HOME_DIR/go/src/github.com/intelsdi-x/swan $HOME_DIR
+ln -svf $HOME_DIR/go/src/github.com/intelsdi-x/swan $HOME_DIR
+ln -svf /opt/swan/bin/* /bin/
 chown -R $SWAN_USER:$SWAN_USER $HOME_DIR
 chown -R $SWAN_USER:$SWAN_USER /opt/swan
 
