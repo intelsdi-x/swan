68218ef7dada2f4fe2fcc66781f89a2517f17a18
Iwan (Maciej Iwanowski) <maciej.iwanowski@intel.com>
2017-05-12 14:51:40 +0200

    Updating AMI and Swan version

--- vagrant/provision_development_environment.sh
+++ vagrant/provision_development_environment.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Copyright (c) 2017 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -59,7 +60,8 @@ yum install -y -q \
     tmux \
     htop \
     sudo \
-    git
+    git \
+    nmap-ncat
 
 echo "----------------------------- Preparing SSH access for root (`date`)"
 # root user
