39716d8144d9aef364ea46b8c7d291f4e2d527c0
Dual2Core <patryk.kobielak@intel.com>
2017-09-06 15:17:34 +0200

    Changes requested in review.

--- ansible/prepare_host_env.sh
+++ ansible/prepare_host_env.sh
@@ -1,11 +1,8 @@
-if [ "$EUID" -ne 0 ]
+#!/bin/bash
+
+if [ "$EUID" -ne "0" ]
 	then echo "Please run this setup as root"
 	return
-fi
-
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
-	then echo "Please run this setup with 'source' command"
-	exit
 fi
 
 
@@ -16,14 +13,41 @@ echo "Installing required packages..."
 apt install git -y
 
 # golang
-bash local_scripts/goinstall.sh --64
-source ~/.bashrc
+#set -e
 
-# caffe
+VERSION="1.9"
+PACKAGE="go$VERSION.linux-amd64.tar.gz"
 
+echo "Downloading $PACKAGE ..."
+wget https://storage.googleapis.com/golang/$PACKAGE -O /tmp/go.tar.gz
+if [ $? -ne 0 ]; then
+    	echo "Download failed! Exiting."
+        exit 1
+fi
+echo "Extracting go.tar.gz"
+tar -C "$HOME" -xzf /tmp/go.tar.gz
 
-### Install and configure caffe
+# Be sure to uninstall old go
+rm -rf "$HOME/.go"
 
+# Save Go to .go
+mv "$HOME/go" "$HOME/.go"
+touch "$HOME/.bashrc"
+{
+	echo 'export GOROOT=$HOME/.go'
+	echo 'export PATH=$PATH:$GOROOT/bin'
+	echo 'export GOPATH=$HOME/go'
+	echo 'export PATH=$PATH:$GOPATH/bin'
+} >> "$HOME/.bashrc"
+
+export GOROOT=$HOME/.go
+export PATH=$PATH:$GOROOT/bin
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+
+mkdir -p $HOME/go/{src,pkg,bin}
+echo -e "\nGo $VERSION was installed.\n"
+rm -f /tmp/go.tar.gz
 
 ### Get latest Swan repository
 git clone https://github.com/intelsdi-x/swan.git
