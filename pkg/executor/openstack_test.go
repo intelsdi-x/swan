a3377e45727bb9e31497d571e6ac59597347fde0
Creatone <pawel.szulik@intel.com>
2018-12-07 10:13:58 +0100

    Add openstack test

--- pkg/executor/openstack_test.go
+++ pkg/executor/openstack_test.go
@@ -1,1 +1,24 @@
 package executor
+
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/gophercloud/gophercloud"
+)
+
+func TestDefaultOpenstackConfig(t *testing.T) {
+
+	Convey("OpenStack config is sane by default", t, func() {
+
+		auth := gophercloud.AuthOptions{Username: "cirros"}
+		config := DefaultOpenstackConfig(auth)
+
+		So(config.Auth.Username, ShouldEqual, auth.Username)
+		So(config.Flavor.Disk, ShouldEqual, 10)
+		So(config.Flavor.RAM, ShouldEqual, 1024)
+		So(config.Flavor.VCPUs, ShouldEqual, 1)
+		So(config.Image, ShouldEqual, "cirros")
+		So(config.User, ShouldEqual, "cirros")
+		So(config.SSHKeyPath, ShouldEqual, "~/.ssh/id_rsa")
+	})
+}
